347266814f446037ce574c621b23b235340643b4
Chirag Patel <chirag@chirag.name>
2007-07-18 22:58:04 +0000

    Extended ziya line charts so that there is both a (1) discrete chart with dots & (2) continuous chart with x-axis labels skipped
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@9 04a73145-ec33-0410-b10b-c8dba00f0e28

--- app/controllers/chart_controller.rb
+++ app/controllers/chart_controller.rb
@@ -7,13 +7,23 @@ class ChartController < ApplicationController
    
   end
   
-  def line_chart      
+  def line_chart_dots
     graph  = Ziya::Charts::Line.new( nil, nil, "heartrate" ) #write up this line in the blog
     
-    heartrates = Heartrate.find(:all)     # get information from the database
+    heartrates = Heartrate.find(:all , :limit => 10)     # get information from the database    
     graph.add(:axis_category_text, heartrates.map {|a| a.timeStamp  } )      
     # create an array of labels by calling the name method on each animal
-    graph.add(:series, "Heartrate", heartrates.map {|a| a.heartRate} ) 
+    graph.add(:series, "Discrete Heartrate", heartrates.map {|a| a.heartRate} ) 
+    render :xml => graph.to_s
+  end  
+  
+  def line_chart_continuous
+    graph  = Ziya::Charts::Line.new( nil, nil, "heartrate2" ) #write up this line in the blog
+    
+    heartrates = Heartrate.find(:all , :limit => 20)     # get information from the database    
+    graph.add(:axis_category_text, heartrates.map {|a| a.timeStamp  } )      
+    # create an array of labels by calling the name method on each animal
+    graph.add(:series, "Continuous Heartrate", heartrates.map {|a| a.heartRate} ) 
     render :xml => graph.to_s
   end  
 end
