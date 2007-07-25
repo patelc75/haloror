a59e00b76b17a840061e94df0a6f92e03b26fc22
Chirag Patel <chirag@chirag.name>
2007-07-25 02:00:11 +0000

    First cut at the chart with a CSS layout with tabs (app/views/chart/halo.rhtml)
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@20 04a73145-ec33-0410-b10b-c8dba00f0e28

--- app/controllers/chart_controller.rb
+++ app/controllers/chart_controller.rb
@@ -7,13 +7,14 @@ class ChartController < ApplicationController
    
   end
   
-  def line_chart_dots
+  def line_chart_discrete
     graph  = Ziya::Charts::Line.new( nil, nil, "heartrate" ) #write up this line in the blog
     
     heartrates = Heartrate.find(:all , :limit => 10)     # get information from the database    
     graph.add(:axis_category_text, heartrates.map {|a| a.timeStamp  } )      
     # create an array of labels by calling the name method on each animal
     graph.add(:series, "Discrete Heartrate", heartrates.map {|a| a.heartRate} ) 
+#    graph.add(:user_data, :chart_type, "discrete" ) 
     render :xml => graph.to_s
   end  
   
@@ -24,6 +25,7 @@ class ChartController < ApplicationController
     graph.add(:axis_category_text, heartrates.map {|a| a.timeStamp  } )      
     # create an array of labels by calling the name method on each animal
     graph.add(:series, "Continuous Heartrate", heartrates.map {|a| a.heartRate} ) 
+#    graph.add(:user_data, :chart_type, "continuous" ) 
     render :xml => graph.to_s
   end  
 end
