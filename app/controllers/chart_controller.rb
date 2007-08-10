6014f2c815c1e5196a8ccb30b3a47e189dc4d214
Chirag Patel <chirag@chirag.name>
2007-08-10 02:51:59 +0000

    Add support for static heartrate line chart (2 minutes of realistic looking heartrate)
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@24 04a73145-ec33-0410-b10b-c8dba00f0e28

--- app/controllers/chart_controller.rb
+++ app/controllers/chart_controller.rb
@@ -6,6 +6,17 @@ class ChartController < ApplicationController
   def line
    
   end
+
+  def line_chart_static
+    graph  = Ziya::Charts::Line.new( nil, nil, "heartrate" ) #write up this line in the blog
+    
+    heartrates = Heartrate.find(:all , :limit => 10)     # get information from the database    
+    graph.add(:axis_category_text, ["8:32:15AM", "8:32:30AM", "8:32:45AM", "8:33:00AM","8:33:15AM", "8:33:30AM", "8:33:45AM", "8:34:00AM", "8:34:15AM", "8:34:30AM"] )      
+    # create an array of labels by calling the name method on each animal
+    graph.add(:series, "Discrete Heartrate", [72, 74, 73, 74, 72, 71,74, 72, 71, 70] ) 
+    # graph.add(:user_data, :chart_type, "discrete" ) 
+    render :xml => graph.to_s
+  end  
   
   def line_chart_discrete
     graph  = Ziya::Charts::Line.new( nil, nil, "heartrate" ) #write up this line in the blog
@@ -14,7 +25,7 @@ class ChartController < ApplicationController
     graph.add(:axis_category_text, heartrates.map {|a| a.timeStamp  } )      
     # create an array of labels by calling the name method on each animal
     graph.add(:series, "Discrete Heartrate", heartrates.map {|a| a.heartRate} ) 
-#    graph.add(:user_data, :chart_type, "discrete" ) 
+    # graph.add(:user_data, :chart_type, "discrete" ) 
     render :xml => graph.to_s
   end  
   
