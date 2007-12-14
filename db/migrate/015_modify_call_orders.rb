fdb8d22d7c5c84bdbb97f4c3691b9430c382459b
Chirag Patel <chirag@chirag.name>
2007-12-14 20:23:07 +0000

    Add fields to facilitate buttons in caregiver table and add timestamp to events table
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@85 04a73145-ec33-0410-b10b-c8dba00f0e28

---
+++ db/migrate/015_modify_call_orders.rb
@@ -0,0 +1,11 @@
+class ModifyCallOrders < ActiveRecord::Migration
+  def self.up
+  	add_column :call_orders, :active, :integer, :limit => 1, :null=> false 
+	add_column :call_orders, :phone_active, :integer, :limit => 1, :null=> false 
+	add_column :call_orders, :email_active, :integer, :limit => 1, :null=> false 
+	add_column :call_orders, :text_active, :integer, :limit => 1, :null=> false 
+  end
+
+  def self.down
+  end
+end
---
+++ db/migrate/016_modify_events.rb
@@ -0,0 +1,8 @@
+class ModifyEvents < ActiveRecord::Migration
+  def self.up
+	add_column :events, :timestamp, :timestamp_with_time_zone
+  end
+
+  def self.down
+  end
+end
