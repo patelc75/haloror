7ecbe1e8be189a7bb3b5be5f63f351e96ac6f280
Chirag Patel <chirag@chirag.name>
2007-12-27 20:50:59 +0000

    Added hook for fall to be inserted into Events table
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@103 04a73145-ec33-0410-b10b-c8dba00f0e28

--- app/models/fall_observer.rb
+++ app/models/fall_observer.rb
@@ -1,5 +1,6 @@
 class FallObserver < ActiveRecord::Observer
 	def before_save(fall)
 		email = CriticalMailer.deliver_fall_notification(fall.user)
+		Event.create(:user_id => fall.user_id, :kind => Fall.class_name, :kind_id => fall.id, :timestamp => fall.timestamp)
 	end
 end
