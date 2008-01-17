66992ee9226c1295ca47d47fc3e5848ad0a6d540
Chirag Patel <chirag@chirag.name>
2008-01-17 01:16:01 +0000

    Added user profile every time a user is created
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@142 04a73145-ec33-0410-b10b-c8dba00f0e28

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     @user.save!
+    Profile.create(:user_id => @user.id)
     self.current_user = @user
     #redirect_back_or_default('/')
     flash[:notice] = "Thanks for signing up!"
