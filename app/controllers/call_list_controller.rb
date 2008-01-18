51ea34049455a3f11727024f767e20dc1b063339
Chirag Patel <chirag@chirag.name>
2008-01-18 16:31:52 +0000

    "1st", "2nd", etc, labels
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@146 04a73145-ec33-0410-b10b-c8dba00f0e28

--- app/controllers/call_list_controller.rb
+++ app/controllers/call_list_controller.rb
@@ -1,8 +1,18 @@
 class CallListController < ApplicationController
   def show
-    #layout :none	
     @call_list = User.find(params[:id])
-    #render(:layout => false) # never use a layout
+    
+    @num_ref = {}
+    @num_ref[0] = 'th'
+    @num_ref[1] = 'st'
+    @num_ref[2] = 'nd'
+    @num_ref[3] = 'rd'
+    @num_ref[4] = 'th'
+    @num_ref[5] = 'th'
+    @num_ref[6] = 'th'
+    @num_ref[7] = 'th'
+    @num_ref[8] = 'th'
+    @num_ref[9] = 'th'
   end
   
   def sort
