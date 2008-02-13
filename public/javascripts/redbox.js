429eed55af26b20920d170c8ce5ccf100ebe1735
Chirag Patel <chirag@chirag.name>
2008-02-13 05:55:43 +0000

    Center lightbox in IE
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@180 04a73145-ec33-0410-b10b-c8dba00f0e28

--- public/javascripts/redbox.js
+++ public/javascripts/redbox.js
@@ -76,7 +76,7 @@ var RedBox = {
     var dimensions = Element.getDimensions($("RB_window"));
     var width = dimensions.width;
     var height = dimensions.height;        
-
+	
     $("RB_window").style['left'] = ((pagesize[0] - width)/2) + "px";
     $("RB_window").style['top'] = ((pagesize[1] - height)/2) + "px";
   },
