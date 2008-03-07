143bbbd0b497bcd1a1e83947ec54ffef445f4203
Chirag Patel <chirag@chirag.name>
2008-03-07 18:57:58 +0000

    1)creating table and filling it using migrations ( This fills Carrier table with service provider names ) 2) Mapping profile table and carrier table
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@233 04a73145-ec33-0410-b10b-c8dba00f0e28

---
+++ db/migrate/047_create_carriers.rb
@@ -0,0 +1,24 @@
+class CreateCarriers < ActiveRecord::Migration
+  def self.up
+    create_table :carriers do |t|
+      t.column :id, :primary_key, :null => false 
+      t.column :name, :string
+      t.column :domain, :string
+    end
+    Carrier.create(:name => "Cingular (now AT&T)", :domain => "@cingularname.com")
+    Carrier.create(:name => "Verizon", :domain => "@vtext.com")
+    Carrier.create(:name => "Boost Mobile", :domain => "@myboostmobile.com")
+    Carrier.create(:name => "Nextel", :domain => "@messaging.nextel.com")
+    Carrier.create(:name => "Alltel", :domain => "@message.alltel.com")
+    Carrier.create(:name => "Sprint PCS", :domain => "@messaging.sprintpcs.com")
+    Carrier.create(:name => "AT&T Wireless", :domain => "@mmode.com")
+    Carrier.create(:name => "Virgin Mobile USA", :domain => "@vmobl.com")
+    Carrier.create(:name => "T-Mobile", :domain => "@tmomail.net ")
+    Carrier.create(:name => "Not in the list", :domain => "@teleflip.com")
+   
+  end
+
+  def self.down
+    drop_table :carriers
+  end
+end
---
+++ db/migrate/048_add_carrier_to_profiles.rb
@@ -0,0 +1,9 @@
+class AddCarrierToProfiles < ActiveRecord::Migration
+  def self.up
+    add_column :profiles, :carrier_id, :integer
+  end
+
+  def self.down
+    remove_column :profiles, :carrier_id 
+  end
+end
