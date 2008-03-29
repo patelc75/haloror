71d3abb72b9cea4efcb7f5e250b0b75b81064bf1
Chirag Patel <chirag@chirag.name>
2008-03-29 13:27:10 +0000

    Battery Chart
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@344 04a73145-ec33-0410-b10b-c8dba00f0e28

--- app/controllers/batteries_controller.rb
+++ app/controllers/batteries_controller.rb
@@ -77,4 +77,39 @@ class BatteriesController < RestfulAuthController
       format.xml  { head :ok }
     end
   end
+  
+  def device
+    #@device = Device.find(params[:id])
+    render :layout => 'application'
+  end
+  
+  def chart_data
+    device = Device.find(params[:id])
+    
+    @readings = {}
+    device.batteries.each do |reading|
+      #date = reading.timestamp.to_date.to_s(:db)
+      date = reading.timestamp.to_s
+      
+      @readings[date] = []
+      @readings[date] = reading
+    end
+    
+    render :layout => false
+    
+    # User.find(params[:id]).access_logs.each do |log|
+    #       date = log.created_at.to_date.to_s(:db)
+    #       #date = date[0]
+    #       
+    #       unless @logs[date]
+    #         @logs[date] = {:successful => 0, :failed => 0}
+    #       end
+    #       
+    #       if log.status == 'successful'
+    #         @logs[date][:successful] += 1
+    #       elsif log.status == 'failed'
+    #         @logs[date][:failed] += 1
+    #       end      
+    #     end
+  end
 end
