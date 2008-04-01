98ae34d39b2ef74bc4d4a19b7236c3f4e4eef980
Chirag Patel <chirag@chirag.name>
2008-04-01 22:00:47 +0000

    Database health check for Neospire: "Upon the successful execution of your script/page, the output of the script/page would need to display the following text via HTML if successful: "CONGRATULATIONS 200". Once the script/page has been created, notify us with the location of the script/page to be monitored and we will add the test to our monitoring system. Anytime our monitoring system does not receive "CONGRATULATIONS 200" our administrators will be alerted. This will help ensure the availability of your solution and will also be used to verify successful recovery of your servers after security updates are applied or system maintenance occurs."
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@362 04a73145-ec33-0410-b10b-c8dba00f0e28

---
+++ app/controllers/util_controller.rb
@@ -0,0 +1,11 @@
+class UtilController < ApplicationController
+  session :off
+
+  # Monitors that the application and database connection is alive
+  def check
+    #if ServerInstance.is_task_server?
+      ActiveRecord::Base.connection.select_value("select 1")
+    #end
+    render :text => 'CONGRATULATIONS 200'
+  end
+end
