87599d9fdfda5e7bd4def40b0a5e9ca2c57ac445
Chirag Patel <chirag@chirag.name>
2008-04-06 03:00:49 +0000

    Rename alert_type to type - the table column is called alert_type in schema.rb
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@414 04a73145-ec33-0410-b10b-c8dba00f0e28

--- db/migrate/090_create_battery_alert_types.rb
+++ db/migrate/090_create_battery_alert_types.rb
@@ -1,9 +1,9 @@
 class CreateBatteryAlertTypes < ActiveRecord::Migration
   def self.up
-    AlertType.create :alert_group_id => 6, :alert_type => BatteryPlugged.class_name
-    AlertType.create :alert_group_id => 6, :alert_type => BatteryUnplugged.class_name
-    AlertType.create :alert_group_id => 7, :alert_type => BatteryChargeComplete.class_name
-    AlertType.create :alert_group_id => 7, :alert_type => BatteryCritical.class_name
+    AlertType.create :alert_group_id => 6, :type => BatteryPlugged.class_name
+    AlertType.create :alert_group_id => 6, :type => BatteryUnplugged.class_name
+    AlertType.create :alert_group_id => 7, :type => BatteryChargeComplete.class_name
+    AlertType.create :alert_group_id => 7, :type => BatteryCritical.class_name
   end
 
   def self.down
