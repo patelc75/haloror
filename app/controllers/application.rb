626a6c5812232645ede7ff5bac871a1de14eeee3
Chirag Patel <chirag@chirag.name>
2008-04-06 19:30:16 +0000

    format_datetime in application helper
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@427 04a73145-ec33-0410-b10b-c8dba00f0e28

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -66,7 +66,13 @@ class ApplicationController < ActionController::Base
   def camelcase_to_spaced(word)
     word.gsub(/([A-Z])/, " \\1")
   end
-
+  
+  def format_datetime(datetime)
+    return datetime if !datetime.respond_to?(:strftime)
+    datetime = @user.tz.utc_to_local(datetime) if @user
+    datetime.strftime("%m-%d-%Y %I:%M %p")
+  end
+  
   protected
   def authenticate
     unless logged_in?
