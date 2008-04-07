fe83b34876b2e0461a62c85fd905c9b97d509490
Chirag Patel <chirag@chirag.name>
2008-04-07 22:22:57 +0000

    Graph for hours charging.
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@434 04a73145-ec33-0410-b10b-c8dba00f0e28

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -9,7 +9,7 @@ class DevicesController < ApplicationController
     
     i = 0
     now = Date.today.to_time
-    while i < 1
+    while i < 30
       time = now.ago(i*86400)
       @periods[time] = get_data(params[:type], time, time.tomorrow, @device)
       i += 1
@@ -64,7 +64,11 @@ class DevicesController < ApplicationController
       total_time += end_time.to_i - @event.timestamp.to_i
     end
     
-    total_time = total_time.to_f / 60 / 60
+    if total_time > 0
+      total_time = total_time.to_f / 60 / 60
+    else
+      0
+    end
   end
   
   def out_of_range_data(start_time,end_time,device)
@@ -107,36 +111,40 @@ class DevicesController < ApplicationController
       total_time += end_time.to_i - @event.created_at.to_i
     end
     
-    total_time = total_time.to_f / 60 / 60
+    if total_time > 0
+      total_time = total_time.to_f / 60 / 60
+    else
+      0
+    end
   end
   
   def battery_charge_data(start_time,end_time,device)
-    removed_before = StrapRemoved.find(:first, :conditions => "timestamp < '#{start_time}' and device_id = '#{device.id}'")
-    fastened_before = StrapFastened.find(:first, :conditions => "timestamp < '#{start_time}' and device_id = '#{device.id}'")
+    plugged_before = BatteryPlugged.find(:first, :conditions => "timestamp < '#{start_time}' and device_id = '#{device.id}'")
+    unplugged_before = BatteryUnplugged.find(:first, :conditions => "timestamp < '#{start_time}' and device_id = '#{device.id}'")
     
     cur_time = 0
     
     # does strap begin day removed?
-    if removed_before and fastened_before
-      if removed_before.timestamp < fastened_before.timestamp
+    if plugged_before and unplugged_before
+      if plugged_before.timestamp < unplugged_before.timestamp
         cur_time = start_time.to_i
       end 
     end
     
     events = []
     
-    StrapRemoved.find(:all, :conditions => "timestamp > '#{start_time}' and timestamp < '#{end_time}' and device_id = '#{device.id}'").reverse.each do |event|
+    BatteryPlugged.find(:all, :conditions => "timestamp > '#{start_time}' and timestamp < '#{end_time}' and device_id = '#{device.id}'").reverse.each do |event|
       events << event
     end
     
-    StrapFastened.find(:all, :conditions => "timestamp > '#{start_time}' and timestamp < '#{end_time}' and device_id = '#{device.id}'").reverse.each do |event|
+    BatteryUnplugged.find(:all, :conditions => "timestamp > '#{start_time}' and timestamp < '#{end_time}' and device_id = '#{device.id}'").reverse.each do |event|
       events << event
     end
     
     total_time = 0
     
     events.sort_by { |event| event[:timestamp] }.each do |event|
-      if event.type.class_name == 'StrapRemoved'
+      if event.type.class_name == 'BatteryPlugged'
         cur_time = event.timestamp.to_i
       else
         total_time += event.timestamp.to_i - cur_time.to_i
@@ -146,10 +154,14 @@ class DevicesController < ApplicationController
     end
     
     # time till end of day      
-    if @event.type.class_name == 'StrapRemoved'
-      total_time += end_time.to_i - event.timestamp.to_i
+    if @event and @event.type.class_name == 'BatteryPlugged'
+      total_time += end_time.to_i - @event.timestamp.to_i
     end
     
-    total_time = total_time.to_f / 60 / 60
+    if total_time > 0
+      total_time = total_time.to_f / 60 / 60
+    else
+      0
+    end
   end
 end
