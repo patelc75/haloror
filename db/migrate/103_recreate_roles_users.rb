39c3ce52e1f22bec8738efc0635302b01a6e376d
Chirag Patel <chirag@chirag.name>
2008-04-10 03:27:58 +0000

    Recreate roles_users table with primary key.
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@464 04a73145-ec33-0410-b10b-c8dba00f0e28

---
+++ db/migrate/103_recreate_roles_users.rb
@@ -0,0 +1,17 @@
+class RecreateRolesUsers < ActiveRecord::Migration
+  def self.up
+    drop_table :roles_users
+    
+    create_table :roles_users, :force => true do |t|
+      t.column :id, :primary_key, :null => false
+      t.column :user_id,          :integer
+      t.column :role_id,          :integer
+      t.column :created_at,       :datetime
+      t.column :updated_at,       :datetime
+    end
+  end
+
+  def self.down
+    drop_table :roles_users
+  end
+end
