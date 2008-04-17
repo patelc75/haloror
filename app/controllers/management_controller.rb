f6b1f704c5ca6787cd62365432a963bbafabc801
Matt Smith <matt@mattup.com>
2008-04-17 07:07:59 +0000

    For a "task" ticket, use refs #78
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@489 04a73145-ec33-0410-b10b-c8dba00f0e28

--- app/controllers/management_controller.rb
+++ app/controllers/management_controller.rb
@@ -67,6 +67,50 @@ class ManagementController < ApplicationController
     render :layout => false 
   end
   
+  def firmware_upgrade
+    if params[:keyID].to_i != 0
+      fw = FirmwareUpgrade.find(params[:keyID])
+      fw.update_attribute(params[:field].to_sym, params[:value])
+      fw.save
+      render :json => '{success: true}'
+    else
+      fw = FirmwareUpgrade.new()
+      fw.version = 'New Version'
+      fw.filename = 'filename'
+      ftp = Ftp.find(:first)
+			fw.ftp_id = ftp.id if ftp
+			fw.description = 'description'
+      fw.date_added = Date.today
+      fw.save
+      render :json => "{success:true, newID: #{fw.id}}", :layout => false
+    end  
+  end
+  
+  def firmware_upgrade_delete
+    if params[:ids]
+      RAILS_DEFAULT_LOGGER.warn(params[:ids])
+      str = params[:ids].chop
+        RAILS_DEFAULT_LOGGER.warn(str)
+      str = str[1, str.length-1]
+          RAILS_DEFAULT_LOGGER.warn(str)
+      str_array = str.split(',')
+      str_array.each do |s|
+        FirmwareUpgrade.delete(s)
+      end
+      fus = FirmwareUpgrade.find(:all, :include => 'ftp') 
+      		fus_json = "{ 'rows': ["
+      		count = 0
+      		fus.each do |fu|
+      			fus_json = fus_json + fu.to_json()
+      			count = count + 1
+      			unless count == fus.length
+      				fus_json = "#{fus_json}, "
+      			end
+      		end
+      		fus_json = "#{fus_json} ] }"
+      render :json => fus_json, :layout => false
+    end
+  end
   protected
   
   def get_device
