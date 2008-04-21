5767032d73208515f6d76b0ee5298d8ff68100bc
Chirag Patel <chirag@chirag.name>
2008-04-21 04:22:36 +0000

    Expand testing for device_strap_status
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@495 04a73145-ec33-0410-b10b-c8dba00f0e28

--- test/unit/alerts.rb
+++ test/unit/alerts.rb
@@ -4,21 +4,14 @@ class AlertsTest < ActiveSupport::TestCase
 
   # Replace this with your real tests.
   def test_mgmt_query
-    ActionMailer::Base.deliveries = []
-    ActionMailer::Base.delivery_method = :test
-    assert_equal 0, ActionMailer::Base.deliveries.size
+    setup_email
 
     MgmtQuery.connection.execute("delete from device_latest_queries")
     MgmtQuery.connection.execute("delete from devices")
     MgmtQuery.connection.execute("delete from devices_users")
     MgmtQuery.connection.execute("delete from mgmt_queries")
 
-    user = User.find_by_login('test')
-    user ||= User.create(:login => 'test', :email => 'test@mailinator.com', :password => '234jk234@1!d', :password_confirmation => '234jk234@1!d')
-    device = Device.create(:serial_number => '0123456789', :device_type => 'test')
-    device.users << user
-    device.save!
-    assert_equal 1, device.users.size
+    device = get_test_device
 
     1.upto(10) do
       create_and_test_mgmt_query(device, Time.now, 0)
@@ -35,21 +28,81 @@ class AlertsTest < ActiveSupport::TestCase
     create_and_test_mgmt_query(device, yesterday, 1)
   end
 
-  def test_vitals
-    ActionMailer::Base.deliveries = []
-    ActionMailer::Base.delivery_method = :test
+  def test_straps
+    setup_email
+    device = get_test_device
+    
+    num = Device.connection.select_value("select count(*) from device_strap_status where id = #{device.id}").to_i
+    assert_equal 0, num
 
-    assert_equal 0, ActionMailer::Base.deliveries.size
+    event = StrapFastened.new
+    event.device = device
+    event.user = get_test_user
+    event.timestamp = Time.now
+    event.save!
+
+    num = Device.connection.select_value("select count(*) from device_strap_status where id = #{device.id}").to_i
+    assert_equal 1, num
+
+    num = Device.connection.select_value("select count(*) from device_strap_status where is_fastened > 0 and id = #{device.id}").to_i
+    assert_equal 1, num
+
+    event = StrapRemoved.new
+    event.device = device
+    event.user = get_test_user
+    event.timestamp = Time.now
+    event.save!
+
+    num = Device.connection.select_value("select count(*) from device_strap_status where id = #{device.id}").to_i
+    assert_equal 1, num
+
+    num = Device.connection.select_value("select count(*) from device_strap_status where is_fastened > 0 and id = #{device.id}").to_i
+    assert_equal 0, num
+
+    num = Device.connection.select_value("select count(*) from device_strap_status where is_fastened = 0 and id = #{device.id}").to_i
+    assert_equal 1, num
+    
+  end
+  
+  def test_vitals
+    setup_email
     Vital.job_detect_unavailable_devices
-    assert_equal 0, ActionMailer::Base.deliveries.size
+    assert_number_emails(0)
+    
   end
 
   private
+  def assert_number_emails(num)
+    assert_equal num, ActionMailer::Base.deliveries.size
+  end
+
+  def setup_email
+    ActionMailer::Base.deliveries = []
+    ActionMailer::Base.delivery_method = :test
+    assert_number_emails(0)
+  end
+
+  def get_test_device
+    user = get_test_user
+
+    device = Device.create(:serial_number => '0123456789', :device_type => 'test')
+    device.users << user
+    device.save!
+    assert_equal 1, device.users.size
+
+    device
+  end
+
+  def get_test_user
+    user = User.find_by_login('test')
+    user ||= User.create(:login => 'test', :email => 'test@mailinator.com', :password => '234jk234@1!d', :password_confirmation => '234jk234@1!d')
+  end
+
   def create_and_test_mgmt_query(device, timestamp, expected_number_of_emails)
     ActionMailer::Base.deliveries = []
     MgmtQuery.create(:device_id => device.id, :timestamp_device => timestamp, :timestamp_server => timestamp)
     MgmtQuery.job_detect_disconnected_users
-    assert_equal expected_number_of_emails, ActionMailer::Base.deliveries.size
+    assert_number_emails(expected_number_of_emails)
   end
 
 end
