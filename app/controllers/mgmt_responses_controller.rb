4aa00ccc52a567b103090b4b172da495ee2dea89
Chirag Patel <chirag@chirag.name>
2008-04-24 15:13:27 +0000

    fixes #97
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@514 04a73145-ec33-0410-b10b-c8dba00f0e28

--- app/controllers/mgmt_responses_controller.rb
+++ app/controllers/mgmt_responses_controller.rb
@@ -3,31 +3,31 @@ class MgmtResponsesController < RestfulAuthController
     # 1. Create Response, link with Cmd
     request = params[:management_response_device]
         
-    cmd = MgmtCmd.find(:first, :conditions => {:cmd_type => request[:cmd_type], :device_id => request[:device_id]}, :order => "id asc")
-    
-    unless cmd.mgmt_response
-      response = MgmtResponse.new
-      response.mgmt_cmd_id = cmd.id
-      response.timestamp_device = request[:timestamp]
-      response.timestamp_server = Time.now
-      response.save
+    if cmd = MgmtCmd.find(:first, :conditions => {:cmd_type => request[:cmd_type], :device_id => request[:device_id]}, :order => "id asc")
+      unless cmd.mgmt_response
+        response = MgmtResponse.new
+        response.mgmt_cmd_id = cmd.id
+        response.timestamp_device = request[:timestamp]
+        response.timestamp_server = Time.now
+        response.save
       
-      cmd.pending = false
-      cmd.save
+        cmd.pending = false
+        cmd.save
       
-      if i = request[:info]
-        info = DeviceInfo.new
-        info.device_id = request[:device_id]
-        info.mgmt_response_id = response.id
-        info.user_id = i[:user_id]
-        info.serial_number = i[:serial_num]
-        info.mac_address = i[:mac_address]
-        info.vendor = i[:vendor]
-        info.model = i[:model]
-        info.hardware_version = i[:hardware_version]
-        info.software_version = i[:software_version]        
-        info.save
-      end      
+        if i = request[:info]
+          info = DeviceInfo.new
+          info.device_id = request[:device_id]
+          info.mgmt_response_id = response.id
+          info.user_id = i[:user_id]
+          info.serial_number = i[:serial_num]
+          info.mac_address = i[:mac_address]
+          info.vendor = i[:vendor]
+          info.model = i[:model]
+          info.hardware_version = i[:hardware_version]
+          info.software_version = i[:software_version]        
+          info.save
+        end      
+      end
     end
     
     render :nothing => true
