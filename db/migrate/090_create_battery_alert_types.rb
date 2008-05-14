7cbb9a522f0cb30fc4251ae62ba6f12692db7a95
Chirag Patel <chirag@chirag.name>
2008-05-14 19:10:14 +0000

    Fix migration error when populating alert_types
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@639 04a73145-ec33-0410-b10b-c8dba00f0e28

--- db/migrate/090_create_battery_alert_types.rb
+++ db/migrate/090_create_battery_alert_types.rb
@@ -1,9 +1,10 @@
 class CreateBatteryAlertTypes < ActiveRecord::Migration
   def self.up
-    AlertType.create :alert_group_id => 6, :type => BatteryPlugged.class_name
-    AlertType.create :alert_group_id => 6, :type => BatteryUnplugged.class_name
-    AlertType.create :alert_group_id => 7, :type => BatteryChargeComplete.class_name
-    AlertType.create :alert_group_id => 7, :type => BatteryCritical.class_name
+#Not needed because it is re-populated in migration # 105
+#Commenting out because it causes an error when running migrations#    AlertType.create :alert_group_id => 6, :type => BatteryPlugged.class_name
+#    AlertType.create :alert_group_id => 6, :type => BatteryUnplugged.class_name
+#    AlertType.create :alert_group_id => 7, :type => BatteryChargeComplete.class_name
+#    AlertType.create :alert_group_id => 7, :type => BatteryCritical.class_name
   end
 
   def self.down
