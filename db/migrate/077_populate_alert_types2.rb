469a5bfdff6a1dd41574a935cfdc18a99239c426
Chirag Patel <chirag@chirag.name>
2008-05-14 18:58:28 +0000

    Fix migration error when populating alert_types
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@637 04a73145-ec33-0410-b10b-c8dba00f0e28

--- db/migrate/077_populate_alert_types2.rb
+++ db/migrate/077_populate_alert_types2.rb
@@ -1,34 +1,36 @@
 class PopulateAlertTypes2 < ActiveRecord::Migration
   def self.up
-    execute "truncate table alert_types"
+#Not needed because it is re-populated in migration # 105
+#Commenting out because it causes an error when running migrations
+#    execute "truncate table alert_types"
     
-    # info
-    AlertType.create :alert_group_id => 1, :type => BatteryChargeComplete.class_name
-    AlertType.create :alert_group_id => 1, :type => BatteryUnplugged.class_name
-    AlertType.create :alert_group_id => 1, :type => BatteryPlugged.class_name
-    AlertType.create :alert_group_id => 1, :type => StrapFastened.class_name
-    
-    # high
-    AlertType.create :alert_group_id => 2, :type => StrapRemoved.class_name
-    AlertType.create :alert_group_id => 2, :type => BatteryCritical.class_name
-    AlertType.create :alert_group_id => 2, :type => DeviceUnavailableAlert.class_name
-    AlertType.create :alert_group_id => 2, :type => GatewayOfflineAlert.class_name
-    
-    # critical
-    AlertType.create :alert_group_id => 3, :type => Fall.class_name
-    AlertType.create :alert_group_id => 3, :type => Panic.class_name
-    
-    # battery
-    AlertType.create :alert_group_id => 4, :type => BatteryChargeComplete.class_name
-    AlertType.create :alert_group_id => 4, :type => BatteryUnplugged.class_name
-    AlertType.create :alert_group_id => 4, :type => BatteryPlugged.class_name
-    AlertType.create :alert_group_id => 4, :type => BatteryCritical.class_name
-    
-    # connectivity
-    AlertType.create :alert_group_id => 5, :type => StrapRemoved.class_name
-    AlertType.create :alert_group_id => 5, :type => StrapFastened.class_name
-    AlertType.create :alert_group_id => 5, :type => DeviceUnavailableAlert.class_name
-    AlertType.create :alert_group_id => 5, :type => GatewayOfflineAlert.class_name
+#    # info
+#    AlertType.create :alert_group_id => 1, :type => BatteryChargeComplete.class_name
+#    AlertType.create :alert_group_id => 1, :type => BatteryUnplugged.class_name
+#    AlertType.create :alert_group_id => 1, :type => BatteryPlugged.class_name
+#    AlertType.create :alert_group_id => 1, :type => StrapFastened.class_name
+#    
+#    # high
+#    AlertType.create :alert_group_id => 2, :type => StrapRemoved.class_name
+#    AlertType.create :alert_group_id => 2, :type => BatteryCritical.class_name
+#    AlertType.create :alert_group_id => 2, :type => DeviceUnavailableAlert.class_name
+#    AlertType.create :alert_group_id => 2, :type => GatewayOfflineAlert.class_name
+#    
+#    # critical
+#    AlertType.create :alert_group_id => 3, :type => Fall.class_name
+#    AlertType.create :alert_group_id => 3, :type => Panic.class_name
+#    
+#    # battery
+#    AlertType.create :alert_group_id => 4, :type => BatteryChargeComplete.class_name
+#    AlertType.create :alert_group_id => 4, :type => BatteryUnplugged.class_name
+#    AlertType.create :alert_group_id => 4, :type => BatteryPlugged.class_name
+#    AlertType.create :alert_group_id => 4, :type => BatteryCritical.class_name
+#    
+#    # connectivity
+#    AlertType.create :alert_group_id => 5, :type => StrapRemoved.class_name
+#    AlertType.create :alert_group_id => 5, :type => StrapFastened.class_name
+#    AlertType.create :alert_group_id => 5, :type => DeviceUnavailableAlert.class_name
+#    AlertType.create :alert_group_id => 5, :type => GatewayOfflineAlert.class_name
   end
 
   def self.down
