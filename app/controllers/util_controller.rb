a78400e620bb58e56fc5692e188e52234ce6d4c0
Chirag Patel <chirag@chirag.name>
2008-05-28 19:48:25 +0000

    info page for server config
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@686 04a73145-ec33-0410-b10b-c8dba00f0e28

--- app/controllers/util_controller.rb
+++ app/controllers/util_controller.rb
@@ -1,11 +1,19 @@
 class UtilController < ApplicationController
-  session :off
+  #session :off
 
   # Monitors that the application and database connection is alive
+  
   def check
     #if ServerInstance.is_task_server?
-      ActiveRecord::Base.connection.select_value("select 1")
+    ActiveRecord::Base.connection.select_value("select 1")
     #end
+    
+    # Neospire excpects this message. Please do NOT modify this.
     render :text => 'CONGRATULATIONS 200'
+  end
+  
+  def info
+    #render :text => request.host
+    #render :text => request.env["HTTP_HOST"].to_s
   end
 end
