c560b61592dcc0ea3f3464e59d441d77cbbe726f
Chirag Patel <chirag@chirag.name>
2008-05-31 20:28:36 +0000

    Successful activation email fixed. fixes #134
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@693 04a73145-ec33-0410-b10b-c8dba00f0e28

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,12 +1,9 @@
 module UsersHelper
-
- def current_host
+  def current_host
     Thread.current[:host]
- end
+  end
  
- def self.current_host=(host)
-  Thread.current[:host] = host
- end
-  
-  
+  def self.current_host=(host)
+    Thread.current[:host] = host
+  end
 end
--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,27 +1,27 @@
 class UserMailer < ActionMailer::ARMailer
-   include UsersHelper
+  include UsersHelper
   def signup_notification(user)
-  setup_email(user)
+    setup_email(user)
     @subject    += 'Please activate your new account'
   
     #@body[:url]  = "http://67-207-146-58.slicehost.net/activate/#{user.activation_code}"
     #@body[:url]  = "http://localhost:3000/activate/#{user.activation_code}"
 	
-	@body[:url]  = "http://#{current_host}/activate/#{user.activation_code}"
+    @body[:url]  = "http://#{current_host}/activate/#{user.activation_code}"
   end
   
   def activation(user)
     setup_email(user)
     @subject    += 'Your account has been activated!'
-    @body[:url]  = "http://67-207-146-58.slicehost.net"
+    @body[:url]  = "http://#{current_host}/login"
   end
   
   protected
-    def setup_email(user)
-      @recipients  = "#{user.email}"
-      @from        = "no-reply@halomonitoring.com"
-      @subject     = "Halo: "
-      @sent_on     = Time.now
-      @body[:user] = user
-    end
+  def setup_email(user)
+    @recipients  = "#{user.email}"
+    @from        = "no-reply@halomonitoring.com"
+    @subject     = "Halo: "
+    @sent_on     = Time.now
+    @body[:user] = user
+  end
 end
