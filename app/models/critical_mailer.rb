befbf65f2f82669fb37c0453ff83aa8437a9c50e
Matt Smith <matt@mattup.com>
2008-06-09 22:13:49 +0000

    alert_option_by_type not alert_option testing on sdev
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@750 04a73145-ec33-0410-b10b-c8dba00f0e28

--- app/models/critical_mailer.rb
+++ app/models/critical_mailer.rb
@@ -66,7 +66,7 @@ class CriticalMailer < ActionMailer::ARMailer
     operators = User.operators
     if operators
       operators.each do |operator|
-       recipients_setup(operator, operator.alert_option(fall))
+       recipients_setup(operator, operator.alert_option_by_type(fall))
       end
     end
     if(fall.user.profile)
@@ -87,7 +87,7 @@ class CriticalMailer < ActionMailer::ARMailer
     operators = User.operators
     if operators
       operators.each do |operator|
-        recipients_setup(operator, operator.alert_option(panic))
+        recipients_setup(operator, operator.alert_option_by_type(panic))
       end
     end
     if(panic.user.profile)
