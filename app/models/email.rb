5c05ae2a578baccdcd55fca485c6d174b36ab42d
Matt Smith <matt@mattup.com>
2008-06-11 15:29:29 +0000

    Email notification Rescue
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@756 04a73145-ec33-0410-b10b-c8dba00f0e28

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,6 +1,10 @@
 class Email < ActiveRecord::Base
   def self.notify_by_priority
-    ActionMailer::ARSendmail.run
+    begin
+      ActionMailer::ARSendmail.run
+    rescue
+      RAILS_DEFAULT_LOGGER.warn("RESCUE called in Email.notify_by_priority")
+    end
     ActiveRecord::Base.verify_active_connections!()
   end
 end
