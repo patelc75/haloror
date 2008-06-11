310f458f5b3b881ca580f49a85ee2206210dc9e5
Matt Smith <matt@mattup.com>
2008-06-11 20:26:35 +0000

    check number of records instead of timestamp... for fall and panic
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@758 04a73145-ec33-0410-b10b-c8dba00f0e28

--- spec/models/fall_spec.rb
+++ spec/models/fall_spec.rb
@@ -1,20 +1,16 @@
-
- 
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Fall do
   
-  before(:each) do
+  before(:all) do
+    @no_records = Fall.count
     curl_cmd = 'curl -v -H "Content-Type: text/xml" -d "<fall><magnitude>60</magnitude><timestamp>Mon Dec 25 15:52:55 -0600 2007</timestamp><user_id>1</user_id></fall>" "http://localhost:3000/falls?gateway_id=1&auth=9ad3cad0f0e130653ec377a47289eaf7f22f83edb81e406c7bd7919ea725e024"'
     puts curl_cmd
     system(curl_cmd)
-    @fall = Fall.find(:first,:conditions=>"timestamp='Mon Dec 25 15:52:55 -0600 2007'")
-    @ts = @fall.timestamp.to_s
   end
 
-  it "should be equal " do
-    Checking = "Tue Dec 25 15:52:55 UTC 2007"
-    @ts.should == Checking.to_s
+  it "should have one more fall" do
+    Fall.should have(@no_records + 1).records
   end
 
 end
--- spec/models/panic_spec.rb
+++ spec/models/panic_spec.rb
@@ -1,23 +1,18 @@
-
- 
-
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Panic do
   
-  before(:each) do
+  before(:all) do
+    @no_records = Panic.count 
     curl_cmd = 'curl -v -H "Content-Type: text/xml" -d "<panic><timestamp>Mon Dec 25 15:52:55 -0600 2007</timestamp><user_id>1</user_id></panic>" "http://localhost:3000/panics?gateway_id=1&auth=9ad3cad0f0e130653ec377a47289eaf7f22f83edb81e406c7bd7919ea725e024"'
     puts curl_cmd
     system(curl_cmd)
-    @panic = Panic.find(:first,:conditions=>"timestamp='Mon Dec 25 15:52:55 -0600 2007'")
-    @tspanic = @panic.timestamp.to_s
   end
 
   
   
-  it "should be equal" do
-    Assumingdata = "Tue Dec 25 15:52:55 UTC 2007"
-    @tspanic.should == Assumingdata.to_s
+  it "should be have one more panic" do
+    Panic.should have(@no_records + 1).records
   end
 
 end
