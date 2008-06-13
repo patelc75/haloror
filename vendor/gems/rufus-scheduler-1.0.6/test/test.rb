06e04617d5a0da3d9608c79fe8761a2e6bb8fe79
Matt Smith <matt@mattup.com>
2008-06-13 02:53:48 +0000

    rufus scheduler upgrade
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@765 04a73145-ec33-0410-b10b-c8dba00f0e28

--- vendor/gems/rufus-scheduler-1.0.6/test/test.rb
+++ vendor/gems/rufus-scheduler-1.0.6/test/test.rb
@@ -1,5 +1,8 @@
 
-require 'time_test'
+require 'time_0_test'
+require 'time_1_test'
+
+require 'scheduler_name_test'
 
 require 'scheduler_0_test'
 require 'scheduler_1_test'
@@ -7,6 +10,7 @@ require 'scheduler_2_test'
 require 'scheduler_3_test'
 require 'scheduler_4_test'
 require 'scheduler_5_test'
+require 'scheduler_6_test'
 
 require 'cron_test'
 require 'cronline_test'
