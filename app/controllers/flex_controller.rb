25176cfe77073b5adaeb18fc8087582f98450cd6
Matt Smith <matt@mattup.com>
2008-06-18 02:57:12 +0000

    array => hash
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@781 04a73145-ec33-0410-b10b-c8dba00f0e28

--- app/controllers/flex_controller.rb
+++ app/controllers/flex_controller.rb
@@ -191,8 +191,12 @@ class FlexController < ApplicationController
   
   def get_status(kind, user)
     event = nil
-    event_id_array = Event.connection.select_all("select max(events.id) from events inner join alert_types on events.event_type = alert_types.alert_type where  events.user_id = #{user.id} AND alert_types.id in (select alert_type_id from alert_groups_alert_types where alert_group_id = (select alert_groups.id from alert_groups where group_type = '#{kind}'))")
-    return Event.find(event_id_array[0]).alert_type.alert_type if event_id_array.size > 0
+    event_id_array = Event.connection.select_all("select max(events.id) as id from events inner join alert_types on events.event_type = alert_types.alert_type where  events.user_id = #{user.id} AND alert_types.id in (select alert_type_id from alert_groups_alert_types where alert_group_id = (select alert_groups.id from alert_groups where group_type = '#{kind}'))")
+    if event_id_array.size > 0
+      id_hash = event_id_array[0] 
+      event_id = id_hash[:id]
+      return Event.find(event_id).alert_type.alert_type
+    end
         # 
         # user.events.each do |event|
         #   if event.alert_type and event.alert_type.alert_group[:group_type] == kind
