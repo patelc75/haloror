04f288a08e17f56010e3a7c4a05b20fb7109063e
Chirag Patel <chirag@chirag.name>
2008-06-19 18:05:03 +0000

    comments about data types refs #205
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@799 04a73145-ec33-0410-b10b-c8dba00f0e28

--- lib/postgre_extensions.rb
+++ lib/postgre_extensions.rb
@@ -8,8 +8,8 @@ class ActiveRecord::ConnectionAdapters::PostgreSQLAdapter
       :string      => { :name => "character varying", :limit => 255 },
       :text        => { :name => "text" },
       :integer     => { :name => "integer" },
-      :float       => { :name => "float" },
-      :decimal     => { :name => "decimal" },
+      :float       => { :name => "float" },    #uses 'numeric' type in Pg (variable length)
+      :decimal     => { :name => "decimal" },  #uses 'double precision type in Pg (8 bytes)
       :datetime    => { :name => "timestamp" },
       :timestamp   => { :name => "timestamp" },
       :timestamp_with_time_zone => { :name => "timestamp with time zone" },
@@ -17,7 +17,7 @@ class ActiveRecord::ConnectionAdapters::PostgreSQLAdapter
       :date        => { :name => "date" },
       :binary      => { :name => "bytea" },
       :boolean     => { :name => "boolean" },
-      :real        => { :name => "real" }
+      :real        => { :name => "real" }     #uses 'real'type in Pg (4 bytes)
     }
   end
 end
