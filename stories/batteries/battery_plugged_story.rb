9a74038b4d22f68b6a64eb4f3d75bbef5dd8ea3e
Matt Smith <matt@mattup.com>
2008-06-24 18:21:43 +0000

    rspec story for battery plugged
    to run use 
    	ruby stories/batteries/battery_plugged_story.rb
    
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@826 04a73145-ec33-0410-b10b-c8dba00f0e28

--- stories/batteries/battery_plugged_story.rb
+++ stories/batteries/battery_plugged_story.rb
@@ -2,37 +2,57 @@ require File.dirname(__FILE__) + '/../helper'
 require 'digest'
 
 steps_for(:battery_plugged) do 
-  	Given("a '$device'") do |device|
-  	  puts ''
-  	  puts  "device id?:  "
-	    @device_id = gets
+  	Given("a '$device_id', '$user_id', '$percentage', '$time_remaining', and a '$gateway_id'") do |device_id, 
+  	                                                                                                user_id, 
+  	                                                                                                percentage, 
+  	                                                                                                time_remaining, 
+  	                                                                                                gateway_id|
+  	  if device_id == 'device id'
+  	    puts ''
+  	    puts  "device id?:  "
+	      @device_id = gets
+      else
+        @device_id = device_id
+      end
 	    @device_id.strip!
+	    if user_id == 'user id'
+  	    puts ''
+  	    puts  "user id?:  "
+	      @user_id = gets
+	    else
+	      @user_id = user_id
+	    end
+      @user_id.strip!
+      if percentage == 'battery percentage'
+  	    puts ''
+  	    puts  "battery percentage?:  "
+	      @percentage = gets
+	      @percentage.strip!
+      else
+        @percentage = percentage
+      end  
+	    @percentage.strip!
+	    if time_remaining == "time remaining"
+  	    puts ''
+  	    puts  "time remaining?:  "
+	      @time_remaining = gets
+	    else
+	      @time_remaining = time_remaining
+	    end
+	    @time_remaining.strip!
+	    if gateway_id == 'gateway id'
+  	    puts ''
+  	    puts  "gateway id?:  "
+	      @gateway_id = gets
+      else
+        @gateway_id = gateway_id
+      end
+	    @gateway_id.strip!
 	  end
-    
+	  
 	  When("I issue a '$command'") do |command|
 	    if(command == "battery plugged command")
-	      puts ''
-
-	      puts  "user id?:  "
-	      @user_id = gets
-	      @user_id.strip!
-
-	      puts "battery percentage?:  "
-	      @percentage = gets 
-	      @percentage.strip!
-
-	      puts  "time remaining?:  "
-	      @time_remaining = gets
-	      @time_remaining.strip!
-
-	      puts  "time stamp?:  "
-	      @timestamp = gets
-	      @timestamp.strip!
-
-	      puts "gateway id?:  "
-	      @gateway_id = gets
-        @gateway_id.strip!
-
+	      @timestamp = Time.now.strftime("%a %b %d %H:%M:%S -0600 %Y")
 	      @serial_number = Device.find_by_id(@gateway_id).serial_number
 	      @serial_number.strip!
 	      s = "#{@timestamp}#{@serial_number}"
@@ -44,8 +64,12 @@ steps_for(:battery_plugged) do
       end
     end
     
-    Then("the user should verify 'battery plugged icon") do |battery|
-      
+    Then("the user should verify '$battery'") do |battery|
+      puts ''
+      puts "Does the #{battery} display?"
+      result = gets
+      result.strip!
+      result.upcase.should == 'YES'
     end
 end
 
