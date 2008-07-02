832ce411b1c62a3d518f5b7811da70ffa7421c42
Chirag Patel <chirag@chirag.name>
2008-07-02 06:41:38 +0000

    refs #249 - Page for home installs (activate users and delete panics)
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@878 04a73145-ec33-0410-b10b-c8dba00f0e28

--- app/controllers/util_controller.rb
+++ app/controllers/util_controller.rb
@@ -23,6 +23,16 @@ class UtilController < ApplicationController
   end
   
   def home_install
-    @users = User.find(:all, :conditions => 'activation_code is not null')
+    #events_per_page = 25
+    #conditions = "event_type = 'Fall' or event_type = 'Panic'"
+    #@users = User.paginate :page => params[:page], :order => "created_at DESC", :conditions => conditions, :per_page => events_per_page
+
+    @users = User.find(:all, :order => "created_at DESC")
+  end
+  
+  def delete_panics
+    Event.delete_all("event_type = 'Panic' and user_id = #{params[:id]}")
+    Panic.delete_all("user_id = #{params[:id]}")
+    redirect_to :controller => :events, :action => :user, :id => params[:id]
   end
 end
