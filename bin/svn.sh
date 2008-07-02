9c66e344f96a1f3084f09a1a6348b074d0101584
Chirag Patel <chirag@chirag.name>
2008-07-02 15:05:56 +0000

    clean up svn.sh script
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@883 04a73145-ec33-0410-b10b-c8dba00f0e28

--- bin/svn.sh
+++ bin/svn.sh
@@ -10,8 +10,6 @@ if ( test "$REVISION" == "" ) then
   REVISION='HEAD'
 fi
 
-R_FLAG='-r '
-
 echo "$USER checking out revision $REVISION from http://svn2.assembla.com/svn/halotrac/trunk as svn user $USERNAME"
-svn co $R_FLAG $REVISION http://svn2.assembla.com/svn/halotrac/trunk/ --username $USERNAME haloror
+svn co -r $REVISION http://svn2.assembla.com/svn/halotrac/trunk/ --username $USERNAME haloror
 
