ad77be0ce9ea4f6e4d4769cfe771d3869923345c
Matt Smith <matt@mattup.com>
2008-07-03 20:22:40 +0000

    refs #265, add logging for rufus jobs (email, strap off)
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@893 04a73145-ec33-0410-b10b-c8dba00f0e28

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,6 +1,7 @@
 class Email < ActiveRecord::Base
   def self.notify_by_priority
     begin
+      ActiveRecord::Base.logger.debug("Email.notify_by_priority running at #{Time.now}")
       ActionMailer::ARSendmail.run
     rescue
       RAILS_DEFAULT_LOGGER.warn("RESCUE called in Email.notify_by_priority")
