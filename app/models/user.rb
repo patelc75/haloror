3ff2257d0b4b605f398daf05b251b10fc1e4ef76
Matt Smith <matt@mattup.com>
2008-07-07 15:53:15 +0000

    refs #288, refs #277, fixes #198
    turn off uniqueness of email validation
    
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@909 04a73145-ec33-0410-b10b-c8dba00f0e28

--- app/models/user.rb
+++ app/models/user.rb
@@ -46,7 +46,7 @@ class User < ActiveRecord::Base
   validates_length_of       :email,    :within => 3..100
   #validates_length_of       :serial_number, :is => 10
   
-  validates_uniqueness_of   :login, :email, :case_sensitive => false
+  validates_uniqueness_of   :login, :case_sensitive => false
   
   before_save :encrypt_password
   before_create :make_activation_code
