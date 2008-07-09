d45341a0bab1e431ff854c40da74b5a1f0fd61c3
Matt Smith <matt@mattup.com>
2008-07-09 17:21:36 +0000

    refs #270, added delete and edit
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@921 04a73145-ec33-0410-b10b-c8dba00f0e28

--- app/controllers/call_center_controller.rb
+++ app/controllers/call_center_controller.rb
@@ -39,24 +39,49 @@ class CallCenterController < ApplicationController
     @operators = User.operators
     number_ext
   end
-  
-  def new_note
-    @event_id = params[:event_id]
-    @user_id = params[:user_id]
-    @note = Note.new
-    render :partial => 'notes', :layout => false
+
+  def toggle_note
+    if params[:new_note]
+      @event_id = params[:event_id]
+      @user_id = params[:user_id]
+      @note = Note.new
+      render :partial => 'notes', :layout => false
+    else
+      render :text => '', :layout => false
+    end
   end
   
   def save_note
-    note = Note.new()
-    note.notes = params[:notes]
-    note.event_id = params[:event_id]
-    note.user_id = params[:user_id]
-    note.created_at = Time.now
-    note.save!
-    render :text => '', :layout => false
+    note = nil
+    if params[:id].blank?
+      note = Note.new()
+      note.event_id = params[:event_id]
+      note.user_id = params[:user_id]
+      note.created_at = Time.now
+      note.notes = params[:notes]
+      note.save!
+      render :text => '', :layout => false
+    else 
+      note = Note.find(params[:id])
+      user_id = note.user_id
+      note.notes = params[:notes]
+      note.save!
+      @note = note
+      render :partial => 'update_note', :layout => false
+    end
+    
   end
   
+  def edit_note
+    @note = Note.find(params[:id])
+    render :partial => 'note', :layout => false
+  end
+  def delete_note
+    note = Note.find(params[:id])
+    user_id = note.user_id
+    Note.delete(note)
+    redirect_to :action => 'all_user_notes', :id => user_id
+  end
   def all_user_notes
     user_id = params[:id]
     @notes = Note.find(:all, :conditions => "user_id = #{user_id}")
