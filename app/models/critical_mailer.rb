746ae239fab6989c9129a94e231a14416bb0625c
Chirag Patel <chirag@chirag.name>
2008-07-09 22:51:06 +0000

    refs #295 Notification to caregiver when critical events are resolved (not accepted)
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@930 04a73145-ec33-0410-b10b-c8dba00f0e28

--- app/models/critical_mailer.rb
+++ app/models/critical_mailer.rb
@@ -1,71 +1,4 @@
 class CriticalMailer < ActionMailer::ARMailer
-  include UtilityHelper
-  #  def device_unavailable_alert_notification(alert, user)
-  ##    setup_email(user, alert)
-  ##    @subject    += "Device Unavailable for User #{user.id}"
-  ##    self.priority = alert.priority
-  #    body :alert_created_at => alert.created_at,
-  #      :login     => user.login,
-  #      :user_id   => user.id,
-  #      :alert_id => alert.id,
-  #      :device_id => alert.device_id
-  #  end
-  #
-  #  # alert: DeviceAvailableAlert
-  #  def device_available_alert_notification(alert, user)
-  #    setup_email(user, alert)
-  #    @subject    += "Device Available for User #{user.id}"
-  #    self.priority = alert.priority
-  #    body :alert_created_at => alert.created_at,
-  #      :login     => user.login,
-  #      :user_id   => user.id,
-  #      :alert_id => alert.id,
-  #      :device_id => alert.device_id
-  #  end
-  #  
-  #  def gateway_online_notification(alert, user)
-  #    device = alert.device
-  #    setup_email(user, alert)
-  #    @subject    += "Gateway #{alert.device_id} Is Back Online"
-  #    self.priority = alert.priority
-  #    body :alert_created_at => alert.created_at,
-  #      :login     => user.login,
-  #      :user_id   => user.id,
-  #      :device_id => device.id
-  #  end
-  #
-  #  def gateway_offline_notification(outage, user)
-  #    device = outage.device
-  #    setup_email(user, outage)
-  #    @subject    += "Gateway Offline for Device #{outage.device_id}"
-  #    self.priority = outage.priority    
-  #    body :outage_created_at => outage.created_at,
-  #      :login     => user.login,
-  #      :user_id   => user.id,
-  #      :device_id => device.id
-  #  end
-  #
-  #  def strap_on_notification(alert, user)
-  #    device = alert.device
-  #    setup_email(user, alert)
-  #    @subject    += "Strap On for Device #{alert.device_id}"
-  #    self.priority = alert.priority    
-  #    body :alert_created_at => alert.created_at,
-  #      :login     => user.login,
-  #      :user_id   => user.id,
-  #      :device_id => device.id
-  #  end
-  #
-  #  def strap_off_notification(alert, user)
-  #    device = alert.device
-  #    setup_email(user, alert)
-  #    @subject    += "Strap Off for Device #{alert.device_id}"
-  #    self.priority = alert.priority    
-  #    body :alert_created_at => alert.created_at,
-  #      :login     => user.login,
-  #      :user_id   => user.id,
-  #      :device_id => device.id
-  #  end
   def background_task_notification(alert, user)
     body = "Alert ID: #{alert.id}\n" +
       "User #{user.name} (#{user.id})\n" +
@@ -88,7 +21,7 @@ class CriticalMailer < ActionMailer::ARMailer
   def call_center_notification(event_action)
     setup_message(event_action.to_s, event_action.email_body)
     setup_operators(event_action.event.event)
-    setup_caregivers(event_action.event.user, alert) if(event_action.description == "resolved")
+    setup_caregivers(event_action.event.user, event_action.event.event) if(event_action.description == "resolved")
     self.priority  = event_action.priority
   end
   
@@ -99,12 +32,7 @@ class CriticalMailer < ActionMailer::ARMailer
   end
   
   protected
-  #  def setup_email(user, alert)
-  #    setup_message()
-  #    setup_caregivers(user, alert)
-  #    @body[:user] = user  #sends params to body
-  #  end
-  
+
   def setup_message(subject, msg_body)
     @from        = "no-reply@halomonitoring.com"
     @subject     = "[HALO] "
