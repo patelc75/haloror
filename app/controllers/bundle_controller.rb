697ae75bbc74eafca312ac2e004551839b0f3f0b
Matt Smith <matt@mattup.com>
2008-07-14 19:01:36 +0000

    added rescue handler for internal server error, returns 500 status code during if error occurs, rollsback transaction
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@952 04a73145-ec33-0410-b10b-c8dba00f0e28

--- app/controllers/bundle_controller.rb
+++ app/controllers/bundle_controller.rb
@@ -1,26 +1,32 @@
 
 class BundleController < RestfulAuthController
   
-  @@bundled_models = [Vital, StrapRemoved, StrapFastened, Step, SkinTemp, Fall, Panic, Battery, BatteryChargeComplete, BatteryCritical, BatteryPlugged, BatteryUnplugged]
+  @@bundled_models = [Vital, StrapRemoved, StrapFastened, Step, SkinTemp, Battery, BatteryChargeComplete, BatteryCritical, BatteryPlugged, BatteryUnplugged, Fall, Panic]
   def create
     bundle = params[:bundle]    
-    @@bundled_models[0].transaction do
-      @@bundled_models.each do |model|
-        value = bundle[model.node_name]
-        if !value.blank?
-          if value.class == Array
-            value.each do |v|
-              model.new(v).save!
+    begin
+      @@bundled_models[0].transaction do
+        @@bundled_models.each do |model|
+          value = bundle[model.node_name]
+          if !value.blank?
+            if value.class == Array
+              value.each do |v|
+                model.new(v).save!
+              end
+            else
+              model.new(value).save!
             end
-          else
-            model.new(value).save!
           end
         end
       end
-    end
-    
-    respond_to do |format|
-      format.xml { head :ok } 
+      respond_to do |format|
+        format.xml { head :ok } 
+      end
+    rescue
+      RAILS_DEFAULT_LOGGER('ERROR in BundleController')
+      respond_to do |format|
+        format.xml { head :internal_server_error }
+      end
     end
   end
 end
