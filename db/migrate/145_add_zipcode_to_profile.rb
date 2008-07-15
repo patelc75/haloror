58009656f362cf2186d5bfc02b4010aa37a3f31e
Matt Smith <matt@mattup.com>
2008-07-15 17:08:40 +0000

    rename
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@958 04a73145-ec33-0410-b10b-c8dba00f0e28

--- db/migrate/145_add_zipcode_to_profile.rb
+++
@@ -1,9 +0,0 @@
-class AddZipcodeToProfile < ActiveRecord::Migration
-  def self.up
-    add_column :profiles, :zipcode, :string
-  end
-
-  def self.down
-    remove_column :profiles, :zipcode
-  end
-end
