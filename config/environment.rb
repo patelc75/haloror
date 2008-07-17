dc03aa0a21b38fea80436b5874fd163ce2312f67
Matt Smith <matt@mattup.com>
2008-07-17 18:41:02 +0000

    refs #315, moved code to lib/halo_mailer.rb added exception handling
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@968 04a73145-ec33-0410-b10b-c8dba00f0e28

--- config/environment.rb
+++ config/environment.rb
@@ -91,42 +91,7 @@ end
 require 'rubygems'
 require 'will_paginate'
 require 'action_mailer/ar_sendmail'
-
-class ActionMailer::ARMailer < ActionMailer::Base
-  attr_accessor :priority
-  def perform_delivery_activerecord(mail)
-    if self.priority.nil?
-      self.priority = Priority::LOW
-    end
-    if !mail.destinations.blank?
-      if self.priority > Priority::THRESH_HOLD
-        emails = []
-        ar_sendmail = ActionMailer::ARSendmail.new
-        mail.destinations.each do |destination|
-          emails << Email.new(:mail => mail.encoded, :to => destination,
-            :from => mail.from.first, :priority => self.priority)
-          end
-          ar_sendmail.deliver(emails)      
-        else
-          mail.destinations.each do |destination|
-            Email.create  :mail => mail.encoded, :to => destination,
-            :from => mail.from.first, :priority => self.priority
-          end
-        end
-      end
-    end
-  end
-
-class ActionMailer::ARSendmail
-  def find_emails
-    options = { :conditions => ['last_send_attempt < ?', Time.now.to_i - 300], :order => :priority }
-    options[:limit] = batch_size unless batch_size.nil?
-    mail = @email_class.find :all, options
-
-    log "found #{mail.length} emails to send"
-    mail
-  end
-end
+require 'halo_mailer'
 
 require 'postgre_extensions'
 
---
+++ lib/halo_mailer.rb
@@ -0,0 +1,42 @@
+class ActionMailer::ARMailer < ActionMailer::Base
+  attr_accessor :priority
+  def perform_delivery_activerecord(mail)
+    if self.priority.nil?
+      self.priority = Priority::LOW
+    end
+    if !mail.destinations.blank?
+      if self.priority > Priority::THRESH_HOLD
+        emails = []
+        ar_sendmail = ActionMailer::ARSendmail.new
+        mail.destinations.each do |destination|
+          emails << Email.new(:mail => mail.encoded,    :to => destination,
+                              :from => mail.from.first, :priority => self.priority)
+          end
+          ar_sendmail.deliver(emails)      
+        else
+          mail.destinations.each do |destination|
+            begin
+              Email.create  :mail => mail.encoded,    :to => destination, 
+                            :from => mail.from.first, :priority => self.priority
+            rescue Error => e
+              RAILS_DEFAULT_LOGGER.warn("Error sending mail:  #{e}")
+              email = Email.new(:mail => 'Error sending mail',          :to => 'exceptions@halomonitoring.com', 
+                                :from => 'no-reply@halomonitoring.com', :priority => 100)
+              ar_sendmail.deliver(email)
+            end
+          end
+        end
+      end
+    end
+  end
+
+class ActionMailer::ARSendmail
+  def find_emails
+    options = { :conditions => ['last_send_attempt < ?', Time.now.to_i - 300], :order => :priority }
+    options[:limit] = batch_size unless batch_size.nil?
+    mail = @email_class.find :all, options
+
+    log "found #{mail.length} emails to send"
+    mail
+  end
+end
