9bd55cba261723b4b2e77ae1cb1422c465020746
Matt Smith <matt@mattup.com>
2008-07-24 21:38:26 +0000

    refs #315 mail to email
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@1040 04a73145-ec33-0410-b10b-c8dba00f0e28

--- lib/utility_helper.rb
+++ lib/utility_helper.rb
@@ -32,10 +32,11 @@ module UtilityHelper
   
   def self.safe_send_email(message)
     begin
-      email = Email.new(:mail => message, 
+      email = Email.new(:mail => "Messge => #{message}", 
                         :to => 'exceptions_www@halomonitoring.com', 
                         :from => 'no-reply@halomonitoring.com', 
-                        :priority => 100)
+                        :priority => 100
+                        :subject => "SAFE SEND EMAIL::")
       ar_sendmail = ActionMailer::ARSendmail.new
       ar_sendmail.deliver([email])
     rescue
