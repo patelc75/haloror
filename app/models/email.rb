dea0fc0d3d5be579062cbfbf8bad466bcc7bd6ea
Matt Smith <matt@mattup.com>
2008-07-24 18:17:41 +0000

    refs #315 run once => true
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@1030 04a73145-ec33-0410-b10b-c8dba00f0e28

--- app/models/email.rb
+++ app/models/email.rb
@@ -2,7 +2,7 @@ class Email < ActiveRecord::Base
   def self.notify_by_priority
     begin
       RAILS_DEFAULT_LOGGER.warn("Email.notify_by_priority running at #{Time.now}")
-      ActionMailer::ARSendmail.run
+      ActionMailer::ARSendmail.run(:once => true)
     rescue
       RAILS_DEFAULT_LOGGER.warn("RESCUE called in Email.notify_by_priority")
     end
