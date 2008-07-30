ff3beeff230601f8467e33267615c23598268038
Matt Smith <matt@mattup.com>
2008-07-30 16:12:44 +0000

    remove :db from to_s
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@1049 04a73145-ec33-0410-b10b-c8dba00f0e28

--- app/models/daily_reports.rb
+++ app/models/daily_reports.rb
@@ -8,14 +8,14 @@ class DailyReports
     users.each do |user|
       user_id = user.id
       self.lost_data_scan(user_id)
-      lost_data[user_id] = LostData.find(:all, :conditions => "user_id = #{user_id} AND end_time <= '#{end_time.to_s(:db)}' AND begin_time >= '#{begin_time.to_s(:db)}'", :order => "id desc")
+      lost_data[user_id] = LostData.find(:all, :conditions => "user_id = #{user_id} AND end_time <= '#{end_time.to_s()}' AND begin_time >= '#{begin_time.to_s()}'", :order => "id desc")
     end
     return lost_data
   end
   def self.lost_data_scan(user_id)
     prev_timestamp = nil
     if last = VitalScan.find(:first, :conditions => "user_id = #{user_id}", :order => "timestamp desc")
-      conds = " and timestamp > '#{last.timestamp.to_s(:db)}'"
+      conds = " and timestamp > '#{last.timestamp.to_s()}'"
     else
       conds = ""
     end
@@ -23,13 +23,13 @@ class DailyReports
     begin_time = nil
     begin_time = last.timestamp if last
     if begin_time
-      LostData.connection.select_all("select * from lost_data_function(#{user_id}, '#{begin_time.to_s(:db)}', '#{end_time.to_s(:db)})', '#{LOST_DATA_GAP} seconds')")
+      LostData.connection.select_all("select * from lost_data_function(#{user_id}, '#{begin_time.to_s()}', '#{end_time.to_s()})', '#{LOST_DATA_GAP} seconds')")
     else
-      LostData.connection.select_all("select * from lost_data_function(#{user_id}, null, '#{end_time.to_s(:db)})', '#{LOST_DATA_GAP} seconds')")
+      LostData.connection.select_all("select * from lost_data_function(#{user_id}, null, '#{end_time.to_s()})', '#{LOST_DATA_GAP} seconds')")
     end
     lost_data = nil
     if begin_time
-      lost_data = LostData.find(:first, :order => "end_time desc", :conditions => "user_id = #{user_id} AND end_time > '#{begin_time.to_s(:db)}'")
+      lost_data = LostData.find(:first, :order => "end_time desc", :conditions => "user_id = #{user_id} AND end_time > '#{begin_time.to_s()}'")
     else
       lost_data = LostData.find(:first, :order => "end_time desc", :conditions => "user_id = #{user_id}")
     end
