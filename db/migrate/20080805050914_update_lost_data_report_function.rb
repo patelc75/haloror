91e5f950a1346dcaa462650e9563ba4a590e917c
Matt Smith <matt@mattup.com>
2008-08-05 05:21:40 +0000

    order by timestamp asc refs #343
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@1073 04a73145-ec33-0410-b10b-c8dba00f0e28

---
+++ db/migrate/20080805050914_update_lost_data_report_function.rb
@@ -0,0 +1,47 @@
+class UpdateLostDataReportFunction < ActiveRecord::Migration
+  def self.up
+      ddl = <<-eos
+      create or replace function lost_data_function(
+        p_user_id       in integer,
+        p_begin_time    in timestamp with time zone,
+        p_end_time      in timestamp with time zone,
+        p_lost_data_gap in varchar
+      ) returns void as $$
+      declare
+        row record;
+        prev_timestamp timestamp with time zone;
+      begin
+         if p_begin_time IS NULL then
+           for row in (select timestamp from vitals where user_id = p_user_id AND timestamp <= p_end_time order by timestamp asc) loop
+            if(prev_timestamp is null) then
+              prev_timestamp = row.timestamp;
+            else
+              if((row.timestamp - prev_timestamp) > ("interval"(p_lost_data_gap)) ) then
+                insert into lost_datas (user_id, begin_time, end_time) values (p_user_id, prev_timestamp, row.timestamp);
+                prev_timestamp = row.timestamp;
+              end if;
+            end if;
+          end loop;
+        else
+          for row in (select timestamp from vitals where user_id = p_user_id AND timestamp <= p_end_time AND timestamp >= p_begin_time order by timestamp asc) loop
+            if(prev_timestamp is null) then
+              prev_timestamp = row.timestamp;
+            else
+              if((row.timestamp - prev_timestamp) > ("interval"(p_lost_data_gap)) ) then
+                insert into lost_datas (user_id, begin_time, end_time) values (p_user_id, prev_timestamp, row.timestamp);
+                prev_timestamp = row.timestamp;
+              end if;
+            end if;
+          end loop;
+        end if;
+        end;
+      $$ language plpgsql;
+
+          eos
+      execute ddl
+    end
+
+
+    def self.down
+    end
+  end
