f7c71d2f2a6dcd09999568aab66ea396ea74a0a0
Chirag Patel <chirag@chirag.name>
2008-08-09 00:39:44 +0000

    Added atl-web1 to subject line list
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@1090 04a73145-ec33-0410-b10b-c8dba00f0e28

--- lib/server_instance.rb
+++ lib/server_instance.rb
@@ -1,5 +1,5 @@
 module ServerInstance
-  @@prefixes = ["sdev", "idev", "ldev", "dev"]
+  @@prefixes = ["sdev", "idev", "ldev", "dev", "atl-web1"]
   
   def self.current_host
     if Thread.current[:host].nil?
