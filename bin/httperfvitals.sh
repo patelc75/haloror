3162c9e031550108ece77ecbd00d27e526f318ae
Alex Kah <alex@narfonix.com>
2008-08-09 21:34:23 +0000

    adding httperf scripts
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@1091 04a73145-ec33-0410-b10b-c8dba00f0e28

---
+++ bin/httperfvitals.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+# this will generate httperf test for the vitals post
+echo "Enter: Server FQDN [example: sdev.halomonitor.com]"
+read server_name
+echo "Enter: Number of sessions [example: 100]"
+read number_of_sessions
+echo "Enter: Number of sessions per second [example: 20]"
+read number_of_sessions_per_second
+echo "httperf --server $server_name --add-header=\"content-type: text/xml\n\" --wsesslog $number_of_sessions,2,httperf/httperf-vitalspost --rate $number_of_sessions_per_second"
+/usr/bin/httperf --server $server_name --add-header="content-type: text/xml\n" --wsesslog $number_of_sessions,2,httperf/httperf-vitalspost --rate $number_of_sessions_per_second
