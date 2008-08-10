616c85d6d62f2387c2beadf17cbe5feb6d125a52
Chirag Patel <chirag@chirag.name>
2008-08-10 01:46:01 +0000

    refs #350 Do not send emails to caregivers from atl-web1 crit2 server
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@1095 04a73145-ec33-0410-b10b-c8dba00f0e28

--- app/models/device_event_observer.rb
+++ app/models/device_event_observer.rb
@@ -1,4 +1,5 @@
 class DeviceEventObserver < ActiveRecord::Observer
+  include ServerInstance
   observe Fall, Panic, BatteryPlugged, BatteryUnplugged, BatteryCritical, BatteryChargeComplete, StrapFastened, StrapRemoved
   
   def before_save(event)
@@ -6,7 +7,9 @@ class DeviceEventObserver < ActiveRecord::Observer
       raise "#{event.class.to_s}: user_id = #{event.user_id} is invalid"
     elsif event.class == Fall or event.class == Panic #temporary until GW starts sending device_id with falls and panics
       CriticalMailer.deliver_device_event_operator(event)
-      CriticalMailer.deliver_device_event_caregiver(event)
+      if(ServerInstance.current_host_short_string() != "ATL-WEB1")
+        CriticalMailer.deliver_device_event_caregiver(event)
+      end
     elsif event.device_id < 1 or event.device == nil
       raise "#{event.class.to_s}: device_id = #{event.device_id} does not exist"
     else
