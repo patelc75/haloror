a28f48ab5656be6d6f326417e234236440a95e40
Matt Smith <matt@mattup.com>
2008-08-17 22:58:38 +0000

    New Event page, needs images added for type refs #346
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@1107 04a73145-ec33-0410-b10b-c8dba00f0e28

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,90 +1,14 @@
 class EventsController < ApplicationController
   include UtilityHelper
-  # GET /events
-  # GET /events.xml
-  # def index
-  #   @events = Event.find(:all)
-  # 
-  #   respond_to do |format|
-  #     format.html # index.rhtml
-  #     format.xml  { render :xml => @events.to_xml }
-  #   end
-  # end
-  # 
-  # # GET /events/1
-  # # GET /events/1.xml
-  # def show
-  #   @event = Event.find(params[:id])
-  # 
-  #   respond_to do |format|
-  #     format.html # show.rhtml
-  #     format.xml  { render :xml => @event.to_xml }
-  #   end
-  # end
-  # 
-  # # GET /events/new
-  # def new
-  #   @event = Event.new
-  # end
-  # 
-  # # GET /events/1;edit
-  # def edit
-  #   @event = Event.find(params[:id])
-  # end
-  # 
-  # # POST /events
-  # # POST /events.xml
-  # def create
-  #   @event = Event.new(params[:event])
-  # 
-  #   respond_to do |format|
-  #     if @event.save
-  #       flash[:notice] = 'Event was successfully created.'
-  #       format.html { redirect_to event_url(@event) }
-  #       format.xml  { head :created, :location => event_url(@event) }
-  #     else
-  #       format.html { render :action => "new" }
-  #       format.xml  { render :xml => @event.errors.to_xml }
-  #     end
-  #   end
-  # end
-  # 
-  # # PUT /events/1
-  # # PUT /events/1.xml
-  # def update
-  #   @event = Event.find(params[:id])
-  # 
-  #   respond_to do |format|
-  #     if @event.update_attributes(params[:event])
-  #       flash[:notice] = 'Event was successfully updated.'
-  #       format.html { redirect_to event_url(@event) }
-  #       format.xml  { head :ok }
-  #     else
-  #       format.html { render :action => "edit" }
-  #       format.xml  { render :xml => @event.errors.to_xml }
-  #     end
-  #   end
-  # end
-  # 
-  # # DELETE /events/1
-  # # DELETE /events/1.xml
-  # def destroy
-  #   @event = Event.find(params[:id])
-  #   @event.destroy
-  # 
-  #   respond_to do |format|
-  #     format.html { redirect_to events_url }
-  #     format.xml  { head :ok }
-  #   end
-  # end
-  
   def user
     @user = User.find(params[:id])
     if(@user.id == current_user.id || @current_user.patients.include?(@user) || current_user.is_administrator?) 
-      events_per_page = 25
       conditions = "user_id = #{@user.id}"
       total = Event.count(:conditions => conditions)
-      @events = Event.paginate :page => params[:page], :order => "(timestamp_server IS NOT NULL) DESC, timestamp_server DESC, timestamp DESC", :conditions => conditions, :per_page => events_per_page
+      @events = Event.paginate :page => params[:page], 
+                               :order => "(timestamp_server IS NOT NULL) DESC, timestamp_server DESC, timestamp DESC", 
+                               :conditions => conditions, 
+                               :per_page => EVENTS_PER_PAGE
       render :layout => 'application'
     else
       redirect_to :action => 'unauthorized', :controller => 'security'
