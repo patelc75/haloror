8fb49ec9b20754efdc4ea839f1438622a237c962
Alex Kah <alex@narfonix.com>
2008-08-23 21:07:19 +0000

    refs #368 Possible issue with the verbose output of ar_sendmail. Modified line 4 of email.rb from arsendmail = ActionMailer::ARSendmail.new(:Once => true, :Verbose => true) to arsendmail = ActionMailer::ARSendmail.new(:Once => true)
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@1125 04a73145-ec33-0410-b10b-c8dba00f0e28

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,7 +1,7 @@
 class Email < ActiveRecord::Base
   def self.notify_by_priority
       RAILS_DEFAULT_LOGGER.warn("Email.notify_by_priority running at #{Time.now}")
-      arsendmail = ActionMailer::ARSendmail.new(:Once => true, :Verbose => true)
+      arsendmail = ActionMailer::ARSendmail.new(:Once => true)
       arsendmail.run
   end
 end
