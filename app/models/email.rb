dde38036d2262646c04a35b9699065c26d6e3966
Alex Kah <alex@narfonix.com>
2008-08-23 01:16:41 +0000

    updated email.rb, ticket #368
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@1122 04a73145-ec33-0410-b10b-c8dba00f0e28

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,13 +1,7 @@
 class Email < ActiveRecord::Base
   def self.notify_by_priority
-    begin
       RAILS_DEFAULT_LOGGER.warn("Email.notify_by_priority running at #{Time.now}")
       arsendmail = ActionMailer::ARSendmail.new(:Once => true, :Verbose => true)
       arsendmail.run
-    rescue Exception => e
-      RAILS_DEFAULT_LOGGER.warn("RESCUE called in Email.notify_by_priority #{e}")
-    rescue
-      RAILS_DEFAULT_LOGGER.warn("RESCUE called in Email.notify_by_priority")
-    end
   end
 end
