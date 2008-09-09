169bc2f86d247aaddc787598e843ce4b3b6d4f65
Alex Kah <alex@narfonix.com>
2008-09-09 10:06:54 +0000

    refs #406 added correct SSL support
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@1164 04a73145-ec33-0410-b10b-c8dba00f0e28

--- bin/httperf/httperfvitals-with-ssl.sh
+++ bin/httperf/httperfvitals-with-ssl.sh
@@ -7,5 +7,8 @@ echo "Enter: Number of sessions [example: 100]"
 read number_of_sessions
 echo "Enter: Number of sessions per second [example: 20]"
 read number_of_sessions_per_second
-echo "httperf --ssl --server $server_name --add-header=\"content-type: text/xml\n\" --wsesslog $number_of_sessions,2,httperf-vitalspost --rate $number_of_sessions_per_second"
-/usr/bin/httperf --server $server_name --add-header="content-type: text/xml\n" --wsesslog $number_of_sessions,2,httperf-vitalspost --rate $number_of_sessions_per_second
+echo "httperf --server $server_name --add-header=\"content-type: text/xml\n\" --wsesslog $number_of_sessions,2,httperf-vitalspost --rate $number_of_sessions_per_second --ssl --ssl-no-reuse"
+/usr/bin/httperf --server $server_name --add-header="content-type: text/xml\n" --wsesslog $number_of_sessions,2,httperf-vitalspost --rate $number_of_sessions_per_second --ssl --ssl-no-reuse
+
+
+
