cbc023fb38d5061e83bf16ae88be2c01df3128ea
Matt Smith <matt@mattup.com>
2008-09-15 20:19:26 +0000

    refs #399 oscope_msgs use timestamp for authentication use individual oscoep_msg timestamp for lookup
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@1176 04a73145-ec33-0410-b10b-c8dba00f0e28

--- app/controllers/oscope_msgs_controller.rb
+++ app/controllers/oscope_msgs_controller.rb
@@ -6,32 +6,43 @@ class OscopeMsgsController < RestfulAuthController
       if msgs.class != Array
         msgs = [msgs]
       end
-      timestamp = o_msgs[:timestamp].to_time if o_msgs[:timestamp]
-#    msg = params[:oscope_msg]
-      o_start_msg = OscopeStartMsg.find_by_timestamp("'#{timestamp.to_s(:db)}'")
-      msgs.each do |msg|
-        timestamp = msg[:timestamp].to_time if msg[:timestamp]
-        channel_num = msg[:channel_num].to_i if msg[:channel_num]
-        user_id = msg[:user_id].to_i if msg[:user_id]
-        o_msg = OscopeMsg.new
-        o_msg.timestamp = timestamp
-        o_msg.channel_num = channel_num
-        o_msg.user_id = user_id
-        o_msg.oscope_start_msg = o_start_msg
-        o_msg.save!
-        points = msg[:point]
-        if points.class == Array
-          points.each do |point|
-            save_point(o_msg, point)
-          end
-        else
-          save_point(o_msg, points)
+      o_start_msg = nil
+      timestamp = nil
+      user_id = nil
+      if msgs.size > 0
+        timestamp = msgs[0][:timestamp].to_time if msgs[0][:timestamp]
+        user_id = msgs[0][:user_id] if msgs[0][:user_id]
+        if user_id && timestamp
+          o_start_msg = OscopeStartMsg.find_by_timestamp_and_user_id("'#{timestamp.to_s(:db)}'", user_id)
+        end
       end
-    end
-    respond_to do |format|
+      if o_start_msg
+        msgs.each do |msg|
+          timestamp = msg[:timestamp].to_time if msg[:timestamp]
+          channel_num = msg[:channel_num].to_i if msg[:channel_num]
+          user_id = msg[:user_id].to_i if msg[:user_id]
+          o_msg = OscopeMsg.new
+          o_msg.timestamp = timestamp
+          o_msg.channel_num = channel_num
+          o_msg.user_id = user_id
+          o_msg.oscope_start_msg = o_start_msg
+          o_msg.save!
+          points = msg[:point]
+          if points.class == Array
+            points.each do |point|
+              save_point(o_msg, point)
+            end
+          else
+            save_point(o_msg, points)
+          end
+        end
+      else
+        raise "No oscope start msg found."
+      end
+      respond_to do |format|
         format.xml { head :ok } 
-    end
-    rescue RuntimeError => e
+      end
+    rescue Exception => e
       RAILS_DEFAULT_LOGGER.warn("ERROR in OscopeMsgController:  #{e}")
       respond_to do |format|
         format.xml { head :internal_server_error }
