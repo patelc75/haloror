b85d2c7f1bc9f6f5f9b61ee80b2fbb1b0d79f928
Matt Smith <matt@mattup.com>
2008-10-03 15:53:09 +0000

    refs #428, open up check and hostname util methods
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@1213 04a73145-ec33-0410-b10b-c8dba00f0e28

--- app/controllers/util_controller.rb
+++ app/controllers/util_controller.rb
@@ -1,5 +1,5 @@
 class UtilController < ApplicationController
-  before_filter :authenticate_super_admin?, :except => 'check'
+  before_filter :authenticate_super_admin?, :except => ['check', 'hostname']
   #session :off
   
   # Monitors that the application and database connection is alive
