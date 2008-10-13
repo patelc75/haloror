902b23d0d1a2a83bf60016d368f61646c5bbf6ac
Matt Smith <matt@mattup.com>
2008-10-13 22:35:52 +0000

    refs #432 self_test_result refactoring
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@1233 04a73145-ec33-0410-b10b-c8dba00f0e28

--- app/controllers/self_test_results_controller.rb
+++ app/controllers/self_test_results_controller.rb
@@ -2,8 +2,13 @@ class SelfTestResultsController < RestfulAuthController
   
   def create
     begin
-      self_test_result = SelfTestResult.new(params[:self_test_result])
-      self_test_item_results = params[:self_test_item_result]
+      request = params[:self_test_result]
+      self_test_result = SelfTestResult.new
+      self_test_result.result = request[:result]
+      self_test_result.cmd_type = request[:cmd_type]
+      self_test_result.device_id = request[:device_id]
+      self_test_result.timestamp = request[:timestamp]
+      self_test_item_results = request[:self_test_item_result]
       if !self_test_item_results.blank?
         if self_test_item_results.class != Array
           self_test_item_results = [self_test_item_results]
