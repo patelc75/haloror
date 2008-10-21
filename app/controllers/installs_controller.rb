9ddac3dd98ac79e75c1282343688b7d13989e3ff
Matt Smith <matt@mattup.com>
2008-10-21 20:44:49 +0000

    refs #432
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@1295 04a73145-ec33-0410-b10b-c8dba00f0e28

--- app/controllers/installs_controller.rb
+++ app/controllers/installs_controller.rb
@@ -314,7 +314,8 @@ class InstallsController < ApplicationController
   def stop_range_test
     init_devices_user
     create_mgmt_cmd('range_test_stop', @strap.id)
-      previous_step = @self_test_session.self_test_steps.find(:first, :conditions => "self_test_step_description_id = #{HEARTRATE_DETECTED_ID}")
+    create_self_test_step(RANGE_TEST_COMPLETE_ID)
+      previous_step = @self_test_session.self_test_steps.find(:first, :conditions => "self_test_step_description_id = #{SELF_TEST_PHONE_COMPLETE_ID}")
       message = self_test_step.self_test_step_description.description + "  --  #{self_test_step.timestamp - previous_step.timestamp}"                        
     create_mgmt_cmd('mgmt_poll_rate', @gateway.id, MGMT_POLL_RATE)
     create_self_test_step(SLOW_POLLING_MGMT_COMMAND_CREATED_ID)
