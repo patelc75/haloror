f35784574096f6409e0de73f46b2e334c8bf46ef
Matt Smith <matt@mattup.com>
2008-10-30 19:47:54 +0000

    if halouser? goto chart
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@1330 04a73145-ec33-0410-b10b-c8dba00f0e28

--- app/controllers/redirector_controller.rb
+++ app/controllers/redirector_controller.rb
@@ -1,7 +1,9 @@
 class RedirectorController < ApplicationController
 
   def index
-    if(current_user.is_moderator? || current_user.is_admin? || current_user.is_super_admin?)
+    if(current_user.is_halouser?)
+      redirect_to :controller => 'chart', :action => 'flex'
+    elsif(current_user.is_moderator? || current_user.is_admin? || current_user.is_super_admin?)
       redirect_to :controller => 'reporting', :action => 'users'
     elsif(current_user.is_operator?)
       redirect_to :controller => 'call_center', :action => 'index'
