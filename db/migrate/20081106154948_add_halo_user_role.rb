6436ea1a4400c9a60cfb878cfdaa1c2541763e38
Matt Smith <matt@mattup.com>
2008-11-06 15:57:15 +0000

    add_halouser role
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@1333 04a73145-ec33-0410-b10b-c8dba00f0e28

---
+++ db/migrate/20081106154948_add_halo_user_role.rb
@@ -0,0 +1,19 @@
+class AddHaloUserRole < ActiveRecord::Migration
+  def self.up
+    halouser_role = Role.find_by_name('halouser')
+    unless halouser_role
+      now = Time.now
+      groups = Groups.find(:all)
+      groups.each do |group|
+        Role.create(:name => 'halouser', 
+                    :authorizable_type => 'Group', 
+                    :authorizable_id => group.id,
+                    :created_at => now,
+                    :update_at => now)
+      end
+    end
+  end
+
+  def self.down
+  end
+end
