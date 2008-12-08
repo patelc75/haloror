775d593708635dd8cb216a14b61dcca8c6569946
Matt Smith <matt@mattup.com>
2008-12-08 16:03:17 +0000

    refs #493
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@1433 04a73145-ec33-0410-b10b-c8dba00f0e28

--- app/controllers/installs_controller.rb
+++ app/controllers/installs_controller.rb
@@ -304,7 +304,7 @@ class InstallsController < ApplicationController
         session[:progress_count][:phone] = nil
         step = create_self_test_step(SELF_TEST_PHONE_TIMEOUT_ID)
         @self_test_step_id = step.id
-        message = "Self Test Phone timeout.  Please use phone to check for dial tone and plug phone line in to gateway."
+        message = "Self Test Phone timeout.  Please use phone to check for dial tone and plug phone line in to gateway.  Either restart the wizard or you can agree to disable the dial backup feature and continue by clicking <a href=\"javascript:continueWithoutPhone(" + @self_test_step_id.to_s + ");\" >here</a>."
         clear_session_data
         render_update_timeout('phone_div_id', message, 'updateCheckSelfTestPhone', 'install_wizard_launch')
       elsif session[:halo_check_phone_self_test_result] && !session[:halo_check_phone_self_test_result].result
