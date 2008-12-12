3d21b6097d89b9f1bc25a0c0be1c462268d1082f
Matt Smith <matt@mattup.com>
2008-12-12 20:03:54 +0000

    add time to timestamps
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@1454 04a73145-ec33-0410-b10b-c8dba00f0e28

--- app/models/data_transformer.rb
+++ app/models/data_transformer.rb
@@ -4,15 +4,48 @@ class DataTransformer
     prev_timestamp = nil
     count = beginning_id
     while(count < (ending_id + 1))
-      vital = Vital.find(:first, :conditions => "user_id = #{82} AND id = #{count}")
+      vital = Vital.find(:first, :conditions => "user_id = #{user_id} AND id = #{count}")
       if vital && vital.timestamp != prev_timestamp
         new_vital = Vital.create(:heartrate => vital.heartrate, :hrv => vital.hrv,
                                 :activity => vital.activity, :orientation => vital.orientation,
                                 :user_id => vital.user_id, :timestamp => timestamp)
         new_vital.save!
-        prev_timestamp = timestamp
+        prev_timestamp = vital.timestamp
         timestamp = timestamp + 15.seconds
       end      
+      count += 1
+    end
+  end
+  
+  def self.find_vitals_timestamps(user_id, beginning_id, ending_id, begin_timestamp)
+    timestamp = begin_timestamp.to_time
+    prev_timestamp = nil
+    count = beginning_id
+    while(count < (ending_id + 1))
+      vital = Vital.find(:first, :conditions => "user_id = #{user_id} AND id = #{count}")
+      if prev_timestamp == nil
+        prev_timestamp = vital.timestamp
+      end
+      if vital && (prev_timestamp - vital.timestamp) < 15.seconds
+        puts vital.inspect
+        prev_timestamp = vital.timestamp
+      end      
+      count += 1
+    end
+  end
+  
+  def self.transform_add_timestamps(user_id, beginning_id, ending_id, seconds)
+    prev_timestamp = nil
+    count = beginning_id
+    while(count < (ending_id + 1))
+      vital = Vital.find(:first, :conditions => "user_id = #{user_id} AND id = #{count}")
+      if vital
+        timestamp = vital.timestamp + seconds.seconds
+        new_vital = Vital.create(:heartrate => vital.heartrate, :hrv => vital.hrv,
+                                :activity => vital.activity, :orientation => vital.orientation,
+                                :user_id => vital.user_id, :timestamp => timestamp)
+        new_vital.save!
+      end
       count += 1
     end
   end
