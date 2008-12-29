9ab6e1cf5ed862429cbdd748fab4ec890df49d37
Matt Smith <matt@mattup.com>
2008-12-29 16:02:48 +0000

    refs #500 added ability to edit emergency number
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@1537 04a73145-ec33-0410-b10b-c8dba00f0e28

--- app/controllers/emergency_numbers_controller.rb
+++ app/controllers/emergency_numbers_controller.rb
@@ -20,6 +20,27 @@ class EmergencyNumbersController < ApplicationController
       redirect_to :action => 'index'
     end
   end
+  def edit    
+      if check_params_for_group
+        group_name = params[:group]
+        @group = Group.find_by_name(group_name)
+        @emergency_number = EmergencyNumber.find(params[:id])
+      else
+        redirect_to :action => 'index'
+      end
+  end
+  def update
+    if check_params_for_group
+      @emergency_number = EmergencyNumber.find(params[:id])
+      @emergency_number.update_attributes(params[:emergency_number])
+      group = Group.find_by_name(params[:group])
+      @emergency_number.group_id = group.id
+      @emergency_number.save!
+      redirect_to :action => 'index', :group => params[:group]
+    else
+      redirect_to :action => 'index'
+    end
+  end
   
   def create
     if check_params_for_group
