8fe529739ea1b3e642a2f85a5949dc7f5b673e3c
Matt Smith <matt@mattup.com>
2008-12-30 15:43:55 +0000

    refs #400
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@1540 04a73145-ec33-0410-b10b-c8dba00f0e28

---
+++ app/controllers/atp_test_results_controller.rb
@@ -0,0 +1,35 @@
+class AtpTestResultsController < ApplicationController
+  
+  def create
+    begin
+      request = params[:atp_test_result]
+      atp_test_result = AtpTestResult.new
+      atp_test_result.result = request[:result]
+      atp_test_result.operator_id = request[:operator_id]
+      atp_test_result.device_id = request[:device_id]
+      atp_test_result.timestamp = request[:timestamp]
+      atp_item_results = request[:atp_item_result]
+      if !atp_item_results.blank?
+        if atp_item_results.class != Array
+          atp_item_results = [atp_item_results]
+        end
+        items = []
+        atp_item_results.each do |item|
+          i = AtpItemResult.new(item)
+          i.operator_id = current_user.id
+          items << i
+        end
+        atp_test_result.atp_item_results = items
+      end
+      atp_test_result.save!
+      respond_to do |format|
+        format.xml { head :ok } 
+      end
+    rescue Exception => e
+      RAILS_DEFAULT_LOGGER.warn("ERROR in AtpTestResultsController:  #{e}")
+      respond_to do |format|
+        format.xml { head :internal_server_error }
+      end
+    end
+  end
+end
