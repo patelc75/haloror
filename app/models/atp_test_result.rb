2c722bbc24a94446a2947ab30ab66f362fc89d91
Matt Smith <matt@mattup.com>
2008-12-30 19:50:12 +0000

    refs #400
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@1549 04a73145-ec33-0410-b10b-c8dba00f0e28

--- app/models/atp_test_result.rb
+++ app/models/atp_test_result.rb
@@ -1,5 +1,5 @@
 class AtpTestResult < ActiveRecord::Base
-  has_many :work_orders, :through => :atp_test_results_work_orders
-  has_many :rmas, :through => :atp_test_results_rmas
-  has_many :atp_item_results, :through => :atp_item_results_atp_test_results
+  has_many :work_orders
+  has_many :rmas
+  has_many :atp_item_results
 end
