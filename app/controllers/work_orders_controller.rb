f9f4277b515e4657ab7b90e38a11eba537bb6a15
Matt Smith <matt@mattup.com>
2009-01-23 23:08:50 +0000

    refs #400
    git-svn-id: http://svn2.assembla.com/svn/halotrac/trunk@1644 04a73145-ec33-0410-b10b-c8dba00f0e28

--- app/controllers/work_orders_controller.rb
+++ app/controllers/work_orders_controller.rb
@@ -2,7 +2,7 @@ class WorkOrdersController < ApplicationController
   def index
     work_orders = find_work_orders
     if work_orders
-      xml = work_orders.to_xml(:skip_types => true, :dasherize => false, :include => {:device_revisions => {:include => {:device_model => {:include => :device_type }}}})
+      xml = work_orders.to_xml(:skip_types => true, :dasherize => false, :include => {:device_revisions => {:include => {:device_model => {:include => {:device_type => {:include => :atp_items}}}}}})
       respond_to do |format|
         format.xml { render :xml => xml }
       end
