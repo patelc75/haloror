793dfface7c91a20f44e59a384139f2809d5495e
Matt Smith <matt@mattup.com>
2009-02-09 15:24:04 -0600

    refs #449 migration from att to cingulat and att

---
+++ db/migrate/20090209211832_convert_att_to_cingular_att_carrier_in_profile.rb
@@ -0,0 +1,17 @@
+class ConvertAttToCingularAttCarrierInProfile < ActiveRecord::Migration
+  def self.up
+    cingular = Carrier.find_by_domain("@cingularme.com")
+    att = Carrier.find_by_domain("@mmode.com")
+    
+    profiles = Profile.find_all_by_carrier_id(att.id)
+    if profiles
+      profiles.each do |profile|
+        profile.carrier_id = cingular.id
+        profile.save! 
+      end
+    end
+  end
+
+  def self.down
+  end
+end
