df8214df4c8d8edd564d94ca7516c660f5816f5c
Matt Smith <matt@mattup.com>
2009-02-13 09:43:52 -0600

    refs #400

--- app/controllers/atp_kits_controller.rb
+++ app/controllers/atp_kits_controller.rb
@@ -3,7 +3,8 @@ class AtpKitsController < ApplicationController
     xml = ""
     bad_serials = []
     kit_hash = params[:kit]
-    serial_numbers = kit_hash[:serial_number]
+    sns = kit_hash[:serial_numbers]
+    serial_numbers = sns[:serial_number]
     if serial_numbers && serial_numbers.class != Array
       serial_numbers = [serial_numbers]
     end
@@ -29,11 +30,11 @@ class AtpKitsController < ApplicationController
         RAILS_DEFAULT_LOGGER.warn("ERROR in AtpKitsController:  #{e}")
     end
       if bad_serials.size > 0
-        xml = "<kit>"
+        xml = "<kit><serial_numbers>"
         bad_serials.each do |sn|
           xml += "<serial_number>#{sn}</serial_number>"
         end
-        xml += "</kit>"
+        xml += "</serial_numbers></kit>"
       else
         xml = "<kit><id>#{@kit.id}</id></kit>"
       end
