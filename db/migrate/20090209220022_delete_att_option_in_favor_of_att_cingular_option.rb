d5718bbb9337b9514e00221aef9ff4efa58691d7
Chirag Patel <chirag@chirag.name>
2009-02-18 09:24:05 +0530

    refs #449 some data checks for removing AT&T in Carriers model

--- db/migrate/20090209220022_delete_att_option_in_favor_of_att_cingular_option.rb
+++ db/migrate/20090209220022_delete_att_option_in_favor_of_att_cingular_option.rb
@@ -1,7 +1,7 @@
 class DeleteAttOptionInFavorOfAttCingularOption < ActiveRecord::Migration
   def self.up
     att = Carrier.find_by_domain('@mmode.com')
-    Carrier.delete(att.id)
+    Carrier.delete(att.id) if !att.nil?
   end
 
   def self.down
