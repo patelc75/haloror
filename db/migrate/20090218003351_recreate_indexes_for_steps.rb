4681c4304fc83cb1e4d9a7e7c48048a96300384e
Matt Smith <matt@mattup.com>
2009-02-17 18:45:57 -0600

    refs #580 index fix for steps user_id then timestamp

---
+++ db/migrate/20090218003351_recreate_indexes_for_steps.rb
@@ -0,0 +1,11 @@
+class RecreateInexesForSteps < ActiveRecord::Migration
+  def self.up
+    remove_index :steps, [:begin_timestamp, :user_id]
+    add_index :steps, [:user_id, :begin_timestamp]
+  end
+
+  def self.down
+    remove_index :steps, [:user_id, :begin_timestamp]
+    add_index :steps, [:begin_timestamp, :user_id]
+  end
+end
