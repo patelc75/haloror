395f331253b02d1083bee3a35c14b3e00d12a182
unknown <Lisa Patel@.(none)>
2009-02-20 16:40:14 +0530

    refs #568 fix device id =0 insertion

--- db/migrate/20090220075023_add_device_id_zero.rb
+++ db/migrate/20090220075023_add_device_id_zero.rb
@@ -1,11 +1,10 @@
 class AddDeviceIdZero < ActiveRecord::Migration
   def self.up
-  	if(device=Device.find_by_id(0).nil?)
-  	  if(device.serial_number != "0123456789")
-  	    execute "UPDATE devices SET serial_number='0123456789' WHERE id=0"	
-  	  else  	
-  		execute "INSERT INTO devices (id, serial_number) VALUES ('0', '0123456789')"	
-  	  end
+  	device=Device.find_by_id(0)
+  	if(device.nil?)
+  	  execute "INSERT INTO devices (id, serial_number) VALUES ('0', '0123456789')"	
+  	elsif(device.serial_number != "0123456789")
+  	  execute "UPDATE devices SET serial_number='0123456789' WHERE id=0"	
   	end
   end
 
