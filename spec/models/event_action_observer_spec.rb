64da52a8eaf38467b8098303fac0b4ed3bca5a38
Matt Smith <matt@mattup.com>
2009-02-24 17:05:16 -0600

    refs #565 rspec for event action observer

---
+++ spec/models/event_action_observer_spec.rb
@@ -0,0 +1,14 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe 'EventActionObserver' do
+        before :each do
+                CriticalMailer.stub!(:deliver_call_center_operator).and_return(:default_value)
+                @event_action = EventAction.new(:description => 'accepted', :user_id => 1)
+                @observer = EventActionObserver.instance
+        end
+
+        it "should invoke after_save on the observed object" do
+                @observer.should_receive(:send_to_backup).with("accepted", @event_action)
+                @observer.before_save(@event_action)
+        end
+end
