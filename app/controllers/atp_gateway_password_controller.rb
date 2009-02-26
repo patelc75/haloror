56d8177ce604416744af16b082493045086bbfcc
Matt Smith <matt@mattup.com>
2009-02-26 15:36:01 -0600

    refs #588 retrieve gateway password

--- app/controllers/atp_gateway_password_controller.rb
+++ app/controllers/atp_gateway_password_controller.rb
@@ -2,11 +2,26 @@ class AtpGatewayPasswordController < ApplicationController
   def index
     serial_number = params[:gateway_access][:serial_number]  if params[:gateway_access]
     password = GatewayPassword.generate_password(serial_number)
-    xml = "<gateway_access><serial_number>#{serial_number}</serial_number>"
-    xml = xml + "<password>#{password}</password>" if password
-    xml = xml + "</gateway_access>"
+    xml = get_gateway_access_xml(serial_number, password)
     respond_to do |format|
       format.xml {render(:xml => xml)}
     end
+  end
+  
+  def retrieve
+    serial_number = params[:gateway_access][:serial_number]  if params[:gateway_access]
+    password = GatewayPassword.retrieve_password(serial_number)
+    xml = get_gateway_access_xml(serial_number, password)
+    respond_to do |format|
+      format.xml {render(:xml => xml)}
+    end
+  end
+  private
+  
+  def get_gateway_access_xml(serial_number, password)
+    xml = "<gateway_access><serial_number>#{serial_number}</serial_number>"
+    xml = xml + "<password>#{password}</password>" if password
+    xml = xml + "</gateway_access>"
+    return xml
   end
 end
