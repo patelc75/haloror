2212408250ac20c5153ee43ed5946567463e2e71
Matt Smith <matt@mattup.com>
2009-03-06 14:12:24 -0600

    refs #551

---
+++ db/migrate/20090306194710_setup_alert_type_for_gw_alarm.rb
@@ -0,0 +1,12 @@
+class SetupAlertTypeForGwAlarm < ActiveRecord::Migration
+  def self.up
+    alert_type = AlertType.new(:alert_type => 'GwAlarmButton')
+    alert_type.save!
+    alert_group = AlertGroup.find_by_group_type('critical')
+    alert_type.alert_groups << alert_group if alert_group
+    alert_type.save!
+  end
+
+  def self.down
+  end
+end
