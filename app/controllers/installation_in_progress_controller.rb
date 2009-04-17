f44a1d17f10d431ddec5ef584017a030c483ee14
Chirag Patel <patelc75@yahoo.com>
2009-04-16 20:27:12 -0400

    refs #665 fix this:run and fail, restart and pass, in progress still says true because the first one is within the last day

--- app/controllers/installation_in_progress_controller.rb
+++ app/controllers/installation_in_progress_controller.rb
@@ -3,12 +3,19 @@ class InstallationInProgressController < RestfulAuthController
   def create
     user_id = params[:installation][:user_id]
     range_timestamp = 1.day.ago
+
     self_test_session = SelfTestSession.find(:first, 
-                                            :conditions => "user_id = #{user_id} AND created_at > '#{range_timestamp}' AND completed_on IS NULL", 
+                                            :conditions => "user_id = #{user_id} AND created_at > '#{range_timestamp}'", #AND completed_on IS NULL",  
                                             :order => 'created_at desc')
-    xml = nil
+
+    debugger
+	xml = nil
     if self_test_session
-      xml = installation_in_progress_xml(true)
+      	if self_test_session.completed_on == nil
+			xml = installation_in_progress_xml(true)
+		else
+			xml = installation_in_progress_xml(false)
+		end	
     else
       xml = installation_in_progress_xml(false)
     end
