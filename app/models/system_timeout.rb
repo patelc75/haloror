d5c9abf77b2a8b6d8e9e418e5b959f41fe0793f0
Chirag Patel <patelc75@yahoo.com>
2009-04-21 21:40:27 -0400

    refs #671 support for new web service that returns poll rate based on access mode, also convert timeouts to seconds (vs minutes

--- app/models/system_timeout.rb
+++ app/models/system_timeout.rb
@@ -1,6 +1,6 @@
 class SystemTimeout < ActiveRecord::Base
   validates_presence_of :mode
-  validates_presence_of :gateway_offline_timeout_min
-  validates_presence_of :device_unavailable_timeout_min
-  validates_presence_of :strap_off_timeout_min
+  validates_presence_of :gateway_offline_timeout_sec
+  validates_presence_of :device_unavailable_timeout_sec
+  validates_presence_of :strap_off_timeout_sec
 end
