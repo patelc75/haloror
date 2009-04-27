71f57db567d4eb080ceaff545b0770ff3bad217b
Alex Kah <alex@narfonix.com>
2009-04-27 00:27:09 -0400

    refs #665 adding real comments to shell script

--- bin/updaterepos.sh
+++ bin/updaterepos.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
-# adding more comments
-# added comments
 
+# pull for each git repo for devredmine and redmine
 cd /home/web/devredmine/git_repos/haloror && git pull >> /dev/null 
 cd /home/web/devredmine/git_repos/haloflex && git pull >> /dev/null
 
+# add changesets to each ticket
 cd /home/web/devredmine && ruby script/runner "Repository.fetch_changesets" -e production >> /dev/null
 
