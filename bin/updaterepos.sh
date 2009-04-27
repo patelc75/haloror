dca3ff3ad1628839e5b4ca71492e12b551906ebe
Alex Kah <alex@narfonix.com>
2009-04-27 00:23:48 -0400

    refs #665 even more redmine test comments!

--- bin/updaterepos.sh
+++ bin/updaterepos.sh
@@ -1,5 +1,6 @@
 #!/bin/bash 
 # adding more comments
+# added comments
 
 cd /home/web/devredmine/git_repos/haloror && git pull >> /dev/null 
 cd /home/web/devredmine/git_repos/haloflex && git pull >> /dev/null
