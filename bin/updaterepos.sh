53885bf1446a8fc004d25d05469fd441b6a5c54a
Alex Kah <alex@narfonix.com>
2009-04-26 20:30:25 -0400

    refs #670 alex just testing redmine stuff

---
+++ bin/updaterepos.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+cd /home/web/devredmine/git_repos/haloror && git pull >> /dev/null 
+cd /home/web/devredmine/git_repos/haloflex && git pull >> /dev/null
+
+cd /home/web/devredmine && ruby script/runner "Repository.fetch_changesets" -e production >> /dev/null
+
