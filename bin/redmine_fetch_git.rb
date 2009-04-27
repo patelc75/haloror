380e5227c5abbd95d955f580b79aef29d61c5b6d
Alex Kah <alex@narfonix.com>
2009-04-27 02:24:59 -0400

    refs #867 ruby script to update all git LIVE and DEV repos for redmine

---
+++ bin/redmine_fetch_git.rb
@@ -0,0 +1,46 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'fileutils' 
+require 'active_record'
+
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+time_zone = 'CDT'
+
+ROOT = '/home/web/redmine'
+DEVROOT = '/home/web/devredmine'
+REPOS = '/git_repos/*'
+REPOROOT = ROOT + REPOS
+REPODEVROOT = DEVROOT + REPOS 
+
+# Output time to Log File
+puts Time.now
+
+# Update Each Git Repo for LIVE Redmine
+Dir[REPOROOT].each do |path|  
+  puts path 
+  FileUtils.cd(path)  
+  system("git pull")
+end
+
+# Update Each Git Repo for DEV Redmine
+Dir[REPODEVROOT].each do |path|
+  puts path 
+  FileUtils.cd(path)  
+  system("git pull")
+end
+
+# Update Changesets for DEV Redmine
+puts DEVROOT 
+FileUtils.cd(DEVROOT)
+puts "...fetching changesets"
+system("/usr/local/bin/ruby script/runner --verbose \"Repository.fetch_changesets\" -e production")
+
+# Update Changesets for LIVE Redmine
+puts ROOT
+FileUtils.cd(ROOT)
+puts "....fetching changesets"
+system("/usr/local/bin/ruby script/runner --verbose \"Repository.fetch_changesets\" -e production")
+
+# Output Ending Statement
+puts "Update Done! \n \n \n"
