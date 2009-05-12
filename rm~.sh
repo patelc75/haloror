0150bbceeaadc2ffef002ed28283731049e24617
Chirag Patel <patelc75@yahoo.com>
2009-05-12 17:32:18 -0400

    testing .gitignore

--- rm~.sh
+++ rm~.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-echo "recursively removing backup files from"
+echo "recursively removing backup files from the app root directory"
 pwd
 find ./ -name '*~' -exec rm '{}' \; -print -or -name ".*~" -exec rm {} \; -print
