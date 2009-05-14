954e3c2a3ad49f655330bf73730dbbedd566cd30
Chirag Patel <patelc75@yahoo.com>
2009-05-14 11:50:32 -0400

    refs #1545 format emails for gw alarm button support

--- app/models/gw_alarm_button_timeout.rb
+++ app/models/gw_alarm_button_timeout.rb
@@ -12,7 +12,7 @@ class GwAlarmButtonTimeout < DeviceAlert
   end
   
   def email_body
-    "It has been #{GW_RESET_BUTTON_FOLLOW_UP_TIMEOUT / 60} minutes and we have detected that the Gateway Alarm button has not been pushed for #{user.name}'s #{event.event_type} on #{timestamp}\n\n" +
+    "It has been #{GW_RESET_BUTTON_FOLLOW_UP_TIMEOUT / 60} minutes and we have detected that the Gateway Alarm button has not been pushed for #{user.name}'s #{event.class.class_name} on #{timestamp}\n\n" +
       "Sincerely, Halo Staff"
   end
 
