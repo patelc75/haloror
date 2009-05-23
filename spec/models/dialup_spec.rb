4e4267f7c06d8fc7dd634e6f785a85a373c5ac55
Chirag Shah <chirag.sweng@gmail.com>
2009-05-23 17:31:18 +0530

    refs #643 added new dialup_spec file for dialup bundle_job process

---
+++ spec/models/dialup_spec.rb
@@ -0,0 +1,31 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe BundleJob do
+	before(:all) do
+		
+		FileUtils.cp "#{RAILS_ROOT}/spec/H200000023_1240876150.tar.bz2","#{RAILS_ROOT}/dialup"
+		BundleJob.job_process_bundles
+		@battery = Battery.count(:conditions => ["user_id = ?",44])
+	end
+
+	it "should have 94 batteries" do
+	@battery.should == 94
+	end
+
+	it "should have 1 battery_unplugged" do
+	BatteryUnplugged.count(:conditions => ["user_id = ?",44]).should == 1
+	end
+
+	it "should have 93 vitals" do
+	Vital.count(:conditions => ["user_id = ?",44]).should == 93
+	end
+	
+	it "should have 93 steps" do
+	Step.count(:conditions => ["user_id = ?",44]).should == 93
+	end
+	
+	it "should have 93 skin_temps" do
+	SkinTemp.count(:conditions => ["user_id = ?",44]).should == 93
+	end
+	
+end
