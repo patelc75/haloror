95d1dbe0126e57b8423648132dbf7abdb81f3cd2
Chirag Shah <chirag.sweng@gmail.com>
2009-06-16 00:39:27 +0530

    refs #1612 changes for battery critical mails working for caregivers when mode is stop or nil

--- app/models/battery_critical.rb
+++ app/models/battery_critical.rb
@@ -1,10 +1,11 @@
 class BatteryCritical < ActiveRecord::Base
   set_table_name "battery_criticals"
-  
+  belongs_to :user
   def after_save
   	if self.mode == 'stop'
   		@most_recent = BatteryReminder.most_recent_reminder(self.device_id)
 		@most_recent.update_attributes(:stopped_at => Time.now)	if @most_recent
+		DeviceAlert.notify_carigivers(self)
 	elsif self.mode == 'start'
 		BatteryReminder.create(:reminder_num => 1,:user_id =>self.user_id ,:device_id => self.device_id)
 	elsif self.mode == nil
@@ -14,6 +15,14 @@ class BatteryCritical < ActiveRecord::Base
   
   def to_s
     "Battery critically low on #{UtilityHelper.format_datetime_readable(timestamp, user)}"
+  end
+  
+  def email_body
+  	if mode == 'stop'
+  		"Battery critical state resolved on #{UtilityHelper.format_datetime_readable(timestamp, user)}"
+  	else
+   	   "Battery critically low on #{UtilityHelper.format_datetime_readable(timestamp, user)}"
+    end
   end
   
   def self.new_initialize(random=false)
