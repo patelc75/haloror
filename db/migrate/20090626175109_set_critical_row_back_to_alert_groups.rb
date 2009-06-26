9a7eeaba58718230d2f67095ed3828748e9ac461
Chirag Shah <chirag.sweng@gmail.com>
2009-06-27 01:28:56 +0530

    refs #1709 comment the whole migration code for did not want to run any of the server

--- db/migrate/20090626175109_set_critical_row_back_to_alert_groups.rb
+++ db/migrate/20090626175109_set_critical_row_back_to_alert_groups.rb
@@ -1,18 +1,20 @@
 class SetCriticalRowBackToAlertGroups < ActiveRecord::Migration
-  def self.up
-  	alert_group = AlertGroup.find_by_group_type('critical')
-  	if not alert_group  		
-  		sql = ActiveRecord::Base.connection()
-  		sql.execute "Insert into alert_groups(id,group_type,created_at,updated_at) values (3,'critical','#{Time.now.utc}','#{Time.now.utc}');"
-  		alert_group = AlertGroup.find_by_group_type('critical')
-  		@alerttypes = AlertType.find_all_by_alert_type ['Fall','Panic','GwAlarmButton','GwAlarmButtonTimeout']
-  		@alerttypes.each do |alert_type|
-  		alert_type.alert_groups << alert_group
-  		end
-  	end
+# Run this migration on dev,idev,cdev and sdev on 27th june 2009 by chirag shah	
+	
+#  def self.up
+#  	alert_group = AlertGroup.find_by_group_type('critical')
+#  	if not alert_group  		
+#  		sql = ActiveRecord::Base.connection()
+#  		sql.execute "Insert into alert_groups(id,group_type,created_at,updated_at) values (3,'critical','#{Time.now.utc}','#{Time.now.utc}');"
+#  		alert_group = AlertGroup.find_by_group_type('critical')
+#  		@alerttypes = AlertType.find_all_by_alert_type ['Fall','Panic','GwAlarmButton','GwAlarmButtonTimeout']
+#  		@alerttypes.each do |alert_type|
+#  		alert_type.alert_groups << alert_group
+#  		end
+#  	end
   	
-  end
+#  end
 
-  def self.down
-  end
+#  def self.down
+#  end
 end
