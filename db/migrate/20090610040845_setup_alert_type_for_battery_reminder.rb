0e7463295be4d104fd8b4a7598870df00ac7fb8c
Chirag Patel <chirag@chirag.name>
2009-06-25 20:06:14 -0500

    fix migrations that wiped out critical alert_groups type

--- db/migrate/20090610040845_setup_alert_type_for_battery_reminder.rb
+++ db/migrate/20090610040845_setup_alert_type_for_battery_reminder.rb
@@ -2,7 +2,7 @@ class SetupAlertTypeForBatteryReminder < ActiveRecord::Migration
   def self.up
     alert_type = AlertType.new(:alert_type => 'BatteryReminder')
     alert_type.save!
-    alert_group = AlertGroup.find_by_group_type('critical')
+    alert_group = AlertGroup.find_by_group_type('battery')
     alert_type.alert_groups << alert_group if alert_group
     alert_type.save!	
   end
--- db/migrate/20090611052113_change_alert_type_for_battery_reminder.rb
+++ db/migrate/20090611052113_change_alert_type_for_battery_reminder.rb
@@ -1,14 +1,14 @@
 class ChangeAlertTypeForBatteryReminder < ActiveRecord::Migration
-  def self.up
-  	alert_type = AlertType.find_by_alert_type('BatteryReminder')
-    alert_group = AlertGroup.find_by_group_type('battery')
-	alert_type.alert_groups.each do |group|
-	group.destroy
-	end
-    alert_type.alert_groups << alert_group if alert_group
-    alert_type.save!
-  end
-
-  def self.down
-  end
+#  def self.up
+#  	alert_type = AlertType.find_by_alert_type('BatteryReminder')
+#    alert_group = AlertGroup.find_by_group_type('battery')
+#	alert_type.alert_groups.each do |group|
+#	group.destroy
+#	end
+#    alert_type.alert_groups << alert_group if alert_group
+#    alert_type.save!
+#  end
+#
+#  def self.down
+#  end
 end
