c937509747a230404cb232234619a1cf34f8d7e8
Chirag Patel <chirag@halomonitoring.com>
2009-10-08 13:16:30 -0500

    update caregivers_by_user_id() postgresql function

---
+++ db/migrate/20091008060304_update_caregivers_by_user_id.rb
@@ -0,0 +1,35 @@
+class UpdateCaregiversByUserId < ActiveRecord::Migration
+  def self.up
+	ddl = <<-eos
+	  drop function caregivers_by_user_id(integer);
+	  drop type caregiver_by_user;
+	  
+		create type caregiver_by_user AS (
+		    roles_user_id integer,
+		    email_addr character varying(255),
+		    user_id integer,
+		    pos integer,
+		    removed boolean,
+		    active boolean,
+			  phone boolean,
+			  email boolean,
+			  text boolean,
+			  rel character varying(255),
+		    key boolean
+	  	);
+	  	
+		CREATE OR REPLACE FUNCTION caregivers_by_user_id(integer)
+		RETURNS setof caregiver_by_user
+		AS
+		$$
+      select roles_users.id as roles_user_id, users.email as email_addr, users.id as user_id, roles_users_options.position as pos, roles_users_options.removed as removed, roles_users_options.active as active, roles_users_options.phone_active as phone, roles_users_options.email_active as email, roles_users_options.text_active as txt, roles_users_options.relationship as rel, roles_users_options.is_keyholder as key from roles_users, users, roles_users_options, roles where (roles_users.user_id = users.id and roles_users_options.roles_user_id = roles_users.id) and roles.id = roles_users.role_id and roles.authorizable_id = $1 order by pos;
+		$$ 
+		LANGUAGE 'sql' STABLE;
+    eos
+    
+    execute ddl
+  end
+
+  def self.down
+  end
+end
