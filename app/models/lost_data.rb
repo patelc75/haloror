c621cce5b4692ce164145957af985f07d4b31a7d
Chirag Patel <chirag@halomonitoring.com>
2009-10-13 08:49:05 -0500

    refs #2078 Refactored and cleaned up compliance code (date ranges instead of daily reports in many methods)
    git commit -m refs

--- app/models/lost_data.rb
+++ app/models/lost_data.rb
@@ -1,5 +1,9 @@
 class LostData < ActiveRecord::Base
-  
+
+  def self.rows_by_user(user_id, begin_time, end_time)
+    LostData.find(:all, :conditions => "user_id = #{user_id} AND end_time <= '#{end_time.to_s(:db)}' AND begin_time >= '#{begin_time.to_s(:db)}'", :order => "id desc")  
+  end
+    
   def self.user_ids_with_lost_data(start_time, stop_time)
     result = LostData.find(:all, :conditions => "begin_time > '#{start_time}' AND end_time < '#{stop_time}'")
     user_ids = []
--- app/models/strap_not_worn.rb
+++ app/models/strap_not_worn.rb
@@ -1,5 +1,9 @@
 class StrapNotWorn < ActiveRecord::Base
 
+  def self.rows_by_user(user_id, begin_time, end_time)
+    StrapNotWorn.find(:all, :conditions => "user_id = #{user_id} AND end_time <= '#{end_time.to_s(:db)}' AND begin_time >= '#{begin_time.to_s(:db)}'", :order => "id desc")  
+  end
+  
   def self.user_ids_with_strap_not_worns(start_time, stop_time)
     result = StrapNotWorn.find(:all, :conditions => "begin_time > '#{start_time}' AND end_time < '#{stop_time}'")
     user_ids = []
