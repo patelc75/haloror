8b96086cb1aa07187bf0f143d0e8cef0299dd0e8
Chirag Patel <chirag@halomonitoring.com>
2009-10-23 16:52:57 -0500

    merged subscriber agreement from belt-clip branch

--- bin/live.sh
+++ bin/live.sh
@@ -1,1 +1,1 @@
-rake halo:post vital=all method=activerecord increment=15 duration=5000 frequency=5 type=live swing=10 slope=10 curve=random user_id=1 gateway_id=1 gateway_serial_num=0123456789 RAILS_ENV="production"
+rake halo:post vital=all method=activerecord increment=15 duration=5000 frequency=5 type=live swing=10 slope=10 curve=random user_id=5 gateway_id=0 gateway_serial_num=0123456789 RAILS_ENV="production"
