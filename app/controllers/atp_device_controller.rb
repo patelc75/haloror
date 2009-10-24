55833f4b22c5a7f68d2039d38e0ba0f4c09874b8
Chirag Patel <chirag@halomonitoring.com>
2009-10-23 19:18:03 -0500

    commented https://atp.halomonitor.com/atp_device.xml method

--- app/controllers/atp_device_controller.rb
+++ app/controllers/atp_device_controller.rb
@@ -1,4 +1,11 @@
-class AtpDeviceController < ApplicationController
+#Important associations for this file
+# devices ---> device_revisions <---> atp_items
+#              device_revisions ----> device_model ---> device_type
+#              device_revisions <---> work_orders
+  class AtpDeviceController < ApplicationController
+  
+  #returns an XML doc of a single device (revision, model, type) along with its associated atp items
+  #The device_revision is needed to get all model, type, and associated atp items. If it's not passed in, then it get be queried from the work_order_id
   def index
     serial_number = params[:serial_number]
     work_order_id = params[:work_order_id]
@@ -26,6 +33,9 @@ class AtpDeviceController < ApplicationController
   end
   
   private
+  
+  #returns the device_revision_id based on the serial number's first two chars (eg. H1) and the work_order_id
+  #by searching the many-to-many device_revisons_work_orders table. If the device_revision_id is passed in, return it back without doing anything
   def get_device_revision_id(id, work_order_id, serial_number)
     if !id.blank?
       return id
@@ -38,7 +48,7 @@ class AtpDeviceController < ApplicationController
                                             :include => {:device_revision => {:device_model => :device_type}},
                                             :conditions => "work_order_id = #{work_order_id} AND device_revisions_work_orders.device_revision_id IN (Select device_revisions.id from device_revisions inner join (device_models inner join device_types on device_models.device_type_id = device_types.id) on device_revisions.device_model_id = device_models.id Where device_types.device_type = '#{t}')",
                                             :order => "device_revisions_work_orders.created_at desc")
-      return drwo.device_revision_id      
+      return drwo.device_revision_id     
     end
   end
 end
