d1959b1cc45393c3cb331173bf47ff464b8c6326
Chirag Shah <chirag.sweng@gmail.com>
2009-10-26 23:41:13 +0530

    refs #2131 check for existing device type, model, revision

--- db/migrate/20090722205433_new_device_type_for_belt_clip.rb
+++ db/migrate/20090722205433_new_device_type_for_belt_clip.rb
@@ -1,11 +1,23 @@
 class NewDeviceTypeForBeltClip < ActiveRecord::Migration
   def self.up
-  	  belt_clip_type = DeviceType.new(:device_type => 'Belt Clip', :mac_address_type => 0)
-      belt_clip_type.save!
-      belt_clip_model = DeviceModel.new(:device_type_id => belt_clip_type.id, :part_number => '22001007')
-      belt_clip_model.save!
-      belt_clip_revision = DeviceRevision.new(:device_model_id => belt_clip_model.id, :revision => 'A')
-      belt_clip_revision.save!
+  	  belt_clip_type = DeviceType.find_by_device_type_and_mac_address_type('Belt Clip',0)
+  	  if not belt_clip_type
+  	  	belt_clip_type = DeviceType.new(:device_type => 'Belt Clip', :mac_address_type => 0)
+      	belt_clip_type.save!
+      end
+      
+      belt_clip_model = DeviceModel.find_by_device_type_id_and_part_number(belt_clip_type.id,'22001007')
+      if not belt_clip_model
+      	belt_clip_model = DeviceModel.new(:device_type_id => belt_clip_type.id, :part_number => '22001007')
+      	belt_clip_model.save!
+  	  end
+      
+  	  belt_clip_revision = DeviceRevision.find_by_device_model_id_and_revision(belt_clip_model.id,'A')
+  	  if not belt_clip_revision
+  	  	belt_clip_revision = DeviceRevision.new(:device_model_id => belt_clip_model.id, :revision => 'A')
+      	belt_clip_revision.save!
+  	  end
+  	  
   end
 
   def self.down
