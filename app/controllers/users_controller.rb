c44e2879c1e678916a2bd9a092f87f32d67a07f0
Chirag Shah <chirag.sweng@gmail.com>
2009-10-29 01:18:12 +0530

    refs #2136 solve issue for generate multiple users with subscriber page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,17 +39,18 @@ class UsersController < ApplicationController
   	RAILS_DEFAULT_LOGGER.debug("****START create_subscriber")
   	
 	senior_user_id = params[:user_id]
-	if !request.post?
+	if !request.post?  # when click on the skip this page
 		@user = User.find(params[:user_id])
+		session[:new_user] = nil
 	else
+		
   		if params[:users][:same_as_senior] == "1"
   			@user = User.find_by_id(params[:user_id])
-  			@max_position = get_max_caregiver_position(@user)
   			subscriber_user_id = senior_user_id
 			RAILS_DEFAULT_LOGGER.debug("**same as senior == 1 ; subscriber_user_id = #{@senior_user_id}")
 			bill_to_fn = @user.profile.first_name
 			bill_to_ln = @user.profile.last_name
-
+			credit_card_validate(senior_user_id,subscriber_user_id,@user)
   		else
   			@user = User.new
   			@user.email = params[:email]
@@ -84,92 +85,13 @@ class UsersController < ApplicationController
 	        	else
 	        		raise "Invalid Subscriber"
 	  			end
+	  			subscriber_user_id = @user.id
+				RAILS_DEFAULT_LOGGER.debug("**subscriber_user_id= #{@user.id}")
+	  			credit_card_validate(senior_user_id,subscriber_user_id,@user)
   			end
-  		
-	  		subscriber_user_id = @user.id
-			RAILS_DEFAULT_LOGGER.debug("**subscriber_user_id= #{@user.id}")
-			bill_to_fn = @user.profile.first_name
-			bill_to_ln = @user.profile.last_name
 	
 	  	end
-  		@senior = User.find(senior_user_id)
-		patient = User.find(params[:user_id].to_i)
-		role = @user.has_role 'subscriber', patient
 
-		subscriber = @user
-
-		@roles_user = patient.roles_user_by_subscriber(subscriber)
-
-		update_from_position(@max_position, @roles_user.role_id, subscriber.id)
-
-		RolesUsersOption.create(:roles_user_id => @roles_user.id, :position => @max_position, :active => 0)
-  
-  		@gateway = Device.new
-  		@strap = Device.new    
-
-    	# Start authorize.net create subscription code
-  		auth = MerchantAuthenticationType.new(AUTH_NET_LOGIN, AUTH_NET_TXN_KEY)
-  		# subscription name - use subscriber user id
-		subname = "sen_uid-#{senior_user_id}-sub_uid-#{subscriber_user_id}"
-
-		RAILS_DEFAULT_LOGGER.info("Attempting to create Authorize.Net subscription.  Subscription name = #{subname}")
-		RAILS_DEFAULT_LOGGER.debug("Authorize.Net AUTH_NET_LOGIN= #{AUTH_NET_LOGIN}, AUTH_NET_TXN_KEY= #{AUTH_NET_TXN_KEY}")
-
-	  	# 1 month interval
-		interval = IntervalType.new(AUTH_NET_SUBSCRIPTION_INTERVAL, AUTH_NET_SUBSCRIPTION_INTERVAL_UNITS)
-
-		sub_start_date = Time.now
-		schedule = PaymentScheduleType.new(interval, sub_start_date.strftime("%Y-%m-%d"), AUTH_NET_SUBSCRIPTION_TOTAL_OCCURANCES, 0)
-	
-		RAILS_DEFAULT_LOGGER.info("Authorize.Net Subscription startdate = #{sub_start_date.strftime("%Y-%m-%d")} num_occurances = #{AUTH_NET_SUBSCRIPTION_TOTAL_OCCURANCES} interval = #{AUTH_NET_SUBSCRIPTION_INTERVAL} interval_units = #{AUTH_NET_SUBSCRIPTION_INTERVAL_UNITS}")
-	
-		cc_exp = "#{params[:credit_card][:"expiration_time(1i)"]}-#{params[:credit_card][:"expiration_time(2i)"]}"
-
-		cinfo = CreditCardType.new(params[:credit_card][:number], cc_exp)
-		
-		binfo = NameAndAddressType.new(bill_to_fn, bill_to_ln)
-		RAILS_DEFAULT_LOGGER.info("Authorize.Net Subscription cc fn = #{binfo.firstName} cc ln = #{binfo.lastName}")
-	
-		charge = @senior.group_recurring_charge
-	
-		aReq = ArbApi.new
-		xmlout = aReq.CreateSubscription(auth,subname,schedule,charge,0, cinfo,binfo)
-	
-		RAILS_DEFAULT_LOGGER.info("Authorize.Net Subscription amount = #{AUTH_NET_SUBSCRIPTION_BILL_AMOUNT_PER_INTERVAL}")
-	
-		RAILS_DEFAULT_LOGGER.info("Authorize.Net Submitting to URL = #{AUTH_NET_URL}")
-		xmlresp = HttpTransport.TransmitRequest(xmlout, AUTH_NET_URL)
-
-		apiresp = aReq.ProcessResponse(xmlresp)
-	
-		RAILS_DEFAULT_LOGGER.info("\nXML Dump:" + xmlresp)
-
-		if apiresp.success 
-		   RAILS_DEFAULT_LOGGER.info("Subscription Created successfully")
-		   RAILS_DEFAULT_LOGGER.info("Subscription id : " + apiresp.subscriptionid)
-		else
-			RAILS_DEFAULT_LOGGER.info("Subscription Creation Failed")
-			apiresp.messages.each { |message| 
-				RAILS_DEFAULT_LOGGER.info("Error Code=" + message.code)
-				RAILS_DEFAULT_LOGGER.info("Error Message = " + message.text)
-				flash[:warning] = "Unable to create subscription.  Check credit card information. Error Code=" + message.code + ", Error Message = " + message.text
-			}   
-			raise "Unable to create subscription."
-
-		end
-		
-		@subscription = Subscription.new
-		@subscription[:arb_subscriptionId] = apiresp.subscriptionid
-		@subscription[:senior_user_id] = senior_user_id
-		@subscription[:subscriber_user_id] = subscriber_user_id
-		@subscription[:cc_last_four] = (params[:credit_card][:number]).last(4)
-		@subscription[:special_notes] = params[:credit_card][:special_notes]
-		@subscription[:bill_amount] = charge
-		@subscription[:bill_to_first_name] = bill_to_fn
-		@subscription[:bill_to_last_name] = bill_to_ln
-		@subscription[:bill_start_date] = sub_start_date
-		@subscription.save!
-		
 	  	# End authorize.net create subscription code 	
   	end
 
@@ -738,6 +660,83 @@ end
   end
   
   private
+  
+  def credit_card_validate(senior_user_id,subscriber_user_id,user)
+  		bill_to_fn = user.profile.first_name
+		bill_to_ln = user.profile.last_name
+  	  	
+		@senior = User.find(senior_user_id)
+		patient = User.find(params[:user_id].to_i)
+		role = user.has_role 'subscriber', patient
+  
+  		#@gateway = Device.new
+  		#@strap = Device.new    
+
+    	# Start authorize.net create subscription code
+  		auth = MerchantAuthenticationType.new(AUTH_NET_LOGIN, AUTH_NET_TXN_KEY)
+  		# subscription name - use subscriber user id
+		subname = "sen_uid-#{senior_user_id}-sub_uid-#{subscriber_user_id}"
+
+		RAILS_DEFAULT_LOGGER.info("Attempting to create Authorize.Net subscription.  Subscription name = #{subname}")
+		RAILS_DEFAULT_LOGGER.debug("Authorize.Net AUTH_NET_LOGIN= #{AUTH_NET_LOGIN}, AUTH_NET_TXN_KEY= #{AUTH_NET_TXN_KEY}")
+
+	  	# 1 month interval
+		interval = IntervalType.new(AUTH_NET_SUBSCRIPTION_INTERVAL, AUTH_NET_SUBSCRIPTION_INTERVAL_UNITS)
+
+		sub_start_date = Time.now
+		schedule = PaymentScheduleType.new(interval, sub_start_date.strftime("%Y-%m-%d"), AUTH_NET_SUBSCRIPTION_TOTAL_OCCURANCES, 0)
+	
+		RAILS_DEFAULT_LOGGER.info("Authorize.Net Subscription startdate = #{sub_start_date.strftime("%Y-%m-%d")} num_occurances = #{AUTH_NET_SUBSCRIPTION_TOTAL_OCCURANCES} interval = #{AUTH_NET_SUBSCRIPTION_INTERVAL} interval_units = #{AUTH_NET_SUBSCRIPTION_INTERVAL_UNITS}")
+	
+		cc_exp = "#{params[:credit_card][:"expiration_time(1i)"]}-#{params[:credit_card][:"expiration_time(2i)"]}"
+
+		cinfo = CreditCardType.new(params[:credit_card][:number], cc_exp)
+		
+		binfo = NameAndAddressType.new(bill_to_fn, bill_to_ln)
+		RAILS_DEFAULT_LOGGER.info("Authorize.Net Subscription cc fn = #{binfo.firstName} cc ln = #{binfo.lastName}")
+	
+		charge = @senior.group_recurring_charge
+	
+		aReq = ArbApi.new
+		xmlout = aReq.CreateSubscription(auth,subname,schedule,charge,0, cinfo,binfo)
+	
+		RAILS_DEFAULT_LOGGER.info("Authorize.Net Subscription amount = #{AUTH_NET_SUBSCRIPTION_BILL_AMOUNT_PER_INTERVAL}")
+	
+		RAILS_DEFAULT_LOGGER.info("Authorize.Net Submitting to URL = #{AUTH_NET_URL}")
+		xmlresp = HttpTransport.TransmitRequest(xmlout, AUTH_NET_URL)
+
+		apiresp = aReq.ProcessResponse(xmlresp)
+	
+		RAILS_DEFAULT_LOGGER.info("\nXML Dump:" + xmlresp)
+
+				if apiresp.success 
+			session[:new_user] = nil
+		   RAILS_DEFAULT_LOGGER.info("Subscription Created successfully")
+		   RAILS_DEFAULT_LOGGER.info("Subscription id : " + apiresp.subscriptionid)
+		else
+			RAILS_DEFAULT_LOGGER.info("Subscription Creation Failed")
+			apiresp.messages.each { |message| 
+				RAILS_DEFAULT_LOGGER.info("Error Code=" + message.code)
+				RAILS_DEFAULT_LOGGER.info("Error Message = " + message.text)
+				flash[:warning] = "Unable to create subscription.  Check credit card information. Error Code=" + message.code + ", Error Message = " + message.text
+			}   
+			raise "Unable to create subscription."
+
+		end
+		
+		@subscription = Subscription.new
+		@subscription[:arb_subscriptionId] = apiresp.subscriptionid
+		@subscription[:senior_user_id] = senior_user_id
+		@subscription[:subscriber_user_id] = subscriber_user_id
+		@subscription[:cc_last_four] = (params[:credit_card][:number]).last(4)
+		@subscription[:special_notes] = params[:credit_card][:special_notes]
+		@subscription[:bill_amount] = charge
+		@subscription[:bill_to_first_name] = bill_to_fn
+		@subscription[:bill_to_last_name] = bill_to_ln
+		@subscription[:bill_start_date] = sub_start_date
+		@subscription.save!
+  end
+  
 =begin  
   def enable_by_default(roles_user)
   	
