7be9f5dd3d3a4aec02a59ec40b1f58b5047f60c7
Chirag Patel <chirag@chirag.name>
2009-11-24 16:04:56 -0600

    error in a migration, add '_sec' to one of the columns

--- db/migrate/20091117195711_set_default_to_system_timeouts.rb
+++ db/migrate/20091117195711_set_default_to_system_timeouts.rb
@@ -1,10 +1,10 @@
 class SetDefaultToSystemTimeouts < ActiveRecord::Migration
   def self.up
-  	#change_column :system_timeouts,:critical_event_delay,:default => '120 seconds'
+  	#change_column :system_timeouts,:critical_event_delay_sec,:default => '120 seconds'
   	
   	SystemTimeout.find(:all).each do |system_timeout|
-  		if system_timeout.critical_event_delay.nil? or system_timeout.critical_event_delay == ""
-  			system_timeout.update_attributes(:critical_event_delay => 120)
+  		if system_timeout.critical_event_delay_sec.nil? or system_timeout.critical_event_delay_sec == ""
+  			system_timeout.update_attributes(:critical_event_delay_sec => 120)
   		end
   	end
   	
