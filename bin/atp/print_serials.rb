db8fe21c91319295025ec8fe0a75a3bae135ac52
Chirag Patel <chirag@chirag.name>
2009-12-01 10:31:34 -0600

    alex atp

--- bin/atp/print_serials.rb
+++ bin/atp/print_serials.rb
@@ -2,9 +2,9 @@ require 'rubygems'
 require 'faster_csv'
   
 
-
+b=[]
 FasterCSV.foreach("atp-devices.csv", :quote_char => '"', :col_sep =>'|', :row_sep =>"\n") do |row|
-   b = row[1]
-   puts b
+   b << row
 end
 
+puts b
