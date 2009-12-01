18c24fab367d871c4c25c6467329ff87ec56ce06
Chirag Patel <chirag@chirag.name>
2009-12-01 10:33:27 -0600

    alex atp

--- bin/atp/print_serials.rb
+++ bin/atp/print_serials.rb
@@ -4,7 +4,8 @@ require 'faster_csv'
 
 b=[]
 FasterCSV.foreach("atp-devices.csv", :quote_char => '"', :col_sep =>'|', :row_sep =>"\n") do |row|
-   b << row
+   a = row[0] + ', ' + row[1]
+   b << a 
 end
 
 puts b
