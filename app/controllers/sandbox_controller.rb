e732db9021ef34fdd32161cc35f62da9ddb391ca
Chirag Patel <chirag@halomonitoring.com>
2009-12-22 23:58:30 -0600

    refs #2368 fix local times for battery reminders

--- app/controllers/sandbox_controller.rb
+++ app/controllers/sandbox_controller.rb
@@ -4,10 +4,15 @@ class SandboxController < ApplicationController
   #use this method to debug Rufus jobs
   #run server like this: ruby script/server -p 3001 --debugger
   def ruby_debug
-    debugger
+    #debugger
     #DeviceAlert.job_process_crtical_alerts() #Rufus job
     #BundleJob.process_xml_file("dialup/H200000025_1258657676_2.xml") #Rufus job
-    BatteryReminder.send_reminders()
+    #BatteryReminder.send_reminders()
+		BatteryReminder.create(:device_id => 215, 
+							   :reminder_num => 3,
+							   :user_id => 278,
+							   :time_remaining => 1000,
+							   :battery_critical_id => 7014)
   end
 
   def helloworld
