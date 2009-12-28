8997a4b78db2d8951b43f2603edcbc1743b1516a
Chirag Shah <chirag.sweng@gmail.com>
2009-12-29 01:13:39 +0530

    refs #2324 save caregiver with the alert options

--- db/migrate/20091226172340_add_columns_to_profiles.rb
+++ db/migrate/20091226172340_add_columns_to_profiles.rb
@@ -1,10 +1,10 @@
 class AddColumnsToProfiles < ActiveRecord::Migration
   def self.up
-  	add_column :profiles, :home_phone_order, :integer
-  	add_column :profiles, :work_phone_order, :integer
-  	add_column :profiles, :cell_phone_order, :integer
-  	add_column :profiles, :other_phone_order, :integer
-  	add_column :profiles, :other_phone, :integer
+  	add_column :profiles, :home_phone_order, :string
+  	add_column :profiles, :work_phone_order, :string
+  	add_column :profiles, :cell_phone_order, :string
+  	add_column :profiles, :other_phone_order, :string
+  	add_column :profiles, :other_phone, :string
   end
 
   def self.down
