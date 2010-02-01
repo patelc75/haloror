bb4871ea9b9201b7efc38c6826f15db4daf2703c
ramonrails <ramonrails@gmail.com>
2010-02-01 18:16:11 +0530

    refs #2456. device name is now picked from database after a best guess between certain names. The device name is shown full with model and revision. Should we rather have online_flag on revisions?

--- app/models/device_type.rb
+++ app/models/device_type.rb
@@ -1,4 +1,34 @@
 class DeviceType < ActiveRecord::Base
   has_many :device_models
   has_many :serial_number_prefixes
+  
+  # find device by any name in given comma separated values
+  # usage: find_product_bY_any_name("Halo Complete, Chest Strap")
+  #
+  def self.find_product_by_any_name(phrase)
+    full_name = ""
+    names = phrase.split(',').collect {|p| p.gsub(/^ +/,'').gsub(/ +$/,'') }
+    names.each do |name|
+      unless count(:conditions => {:device_type => name}).zero?
+        full_name = find_by_device_type(name).latest_model_revision_name
+        break
+      end
+    end
+    full_name
+  end
+  
+  # latest revision of latest model will be picked
+  # TODO: should we have online_store flag on device_revisions table?
+  #       device_revisions can also have various price values associated to them
+  #
+  def latest_model_revision_name
+    name = device_type
+    if device_models.length > 0
+      last_model = device_models.last
+      name += " #{last_model.part_number}"
+      if last_model.device_revisions.length > 0
+        name += " #{last_model.device_revisions.last.revision}"
+      end
+    end
+  end
 end
---
+++ db/migrate/20100201113711_device_types_online_store_flag.rb
@@ -0,0 +1,22 @@
+class DeviceTypesOnlineStoreFlag < ActiveRecord::Migration
+  def self.up
+    # new column to identify if this product apears on online store.
+    # TODO: should this be decided in device_revision instead?
+    #
+    add_column :device_types, :online_store, :boolean
+    
+    # migrate existing data for specific names only
+    #
+    ["Chest Strap", "Halo Complete", "Belt Clip"].each do |data|
+      device = DeviceType.find_by_device_type(data)
+      unless device.blank?
+        device.online_store = true
+        device.save
+      end
+    end
+  end
+
+  def self.down
+    remove_column :device_types, :online_store
+  end
+end
