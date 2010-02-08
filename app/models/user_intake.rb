dff24396f726d607d79d3152b4095f2aa156f205
Chirag Shah <chirag.sweng@gmail.com>
2010-02-09 01:20:05 +0530

    refs #2413 set created by user name instead of user id

--- app/models/user_intake.rb
+++ app/models/user_intake.rb
@@ -1,4 +1,9 @@
 class UserIntake < ActiveRecord::Base
 	has_and_belongs_to_many :users
 	validates_numericality_of :order_id
+	
+  def created_by_user_name
+  	User.find(self.created_by).name
+  end
+
 end
