4b399481b3194f916ef68dc99e50257d0a6b80b5
Chirag Patel <chirag@halomonitoring.com>
2010-03-03 09:52:52 -0600

    refs #2580 step to check if email is properly delivered

---
+++ features/step_definitions/user_mailer_steps.rb
@@ -0,0 +1,9 @@
+# user mailer specific steps
+#
+
+# Given
+#And email to "cuc_senior@chirag.name" with subject "Please read before your installation" should be queued
+
+Then /^email to "([^\"]*)" with subject "([^\"]*)" should be queued$/ do |email, subject|
+  pending # express the regexp above with the code you wish you had
+end
