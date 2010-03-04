9de0f17a926b4f11dbf77987808a0f331c719e2b
ramonrails <ramonrails@gmail.com>
2010-03-05 03:37:43 +0530

    refs #2548. in progress...

--- app/controllers/dial_up_statuses_controller.rb
+++ app/controllers/dial_up_statuses_controller.rb
@@ -1,25 +1,50 @@
 class DialUpStatusesController < RestfulAuthController
-layout 'application'
-session :on,:except => :create
-def index
-	conditions = "1=1"
-	conditions += " and device_id = #{params[:device_id]}" if params[:device_id] and params[:device_id] != ""
-	conditions += " and dialup_type = '#{params[:dialup_type]}'" if params[:dialup_type] && params[:dialup_type] != 'Select Type'
-    @dial_up_statuses = DialUpStatus.paginate :page => params[:page],:conditions => conditions,:order => 'created_at desc',:per_page => 50
-end
+  layout 'application'
+  session :on,:except => :create
+  
+  def index
+    # WARNING: Not tested
+    # 1. using named scopes to filter the data. Paginate will just do what it says
+    # 2. 'Select Type' also get covered because no matching record will be found anyways
+    # FIXME: extra query fired here. Can be DRYed
+    #  This logic is causing one database search every time. Needs conditional code for speed optimization.
+    @dial_up_statuses = \
+      DialUpStatus.by_dialup_type(params[:dialup_type]).by_device_id(params[:device_id]).paginate \
+      :page => params[:page], \
+      :order => 'created_at desc', \
+      :per_page => 50
+    # conditions = "1=1"
+    # conditions += " and device_id = #{params[:device_id]}" \
+    #   if params[:device_id] and params[:device_id] != ""
+    #     
+    # conditions += " and dialup_type = '#{params[:dialup_type]}'" \
+    #   if params[:dialup_type] && params[:dialup_type] != 'Select Type'
+    #     
+    # @dial_up_statuses = DialUpStatus.paginate :page => params[:page], \
+    #                                           :conditions => conditions, \
+    #                                           :order => 'created_at desc', \
+    #                                           :per_page => 50
+  end
 
-def last_successful
-	if params[:device_id] and params[:device_id] != ""
-      @dial_up_last_successfuls = DialUpLastSuccessful.paginate :page => params[:page],:conditions => ["device_id = ?",params[:device_id]],:order   => 'created_at desc',:per_page => 20
-	else
-	  @dial_up_last_successfuls = DialUpLastSuccessful.paginate :page => params[:page],:order   => 'created_at desc',:per_page => 20
-    end
-end
+  def last_successful
+    # WARNING: Not tested
+    @dial_up_last_successfuls = DialUpLastSuccessful.by_device_id(params[:device_id]).paginate \
+      :page => params[:page], \
+      :order   => 'created_at desc', \
+      :per_page => 20
 
-def create
-	request = params[:dial_up_status]
-	DialUpStatus.process_xml_hash(request)
-	render :nothing => true
-end
+    # if params[:device_id] and params[:device_id] != ""
+    #   @dial_up_last_successfuls = DialUpLastSuccessful.paginate :page => params[:page],:conditions => ["device_id = ?",params[:device_id]],:order   => 'created_at desc',:per_page => 20
+    # else
+    #   @dial_up_last_successfuls = DialUpLastSuccessful.paginate :page => params[:page],:order   => 'created_at desc',:per_page => 20
+    # end
+  end
 
+  # new data posted from user-agent
+  #
+  def create
+    request = params[:dial_up_status]
+    DialUpStatus.process_xml_hash(request)
+    render :nothing => true
+  end
 end
--- app/models/dial_up_last_successful.rb
+++ app/models/dial_up_last_successful.rb
@@ -1,2 +1,13 @@
 class DialUpLastSuccessful < ActiveRecord::Base
+  # filters, scopes
+  #
+  # dynamically generated named scope
+  #  by_dialup_type([...]), by_device_id([...]), ...
+  # To degine more similar scopes, just add the column name symbl to the array
+  [:device_id].each do |which|
+    named_scope "by_#{which}".to_sym, lambda { |*args|
+      var = (args.blank? ? nil : args.flatten.first)
+      { :conditions => (var.blank? ? {} : {which => var}) }
+    }    
+  end
 end
