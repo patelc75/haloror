1db928d981279d13ea26cf67185850d3a9edb017
ramonrails <ramonrails@gmail.com>
2010-03-04 21:05:24 +0530

    refs #2580. email can be out of string

--- features/step_definitions/user_mailer_steps.rb
+++ features/step_definitions/user_mailer_steps.rb
@@ -5,5 +5,5 @@
 #And email to "cuc_senior@chirag.name" with subject "Please read before your installation" should be queued
 
 Then /^email to "([^\"]*)" with subject "([^\"]*)" should be sent for delivery$/ do |email, subject|
-  assert !Email.count(:conditions => {:to => "#{email}", :subject => subject}).blank?, "Subject not found"
+  assert !Email.count(:conditions => {:to => email, :subject => subject}).blank?, "Subject not found"
 end
