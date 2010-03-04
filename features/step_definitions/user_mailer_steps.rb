f16f25bb85a4b98989349ee7154ce84ba67ab74f
ramonrails <ramonrails@gmail.com>
2010-03-04 20:57:20 +0530

    refs #2580. asset email dispatch

--- features/step_definitions/user_mailer_steps.rb
+++ features/step_definitions/user_mailer_steps.rb
@@ -5,8 +5,7 @@
 #And email to "cuc_senior@chirag.name" with subject "Please read before your installation" should be queued
 
 Then /^email to "([^\"]*)" with subject "([^\"]*)" should be sent for delivery$/ do |email, subject|
-  emails = Email.find(:all, :conditions => '"to" = ' + "'#{email}'")
-  emails.each do |e|
-    found = e.mail.include?(subject)
+  Email.all(:conditions => '"to" = ' + "'#{email}'").each do |message|
+    assert message.mail.include?(subject), "Email not found ..."
   end
 end
