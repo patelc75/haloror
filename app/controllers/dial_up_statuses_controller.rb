6b8ad895f8e871d3ae5aacc8b4fc31fe26bd77c0
Chirag Patel <chirag@halomonitoring.com>
2010-03-05 09:18:32 -0600

    merge master-2548-dialup-status-bundle into prod

--- app/controllers/dial_up_statuses_controller.rb
+++ app/controllers/dial_up_statuses_controller.rb
@@ -1,47 +1,50 @@
 class DialUpStatusesController < RestfulAuthController
-layout 'application'
-session :on,:except => :create
-def index
-	conditions = "1=1"
-	conditions += " and device_id = #{params[:device_id]}" if params[:device_id] and params[:device_id] != ""
-	conditions += " and dialup_type = '#{params[:dialup_type]}'" if params[:dialup_type] && params[:dialup_type] != 'Select Type'
-    @dial_up_statuses = DialUpStatus.paginate :page => params[:page],:conditions => conditions,:order => 'created_at desc',:per_page => 50
-end
+  layout 'application'
+  session :on,:except => :create
+  
+  def index
+    # WARNING: Not tested
+    # 1. using named scopes to filter the data. Paginate will just do what it says
+    # 2. 'Select Type' also get covered because no matching record will be found anyways
+    # FIXME: extra query fired here. Can be DRYed
+    #  This logic is causing one database search every time. Needs conditional code for speed optimization.
+    @dial_up_statuses = \
+      DialUpStatus.by_dialup_type(params[:dialup_type]).by_device_id(params[:device_id]).paginate \
+      :page => params[:page], \
+      :order => 'created_at desc', \
+      :per_page => 50
+    # conditions = "1=1"
+    # conditions += " and device_id = #{params[:device_id]}" \
+    #   if params[:device_id] and params[:device_id] != ""
+    #     
+    # conditions += " and dialup_type = '#{params[:dialup_type]}'" \
+    #   if params[:dialup_type] && params[:dialup_type] != 'Select Type'
+    #     
+    # @dial_up_statuses = DialUpStatus.paginate :page => params[:page], \
+    #                                           :conditions => conditions, \
+    #                                           :order => 'created_at desc', \
+    #                                           :per_page => 50
+  end
 
-def last_successful
-	if params[:device_id] and params[:device_id] != ""
-      @dial_up_last_successfuls = DialUpLastSuccessful.paginate :page => params[:page],:conditions => ["device_id = ?",params[:device_id]],:order   => 'created_at desc',:per_page => 20
-	else
-	  @dial_up_last_successfuls = DialUpLastSuccessful.paginate :page => params[:page],:order   => 'created_at desc',:per_page => 20
-    end
-end
+  def last_successful
+    # WARNING: Not tested
+    @dial_up_last_successfuls = DialUpLastSuccessful.by_device_id(params[:device_id]).paginate \
+      :page => params[:page], \
+      :order   => 'created_at desc', \
+      :per_page => 20
 
-def create
-	request = params[:dial_up_status]
-	
-	#Primary Number
-	DialUpStatus.create(:phone_number => request[:number],:status => request[:status],:device_id => request[:device_id],:configured => request[:configured],:num_failures => request[:num_failures],:consecutive_fails => request[:consecutive_fails],:ever_connected => request[:ever_connected],:dialup_type => 'Local')
-	
-	#Local Alternative Number
-	DialUpStatus.create(:phone_number => request[:alt_number],:status => request[:alt_status],:device_id => request[:device_id],:configured => request[:alt_configured],:num_failures => request[:alt_num_failures],:consecutive_fails => request[:alt_consecutive_fails],:ever_connected => request[:alt_ever_connected],:dialup_type => 'Local')
-	
-	#Global Primary Number
-	DialUpStatus.create(:phone_number => request[:global_prim_number],
-	:status => request[:global_prim_status],
-	:device_id => request[:device_id],
-	:configured => request[:global_prim_configured],
-	:num_failures => request[:global_prim_num_failures],
-	:consecutive_fails => request[:global_prim_consecutive_fails],
-	:ever_connected => request[:global_prim_ever_connected],
-	:dialup_type => 'Global')
-	
-	#Global Alternative Number
-	DialUpStatus.create(:phone_number => request[:global_alt_number],:status => request[:global_alt_status],:device_id => request[:device_id],:configured => request[:global_alt_configured],:num_failures => request[:global_alt_num_failures],:consecutive_fails => request[:global_alt_consecutive_fails],:ever_connected => request[:global_alt_ever_connected],:dialup_type => 'Global')
-	
-	#Last Successful Number
-	DialUpLastSuccessful.create(:device_id => request[:device_id],:last_successful_number => request[:last_successful_number],:last_successful_username => request[:last_successful_username],:last_successful_password => request[:last_successful_password])
-	
-	render :nothing => true
-end
+    # if params[:device_id] and params[:device_id] != ""
+    #   @dial_up_last_successfuls = DialUpLastSuccessful.paginate :page => params[:page],:conditions => ["device_id = ?",params[:device_id]],:order   => 'created_at desc',:per_page => 20
+    # else
+    #   @dial_up_last_successfuls = DialUpLastSuccessful.paginate :page => params[:page],:order   => 'created_at desc',:per_page => 20
+    # end
+  end
 
+  # new data posted from user-agent
+  #
+  def create
+    request = params[:dial_up_status]
+    DialUpStatus.process_xml_hash(request)
+    render :nothing => true
+  end
 end
--- app/helpers/dial_up_statuses_helper.rb
+++ app/helpers/dial_up_statuses_helper.rb
@@ -1,2 +1,34 @@
 module DialUpStatusesHelper
+
+  # split the hash into multiple hashes. it can be saved simply by Model.create(hash) then
+  #
+  def split_hashes_for_dialups(hash)
+    prime = alternate = global_prime = global_alternate = last_successful = {}
+    #
+    { "prime"            => "",
+      "alternate"        => "alt_",
+      "global_prime"     => "global_prim_",
+      "global_alternate" => "global_alt_"
+    }.each do |key, value|
+      [:status, :device_id, :configured, :num_failures, :consecutive_fails, :ever_connected].each do |hash_key|
+        eval("#{key}[:#{hash_key}] = #{value}#{hash_key}")
+      end
+      eval("#{key}[:phone_number] = hash[:#{value}number]") # similar pattern for all
+      eval("#{key}[:device_id] = hash[:device_id]") # same for all
+      # TODO: pending
+    end
+    #
+    # dialup_type
+    [prime, alternate].each {|which| which[:dialup_type] = 'Local'}
+    [global_prime, global_alternate].each {|which| which[:dialup_type] = 'Global'}
+    #
+    # last successful
+    last_successful[:device_id]               = hash[:device_id]
+    last_successful[:last_successful_number]  = hash[:last_successful_number]
+    last_successful[:username]                = hash[:last_successful_username]
+    last_successful[:password]                = hash[:last_successful_password]
+    #
+    # get the hashes back to calling method
+    return prime, alternate, global_prime, global_alternate, last_successful
+  end
 end
