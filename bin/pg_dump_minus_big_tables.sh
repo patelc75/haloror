ff031fd2031ed9514aae420121457731432ef655
Chirag Patel <chirag@halomonitoring.com>
2010-03-17 17:08:05 -0500

    update pg_dump to add more tables to ignore

--- bin/pg_dump_minus_big_tables.sh
+++ bin/pg_dump_minus_big_tables.sh
@@ -1,2 +1,2 @@
 FILENAME=$1
-pg_dump -h localhost -p 5432 -U postgres -T vitals -T batteries -T skin_temps -T steps -T oscope_msgs -T mgmt_queries -T mgmt_cmds -T mgmt_responses -T halo_debug_msgs -T points -F c -b -v -f "$FILENAME`eval date +%Y%m%d`" haloror
+pg_dump -h localhost -p 5432 -U postgres -T battery_reminders -T battery_criticals -T events -T vitals -T batteries -T skin_temps -T steps -T oscope_msgs -T mgmt_queries -T mgmt_cmds -T mgmt_responses -T halo_debug_msgs -T points -F c -b -v -f "$FILENAME`eval date +%Y%m%d`" haloror
