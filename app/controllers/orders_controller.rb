b32cc2c1cff45e71f20d104edc3d3b0032046b7d
ramonrails <ramonrails@gmail.com>
2010-04-13 03:42:35 +0530

    refs #2764. online coupon code feature scenario 11 fixed

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -54,7 +54,7 @@ class OrdersController < ApplicationController
     else # store mode
       # back button needs this
       @same_address = (session[:order].blank? ? "checked" : (session[:order][:bill_address_same] || @order.ship_and_bill_address_same))
-      @order ||= (session[:order].blank? ? Order.new : Order.new(session[:order]))
+      @order = (session[:order].blank? ? Order.new(:coupon_code => params[:coupon_code]) : Order.new(session[:order]))
     end
     
     respond_to do |format|
