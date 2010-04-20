29c99ff305b721d58cbd31ed11e7f64d034fe1cb
Chirag Shah <chirag.sweng@gmail.com>
2010-04-21 01:46:37 +0530

    refs #2808 set purged logs on created at desc order

--- app/controllers/purged_logs_controller.rb
+++ app/controllers/purged_logs_controller.rb
@@ -1,7 +1,7 @@
 class PurgedLogsController < ApplicationController
 
   def index
-  	@purged_logs = PurgedLog.paginate :page => params[:page], :per_page => 20, :order => 'created_at'
+  	@purged_logs = PurgedLog.paginate :page => params[:page], :per_page => 20, :order => 'created_at DESC'
   end
 
 end
