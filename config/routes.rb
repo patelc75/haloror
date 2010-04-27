cf1d92c1b48c55051ddd3824edd3c6bc1ecdd266
Chirag Patel <chirag@halomonitoring.com>
2010-04-26 22:40:02 -0500

    refs #2832 static html files for Senior Helpers storyboard

--- config/routes.rb
+++ config/routes.rb
@@ -104,7 +104,9 @@ ActionController::Routing::Routes.draw do |map|
   map.user_intake_form '/user/user_intake_form',:controller => 'users',:action => 'user_intake_form'
   map.user_intake_post '/user/user_intake_form',:controller => 'users',:action => 'user_intake_form', :method => :post
   map.order '/order/:coupon_code', :controller => 'orders', :action => 'new', :coupon_code => ''
-  map.alert '/alert', :controller => 'alerts', :action => "alert"
+  map.alert '/alert', :controller => 'alerts', :action => "alert"      
+  map.activity '/activity', :controller => 'sandbox', :action => "list-view"
+  
   
   #map.resend '/resend/:id', :controller => 'installs', :action => 'resend'
     # 
