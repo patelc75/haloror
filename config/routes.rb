08487fc96b72806f8db997f1762423b4228e5733
Chirag Patel <chirag@halomonitoring.com>
2010-05-18 16:45:13 -0400

    refs #2938 rename senior helpers demo to dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -107,7 +107,7 @@ ActionController::Routing::Routes.draw do |map|
   map.user_intake_post '/user/user_intake_form',:controller => 'users',:action => 'user_intake_form', :method => :post
   map.order '/order/:coupon_code', :controller => 'orders', :action => 'new', :coupon_code => ''
   map.alert '/alert', :controller => 'alerts', :action => "alert"      
-  map.activity '/senior_helpers', :controller => 'sandbox', :action => "senior_helpers"
+  map.activity '/dashboard', :controller => 'sandbox', :action => "dashboard"
   
   
   #map.resend '/resend/:id', :controller => 'installs', :action => 'resend'
