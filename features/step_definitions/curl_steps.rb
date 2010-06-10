4aaa663383603f7d0a76f36c0b504d84961e32c8
ramonrails <ramonrails@gmail.com>
2010-06-10 23:45:45 +0530

    refs #3027. curl steps optimized.

--- features/step_definitions/curl_steps.rb
+++ features/step_definitions/curl_steps.rb
@@ -2,17 +2,16 @@
 When /^I post the following for user "([^\"]*)":$/ do |user_login, table|
   # fetch values and keys from the table
   options = {}
-  table.raw.each do |name, value|
-    options[name.gsub(/ /,'_').downcase.singularize.to_sym] = value
-  end
+  table.raw.each {|name, value| options[name.gsub(/ /,'_').downcase.singularize.to_sym] = value }
 
   user = User.find_by_login(user_login)
   user.should_not be_blank
 
+  # load data for POST
   xml_string = File.read(File.join(Rails.root, "spec", "data", "curl", options[:file_name]))
   xml_string.should_not be_blank
   xml_string.gsub!("meta_user_id", user.id.to_s) # replace user ID with our user
-
   path = "#{options[:path]}?gateway_id=0&auth=#{options[:auth_key]}"
-  post( path, xml_string, {"Content-type" => "text/xml"}) # post XML content
+  
+  post( path, xml_string, {"Content-type" => "text/xml"}) # POST XML content
 end
