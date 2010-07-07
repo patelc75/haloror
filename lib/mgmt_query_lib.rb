81967bbc47a6acc75e0307191c35a2f75f099749
ramonrails <ramonrails@gmail.com>
2010-07-08 01:26:57 +0530

    refs #3168. headers added to text output

--- lib/mgmt_query_lib.rb
+++ lib/mgmt_query_lib.rb
@@ -6,6 +6,6 @@ module MgmtQueryLib
       timespan = row.time_span_since_last
       "#{row.timestamp_device} | #{row.timestamp_server} | #{row.delay} | #{timespan[0]} | #{timespan[1]}"
       # puts "#{row.timestamp_device.to_s(:rfc822).ljust(25)} | #{row.timestamp_server.to_s(:rfc822).ljust(25)} | #{row.delay.to_s.ljust(25)} | #{timespan[0].ljust(50)} | #{timespan[1].ljust(50)}"
-    end
+    end.insert(0, ["Timestamp Device | Timestamp Server | Delay device/server | Delay device from last | Delay server from last"])
   end
 end
