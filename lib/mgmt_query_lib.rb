4224bc77fed0dea3c5c2a1c1b40d952388810169
ramonrails <ramonrails@gmail.com>
2010-07-08 19:29:18 +0530

    refs #3168. code was all ok. the data actually has no difference between the selected row and its preceding one

--- lib/mgmt_query_lib.rb
+++ lib/mgmt_query_lib.rb
@@ -1,11 +1,11 @@
 class MgmtQueryLib
   include ActionView::Helpers::DateHelper
-  
-  def self.sql_like_output( device_id)
-    MgmtQuery.find_all_by_device_id( device_id).collect do |row|
-      timespan = row.time_span_since_last
-      "#{row.timestamp_device} | #{row.timestamp_server} | #{timespan}"
-      # puts "#{row.timestamp_device.to_s(:rfc822).ljust(25)} | #{row.timestamp_server.to_s(:rfc822).ljust(25)} | #{row.delay.to_s.ljust(25)} | #{timespan[0].ljust(50)} | #{timespan[1].ljust(50)}"
-    end.insert(0, ["Timestamp Device                | Timestamp Server                | Delay server from last"])
+
+  def self.debug( device_id)
+    puts "Timestamp Device                | Timestamp Server                | Last Timestamp Server           | Delay server from last"
+    MgmtQuery.find_all_by_device_id( device_id, :order => "timestamp_server DESC").each do |mgmt|
+      last = MgmtQuery.find_by_id( mgmt.id-1)
+      puts "#{mgmt.timestamp_device} | #{mgmt.id} #{mgmt.timestamp_server} | #{last.id unless last.blank?} #{last.timestamp_server unless last.blank?} | #{mgmt.time_span_since_last}"
+    end
   end
 end
