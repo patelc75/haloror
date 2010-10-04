b6a64cd4a5f0b5fa0704d765857a0f29ed400bcd
Chirag Patel <chirag@chirag.name>
2010-10-04 15:16:57 -0500

    updated pg_dump script to leave out more big tables

--- bin/pg_dump_minus_big_tables.sh
+++ bin/pg_dump_minus_big_tables.sh
@@ -1,2 +1,2 @@
 FILENAME=$1
-pg_dump -h localhost -p 5432 -U postgres -T battery_reminders -T battery_criticals -T vitals -T batteries -T skin_temps -T steps -T oscope_msgs -T mgmt_queries -T mgmt_cmds -T mgmt_responses -T halo_debug_msgs -T points -F c -b -v -f "$FILENAME`hostname`-`eval date +%m-%d-%Y`(`eval date +%H:%M:%S%Z`).sql" haloror
+pg_dump -h localhost -p 5432 -U postgres -T battery_reminders -T battery_criticals -T battery_pluggeds -T battery_unpluggeds -T events -T vitals -T batteries -T skin_temps -T steps -T oscope_msgs -T mgmt_queries -T mgmt_cmds -T mgmt_responses -T halo_debug_msgs -T points -F c -b -v -f "$FILENAME`hostname`-`eval date +%m-%d-%Y`(`eval date +%H:%M:%S%Z`).sql" haloror
