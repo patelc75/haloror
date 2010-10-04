0b0724ddccca0c03c72c3ae026595bb277caaf28
Chirag Patel <chirag@chirag.name>
2010-10-04 15:43:57 -0500

    updated pg_dump schema only script to add more tables

--- bin/pg_dump_schema_only.sh
+++ bin/pg_dump_schema_only.sh
@@ -1,2 +1,2 @@
 FILENAME=$1
-pg_dump -h localhost -p 5432 -U postgres --schema-only -t battery_reminders -t battery_criticals -t vitals -t batteries -t skin_temps -t steps -t oscope_msgs -t mgmt_queries -t mgmt_cmds -t mgmt_responses -t halo_debug_msgs -t points -F c -b -v -f "$FILENAME-`eval date +%Y-%m-%d`.sql" haloror
+pg_dump -h localhost -p 5432 -U postgres --schema-only -t battery_reminders -t battery_criticals -t battery_pluggeds -t battery_unpluggeds -t events -t vitals -t batteries -t skin_temps -t steps -t oscope_msgs -t mgmt_queries -t mgmt_cmds -t mgmt_responses -t halo_debug_msgs -t points -F c -b -v -f "$FILENAME-`eval date +%Y-%m-%d`.sql" haloror
