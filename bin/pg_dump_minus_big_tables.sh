edf13001d0e1c475186b448a2c8fdaaea54cc8b5
Chirag Patel <chirag@chirag.name>
2010-10-11 22:12:21 -0500

    Pg shell script updates

--- bin/pg_dump_minus_big_tables.sh
+++ bin/pg_dump_minus_big_tables.sh
@@ -1,2 +1,3 @@
 FILENAME=$1
-pg_dump -h localhost -p 5432 -U postgres -T battery_reminders -T battery_criticals -T battery_pluggeds -T battery_unpluggeds -T events -T vitals -T batteries -T skin_temps -T steps -T oscope_msgs -T mgmt_queries -T mgmt_cmds -T mgmt_responses -T halo_debug_msgs -T points -F c -b -v -f "$FILENAME`hostname`-`eval date +%m-%d-%Y`(`eval date +%H:%M:%S%Z`).sql" haloror
+pg_dump -h localhost -p 5432 -U postgres -T battery_reminders -T battery_criticals -T battery_pluggeds -T battery_unpluggeds -T events -T vitals -T batteries -T skin_temps -T steps -T oscope_msgs -T mgmt_queries -T mgmt_cmds -T mgmt_responses -T halo_debug_msgs -T points -v -f "$FILENAME`hostname`-`eval date +%m-%d-%Y`(`eval date +%H:%M:%S%Z`).sql" haloror
+#leave out compression switches (-F c -b) when exporting to a different version of DB, use psql to import
--- bin/pg_dump_schema_only.sh
+++ bin/pg_dump_schema_only.sh
@@ -1,2 +1,3 @@
 FILENAME=$1
-pg_dump -h localhost -p 5432 -U postgres --schema-only -t battery_reminders -t battery_criticals -t battery_pluggeds -t battery_unpluggeds -t events -t vitals -t batteries -t skin_temps -t steps -t oscope_msgs -t mgmt_queries -t mgmt_cmds -t mgmt_responses -t halo_debug_msgs -t points -F c -b -v -f "$FILENAME-`eval date +%Y-%m-%d`.sql" haloror
+pg_dump -h localhost -p 5432 -U postgres --schema-only -t battery_reminders -t battery_criticals -t battery_pluggeds -t battery_unpluggeds -t events -t vitals -t batteries -t skin_temps -t steps -t oscope_msgs -t mgmt_queries -t mgmt_cmds -t mgmt_responses -t halo_debug_msgs -t points -F c -b -v -f "$FILENAME`hostname`-`eval date +%m-%d-%Y`(`eval date +%H:%M:%S%Z`).sql" haloror
+#leave out compression switches (-F c -b) when exporting to a different version of DB, use psql to import
