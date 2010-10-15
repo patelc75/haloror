cde38c9f15c8c89cb9bc19449b0a6dc217524f41
Chirag Patel <chirag@chirag.name>
2010-10-15 15:57:27 -0500

    refs #3531 remove BatteryCritical and GwAlarmButtonTimeout from Events list

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,6 +2,10 @@ class EventsController < ApplicationController
   before_filter :authenticate_admin_halouser_caregiver_operator?
   include UtilityHelper
   def user
+    @alert_types = AlertType.types_as_array.sort
+    @alert_types.delete("GwAlarmButtonTimeout")  
+    @alert_types.delete("BatteryCritical")      
+    
     @user = User.find(params[:id])
     groups = @user.group_memberships
     if(@user.id == current_user.id || @current_user.patients.include?(@user) || @current_user.is_super_admin? || @current_user.is_admin_of_any?(groups) || @current_user.is_operator_of_any?(groups)) 
@@ -51,6 +55,6 @@ class EventsController < ApplicationController
       render :layout => 'application'
     else
       redirect_to :action => 'unauthorized', :controller => 'security'
-    end
+    end    
   end
 end
