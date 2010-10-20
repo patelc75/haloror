3cdb03ea7efeafb227d99b245aaf76ea5261e325
ramonrails <ramonrails@gmail.com>
2010-10-21 03:48:46 +0530

    refs #3569. weight_scales view updated to show seniors drop down menu. operator can select the senior for whom to see the stats

--- app/controllers/weight_scales_controller.rb
+++ app/controllers/weight_scales_controller.rb
@@ -1,10 +1,10 @@
 class WeightScalesController < RestfulAuthController
-	include UtilityHelper
+  include UtilityHelper
+  layout 'application' # render application layout for all actions here
 
   def index
-  	#@weight_scales = WeightScale.paginate (:all,:page => params[:page])
-  	@weight_scales = WeightScale.find(:all,:conditions => ["user_id = ? ",params[:id]], :order => "timestamp DESC")
-  	render :layout => 'application'
+    @seniors = current_user.is_caregiver_of_what.compact
+    @senior = User.find_by_id( params[:id])
+    @weight_scales = WeightScale.all( :conditions => { :user_id => params[:id] }, :order => "timestamp DESC").paginate( :page => params[:page], :per_page => 10)
   end
-
 end
--- app/models/weight_scale.rb
+++ app/models/weight_scale.rb
@@ -1,4 +1,10 @@
 class WeightScale < Vital
-	belongs_to :user
-	set_table_name "weight_scales"	
+  belongs_to :user
+  set_table_name "weight_scales"
+
+  # DRYed code from view
+  #
+  def user_summary
+    user.blank? ? '' : ["#{user.name} (#{user.id})", (user.profile.blank? ? nil : "all times are in #{user.profile.time_zone} timezone")].compact.join(' - ')
+  end
 end
