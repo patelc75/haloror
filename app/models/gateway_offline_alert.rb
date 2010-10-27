a8726c7a7d0ff8e8fa67293b52791464324cacde
Chirag Patel <chirag@chirag.name>
2010-10-27 14:03:08 -0500

    refs #2849 add user name in the emails and support if no user_id in various models

--- app/models/gateway_offline_alert.rb
+++ app/models/gateway_offline_alert.rb
@@ -6,8 +6,7 @@
 class GatewayOfflineAlert < ActiveRecord::Base 
   include Priority
   belongs_to :device
-  belongs_to :user  
-                          
+                  
   # If we have detected an outage after our retry period, notify the
   # user immediately
   def after_save
@@ -15,7 +14,6 @@ class GatewayOfflineAlert < ActiveRecord::Base
       logger.debug("[GatewayOfflineAlert] Detected an outage for device #{device_id}. Alert ID #{id}. Number attempts: #{number_attempts}")
       
       device.users.each do |user|    
-        self.user_id = user.id         
         Event.create_event(user.id, GatewayOfflineAlert.class_name, id, created_at)        
         CriticalMailer.deliver_non_critical_caregiver_email(self, user)
         CriticalMailer.deliver_non_critical_caregiver_text(self, user)        
@@ -24,13 +22,11 @@ class GatewayOfflineAlert < ActiveRecord::Base
   end
   
   def to_s
-    user.nil? ? user_info = "" : user_info = " for #{user.name} (#{user.id})"    
     "Gateway back online" + user_info
   end
   
   def email_body
-    user.nil? ? user_info = "" : user_info = " for #{user.name} (#{user.id})"
-    "Hello,\n\nWe have detected that the gateway went offline" + user_info +
+    "Hello,\n\nWe have detected that the gateway went offline" +
       "\n\n- Halo Staff"
   end
 end 
--- app/models/gateway_online_alert.rb
+++ app/models/gateway_online_alert.rb
@@ -3,11 +3,10 @@
 
 class GatewayOnlineAlert < ActiveRecord::Base
   belongs_to :device
-  belongs_to :user  
   include Priority
+  
   def after_save
     device.users.each do |user|
-      self.user_id = user.id
       Event.create_event(user.id, GatewayOnlineAlert.class_name, id, created_at)
       CriticalMailer.deliver_non_critical_caregiver_email(self, user)  
       CriticalMailer.deliver_non_critical_caregiver_text(self, user)      
@@ -15,13 +14,11 @@ class GatewayOnlineAlert < ActiveRecord::Base
   end
   
   def to_s
-    user.nil? ? user_info = "" : user_info = " for #{user.name} (#{user.id})"
-    "Gateway back online" + user_info
+    "Gateway back online"
   end 
   
   def email_body
-    user.nil? ? user_info = "" : user_info = " for #{user.name} (#{user.id})"
-    "Hello,\n\nWe have detected that the gateway is back online" + user_info +
+    "Hello,\n\nWe have detected that the gateway is back online" +
       "\n\n- Halo Staff"
   end
 end 
