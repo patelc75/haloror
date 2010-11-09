60373ab1456099124b634c8da03cd45e30e4b98d
Chirag Patel <chirag@chirag.name>
2010-11-08 19:37:15 -0600

    reformated user stats page

--- app/controllers/reporting_controller.rb
+++ app/controllers/reporting_controller.rb
@@ -126,7 +126,7 @@ class ReportingController < ApplicationController
     # debugger
     #
     # paginate the list of users
-    @users = @users.uniq.paginate :page => params[:page], :per_page => REPORTING_USERS_PER_PAGE
+    @users = @users.uniq.paginate :page => params[:page], :per_page => REPORTING_USERS_PER_PAGE, :order => 'id'
 
     # @users.each do |user|
     #   if user
@@ -149,7 +149,7 @@ class ReportingController < ApplicationController
   # https://redmine.corp.halomonitor.com/issues/3571
   def user_stats
     @groups = (current_user.is_super_admin? ? Group.all(:order => "name") : current_user.group_memberships)
-    @group = (@group_name.blank? ? nil : Group.find_by_name( params[:group_name] ))
+    @group = (@group_name.blank? ? nil : Group.find_by_name( params[:group_name] )) 
   end
   
   def purge_data
