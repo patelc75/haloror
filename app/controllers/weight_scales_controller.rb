0b0f3aebd2ac88d6b475dfaa5dceb1f210c4ae5b
ramonrails <ramonrails@gmail.com>
2010-11-11 03:31:24 +0530

    refs #3569. weight scales index fixed.

--- app/controllers/weight_scales_controller.rb
+++ app/controllers/weight_scales_controller.rb
@@ -3,8 +3,10 @@ class WeightScalesController < RestfulAuthController
   layout 'application' # render application layout for all actions here
 
   def index
+    _id = params[:id]
+    _options = ( _id.blank? ? {} : { :user_id => _id })
     @seniors = current_user.is_caregiver_of_what.compact # For user drop down on form
-    @senior = User.find_by_id( params[:id])
-    @weight_scales = WeightScale.all( :conditions => { :user_id => params[:id] }, :order => "timestamp DESC").paginate( :page => params[:page], :per_page => 10)
+    @senior = ( _id.blank? ? @seniors.first : User.find( _id) )
+    @weight_scales = WeightScale.all( :conditions => _options, :order => "timestamp DESC").paginate( :page => params[:page], :per_page => 10)
   end
 end
--- app/models/weight_scale.rb
+++ app/models/weight_scale.rb
@@ -7,4 +7,12 @@ class WeightScale < Vital
   def user_summary
     user.blank? ? '' : ["#{user.name} (#{user.id})", (user.profile.blank? ? nil : "all times are in #{user.profile.time_zone} timezone")].compact.join(' - ')
   end
+  
+  def weight_string
+    "#{(weight.to_f/1000.0).round(1)} #{weight_unit}"
+  end
+  
+  def battery_string
+    "#{(battery.to_f/1000.0).round(1)}"
+  end
 end
