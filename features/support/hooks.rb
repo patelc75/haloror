e6dd85ed6a9e7a4133abe4aaa3b66e367a6e1de5
ramonrails <ramonrails@gmail.com>
2010-11-16 03:58:12 +0530

    refs #3704. automation of tests

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,13 +1,31 @@
-# Cause scenarios tagged with @fast to fail if the execution takes longer than 0.5 seconds
 #
+# Cause scenarios tagged with @fast to fail if the execution takes longer than 0.5 seconds
 Around('@fast') do |scenario, block|
   Timeout.timeout(0.5) do
     block.call
   end
 end
 
-# Tell Cucumber to quit after this scenario is done - if it failed.
+#  Mon Nov 15 23:44:21 IST 2010, ramonrails
+#  cucumber hooks
+# Scenario setup
 #
-# After do |scenario|
-#   Cucumber.wants_to_quit = true if scenario.failed?
-# end
+Before do
+  #
+  # Mail traces cleared
+  ActionMailer::Base.deliveries.clear
+  #
+  # start considering data as dirty
+  DatabaseCleaner.start
+end
+
+# Scenario teardown
+#
+After do |scenario|
+  #
+  # Clean all dirty data
+  DatabaseCleaner.clean
+  #
+  # Tell Cucumber to quit after this scenario is done - if it failed.
+  Cucumber.wants_to_quit = true if scenario.failed?
+end
