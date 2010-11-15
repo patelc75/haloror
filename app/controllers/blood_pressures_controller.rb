a92c49f05cb0ff976893bee1b0ee9e0034d9c8f6
ramonrails <ramonrails@gmail.com>
2010-11-15 21:15:26 +0530

    refs #3569. list views for weight scale and blood pressures are showing correctly now. a dropdown list of users is also presented to whom the current user is caregiving

--- app/controllers/blood_pressures_controller.rb
+++ app/controllers/blood_pressures_controller.rb
@@ -1,16 +1,16 @@
 class BloodPressuresController < RestfulAuthController
   include UtilityHelper
+  layout 'application'
 
   def index
     # 
     #  Wed Nov 10 03:03:40 IST 2010, ramonrails
     #   * allow to select user
     #   * paginate the list
+    _id = params[:id]
+    _options = ( _id.blank? ? {} : { :user_id => _id })
     @seniors = current_user.is_caregiver_of_what.compact # For user drop down on form
-    @senior = User.find_by_id( params[:id])
-    @blood_pressures = BloodPressure.all( :conditions => { :user_id => params[:id] }, :order => "timestamp DESC").paginate( :page => params[:page], :per_page => 10)
-    # #@blood_pressure = BloodPressure.paginate (:all,:page => params[:page], :order => "timestamp DESC")
-    # @blood_pressure = BloodPressure.find(:all,:conditions => ["user_id = ? ",params[:id]], :order => "timestamp DESC")
-    render :layout => 'application'
+    @senior = ( _id.blank? ? @seniors.first : User.find( _id) )
+    @blood_pressures = BloodPressure.all( :conditions => _options, :order => "timestamp DESC").paginate( :page => params[:page], :per_page => 10)
   end
 end
--- app/models/blood_pressure.rb
+++ app/models/blood_pressure.rb
@@ -1,4 +1,8 @@
 class BloodPressure < Vital
 	belongs_to :user
 	set_table_name "blood_pressures"
+
+  def user_summary
+    user.blank? ? '' : ["#{user.name} (#{user.id})", (user.profile.blank? ? nil : "all times are in #{user.profile.time_zone} timezone")].compact.join(' - ')
+  end
 end
