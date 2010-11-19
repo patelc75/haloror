f34afa8f3138c258ffbf6f3c78e4b0366fb886d5
Chirag Patel <chirag@chirag.name>
2010-11-19 10:48:19 -0500

    refs #3739 .sql files to help write the orientation_threshold_function

--- bin/sql/function_source_code.sql
+++ bin/sql/function_source_code.sql
@@ -1,7 +1,9 @@
-/* Run with psql -f latest_vitals_trigger_function.sql -d haloror */
+/* change line 8 below for the function you want to see the source code for */
+/* Usage: psql -f latest_vitals_trigger_function.sql -d haloror */
+
 SELECT p.prosrc as "Source code"
 FROM pg_catalog.pg_proc p
      LEFT JOIN pg_catalog.pg_namespace n ON n.oid = p.pronamespace
      LEFT JOIN pg_catalog.pg_language l ON l.oid = p.prolang
-WHERE p.proname ~ '^(latest_vitals_trigger_function)$'
+WHERE p.proname ~ '^(orientation_threshold_function)$'
   AND pg_catalog.pg_function_is_visible(p.oid);
