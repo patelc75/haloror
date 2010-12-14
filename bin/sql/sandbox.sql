af10c34244c2d6d0a3e15af4bc9018d49936ac29
Chirag Patel <chirag@chirag.name>
2010-12-14 15:30:05 -0500

    .sql file updates

--- bin/sql/sandbox.sql
+++ bin/sql/sandbox.sql
@@ -1,11 +1,15 @@
 /* refs #3666 auto-increment safetycare account number */
-select id, first_name, last_name, account_number from profiles where account_number is not null order by id desc;
-update profiles set account_number = '' where account_number !='HM0010';
+select id, first_name, last_name, account_number from profiles where account_number is not null order by account_number desc;
+select id, first_name, last_name, account_number from profiles where account_number = '1125' order by account_number desc;
+update profiles set account_number = '1111' where id in (459, 405, 1085, 711, 1108, 34, 1094, 999, 552, 967, 980, 47, 158);
+update profiles set account_number = NULL where id in (459, 405, 1085, 711, 1108, 34, 1094, 999, 552, 967, 980, 47, 158);
+select id, first_name, last_name, account_number from profiles where id in (11, 459, 405, 1085, 711, 1108, 34, 1094, 999, 552, 967, 980, 47, 158);
 
 /* refs #3740 pull all gateways that are in ethernet mode */
 select * from devices 
 where id in (select device_id from access_mode_statuses where mode = 'ethernet') 
 and  id in (select id from devices where serial_number like 'H2%');	
+
 
 curl -v -k -H "Content-Type: text/xml" -d "<panic><device_id>26</device_id><duration_press>1000</duration_press><gw_timestamp>Mon Dec 25 15:52:55 -0600 2007</gw_timestamp><user_id>827</user_id><timestamp>Mon Dec 25 15:52:55 -0600 2007</timestamp></panic>" "https://sdev.myhalomonitor.com/panics?gateway_id=0&auth=9ad3cad0f0e130653ec377a47289eaf7f22f83edb81e406c7bd7919ea725e024"
 
@@ -31,35 +35,14 @@ select count(*) from users where id in (select user_id from users_by_role('halou
 select* from users_by_role('halouser');
 select user_id from users_by_role('halouser') order by user_id;
 
-
-	  create type role_row AS (
-        role_id integer,   
-  	    role text,
-  	    group_or_user text
-    	); 
-
-
-  	CREATE OR REPLACE FUNCTION roles_by_user_id(integer)
-  	RETURNS setof role_row
-  	AS
-  	$$
-    SELECT roles.id as role_id, roles.name, groups.name
-      from users, roles, roles_users, groups, profiles 
-      where users.id = roles_users.user_id 
-      and roles_users.role_id = roles.id 
-      and roles.authorizable_type = 'Group' 
-      and roles.authorizable_id = groups.id
-      and users.id = profiles.user_id
-      and users.id = $1
-      order by roles.name;   
-  	$$ 
-  	LANGUAGE 'sql' STABLE;    
-
-
-
-  	select * from roles_by_user_id(1);
-  	drop function roles_by_user_id(integer);
+drop function roles_by_user_id(integer);
 drop type role_row;
 
-  	select roles_users.*, roles.name from roles_users, roles where roles_users.role_id = roles.id and user_id = 19;
-  	delete from roles_users where 
+select roles_users.*, roles.name from roles_users, roles where roles_users.role_id = roles.id and user_id = 19;
+
+select * from mgmt_cmds where timestamp > '2010-12-06' and timestamp < '2010-12-07';
+
+select * from mgmt_cmds where device_id = 5877 and cmd_type = 'firmware_upgrade';
+delete from mgmt_cmds where id = 3081009;
+
+select * from events where user_id = 292 order by timestamp desc;
--- bin/sql/user_stats.sql
+++ bin/sql/user_stats.sql
@@ -8,6 +8,7 @@ select demo_mode, count(*) from users group by demo_mode;
 
 /* Individual non-demo users not in an installed state */	
 select id, status from users where demo_mode = false and status is not null and status != 'Installed' order by status;
+select users.id, profiles.first_name, profiles.last_name, users.status, users.demo_mode, users.vip, users.created_at from users, profiles where users.status = 'Installed' and users.id = profiles.user_id order by users.created_at;
 
 /* Individual installed users mapped to a device */ 	
 select distinct users.id, users.status, users.demo_mode from users, devices_users, devices where users.status is not null and users.status = 'Installed' and users.id = devices_users.user_id order by users.status;
@@ -17,3 +18,33 @@ select distinct users.id, users.status, users.demo_mode from users, devices_user
 
 /* "Pg function users_by_role_and_group() to show all halousers for safety_care group for example" */
 select * from users_by_role_and_group('halouser', 'ems');
+
+select user_id, first_name, last_name, role_name, group_name, demo_mode, status from users_by_role('halouser') where status like '%ancelled%' order by demo_mode;
+select distinct user_id, first_name, last_name, role_name, group_name, demo_mode, status from users_by_role('halouser') where demo_mode = true order by user_id;
+select * from roles_by_user_id(1);
+
+select user_id, first_name, last_name, role_name, group_name, demo_mode, status from users_by_role('halouser');
+select count(*) from users_by_role('halouser');
+
+/* Convert a set of user_ids to "Cancelled" */
+select users.id, users.login, users.email, users.status, users.demo_mode, profiles.first_name, profiles.last_name from users, profiles where profiles.user_id = users.id and users.id in (269, 624, 667, 668, 687, 697, 708, 729, 738, 739, 740, 749, 754, 759, 781, 782, 787, 811, 973);
+update users set status = 'Cancelled' where id in (269, 624, 667, 668, 687, 697, 708, 729, 738, 739, 740, 749, 754, 759, 781, 782, 787, 811, 973);
+
+/* Convert a set of user_ids to "Installed" */
+select users.id, users.login, users.email, users.status, users.demo_mode, users.vip, profiles.first_name, profiles.last_name 
+from users
+left outer join profiles on profiles.user_id = users.id 
+where users.id in (404);
+update users set status = 'Installed' where id in (404);
+
+/* Set demo_mode = true for a set of user_ids */
+select users.id, users.login, users.email, users.status, users.demo_mode, profiles.first_name, profiles.last_name from users, profiles where profiles.user_id = users.id and users.id in (163, 1146);
+update users set demo_mode = true where id in (163, 1146);
+
+/* Set vip = true for a set of user_ids */
+select users.id, users.login, users.email, users.status, users.demo_mode, users.vip, profiles.first_name, profiles.last_name 
+from users, profiles 
+where profiles.user_id = users.id and users.id in (1065, 712, 688);
+update users set vip = true where id in (1065, 712, 688);
+
+
