4de3c794941f75b96ddca7791abc9a250ee06288
Chirag Patel <chirag@chirag.name>
2011-01-10 11:19:40 -0600

    update debugging files

--- bin/sql/sandbox.sql
+++ bin/sql/sandbox.sql
@@ -11,7 +11,8 @@ where id in (select device_id from access_mode_statuses where mode = 'ethernet')
 and  id in (select id from devices where serial_number like 'H2%');	
 
 
-curl -v -k -H "Content-Type: text/xml" -d "<panic><device_id>26</device_id><duration_press>1000</duration_press><gw_timestamp>Mon Dec 25 15:52:55 -0600 2007</gw_timestamp><user_id>848</user_id><timestamp>Mon Dec 25 15:52:55 -0600 2007</timestamp></panic>" "https://sdev.myhalomonitor.com:3000/panics?gateway_id=0&auth=9ad3cad0f0e130653ec377a47289eaf7f22f83edb81e406c7bd7919ea725e024"
+
+scurl -v -k -H "Content-Type: text/xml" -d "<panic><device_id>26</device_id><duration_press>1000</duration_press><gw_timestamp>Mon Dec 25 15:52:55 -0600 2007</gw_timestamp><user_id>848</user_id><timestamp>Mon Dec 25 15:52:55 -0600 2007</timestamp></panic>" "https://sdev.myhalomonitor.com:3000/panics?gateway_id=0&auth=9ad3cad0f0e130653ec377a47289eaf7f22f83edb81e406c7bd7919ea725e024"
 curl -v -H "Content-Type: text/xml" -d "<panic><device_id>26</device_id><duration_press>1000</duration_press><gw_timestamp>Mon Dec 25 15:52:55 -0600 2007</gw_timestamp><user_id>848</user_id><timestamp>Mon Dec 25 15:52:55 -0600 2007</timestamp></panic>" "http://sdev.myhalomonitor.com:3000/panics?gateway_id=0&auth=9ad3cad0f0e130653ec377a47289eaf7f22f83edb81e406c7bd7919ea725e024"
 
 drop function users_by_role_and_group(text, text);
@@ -43,7 +44,7 @@ select roles_users.*, roles.name from roles_users, roles where roles_users.role_
 
 select * from mgmt_cmds where timestamp > '2010-12-06' and timestamp < '2010-12-07';
 
-select * from mgmt_cmds where device_id = 5877 and cmd_type = 'firmware_upgrade';
+select * from mgmt_cmds where device_id = 5877 and cmd_type = 'firmware_upgrade';/
 delete from mgmt_cmds where id = 3081009;
 
 select * from events where user_id = 292 order by timestamp desc;
@@ -59,3 +60,17 @@ select * from caregivers_by_user_id(5);
 select id, timestamp, timestamp_call_center, timestamp_server, timestamp_call_center-timestamp_server  as delay from falls where timestamp_call_center-timestamp_server > interval'5 minutes' order by id desc limit 100;
 
 select id, timestamp, timestamp_call_center, timestamp_server, timestamp_call_center-timestamp_server  as delay from panics where ((timestamp_call_center-timestamp_server) > interval'5 minutes') order by id desc limit 100;
+
+select users.id, users.login, profiles.first_name, profiles.last_name from users, profiles where users.id in (229, 230,231,232, 233) and profiles.user_id = users.id;
+
+
+curl -v -k -H "Content-Type: text/xml" -d "<dial_up_status><device_id>5773</device_id><alt_number>2567053101</alt_number><alt_status>success</alt_status><alt_username>halo</alt_username><alt_password>gepitka</alt_password><alt_configured>new</alt_configured><alt_num_failures>0</alt_num_failures><alt_consecutive_fails>0</alt_consecutive_fails><alt_ever_connected>False</alt_ever_connected><number>2562700020</number><status>success</status><username>halopoolplan@earthlink.net</username><password>UKA8aKh5</password><configured>new</configured><num_failures>0</num_failures><consecutive_fails>0</consecutive_fails><ever_connected>True</ever_connected><global_alt_number>18772383816</global_alt_number><global_alt_status>success</global_alt_status><global_alt_username>halo</global_alt_username><global_alt_password>gepitka</global_alt_password><global_alt_configured>new</global_alt_configured><global_alt_num_failures>0</global_alt_num_failures><global_alt_consecutive_fails>0</global_alt_consecutive_fails><global_alt_ever_connected>False</global_alt_ever_connected><global_prim_number>18008537921</global_prim_number><global_prim_status>success</global_prim_status><global_prim_username>halopoolplan@earthlink.net</global_prim_username><global_prim_password>UKA8aKh5</global_prim_password><global_prim_configured>new</global_prim_configured><global_prim_num_failures>0</global_prim_num_failures><global_prim_consecutive_fails>0</global_prim_consecutive_fails><global_prim_ever_connected>False</global_prim_ever_connected><last_successful_number>2562700020</last_successful_number><last_successful_username>halopoolplan@earthlink.net</last_successful_username><last_successful_password>UKA8aKh5</last_successful_password><timestamp>Mon Dec 25 15:52:55 -0600 2007</timestamp><lowest_connect_rate>100</lowest_connect_rate><lowest_connect_timestamp>Mon Dec 25 15:52:55 -0600 2011</lowest_connect_timestamp></dial_up_status>" "https://sdev.myhalomonitor.com/dial_up_statuses?gateway_id=0&auth=9ad3cad0f0e130653ec377a47289eaf7f22f83edb81e406c7bd7919ea725e024" 
+
+select * from dial_up_statuses order by id desc;
+select * from dial_up_last_successfuls order by id desc;
+
+<dial_up_status><device_id>5773</device_id><alt_number>2567053101</alt_number><alt_status>success</alt_status><alt_username>halo</alt_username><alt_password>gepitka</alt_password><alt_configured>new</alt_configured><alt_num_failures>0</alt_num_failures><alt_consecutive_fails>0</alt_consecutive_fails><alt_ever_connected>False</alt_ever_connected><number>2562700020</number><status>success</status><username>halopoolplan@earthlink.net</username><password>UKA8aKh5</password><configured>new</configured><num_failures>0</num_failures><consecutive_fails>0</consecutive_fails><ever_connected>True</ever_connected><global_alt_number>18772383816</global_alt_number><global_alt_status>success</global_alt_status><global_alt_username>halo</global_alt_username><global_alt_password>gepitka</global_alt_password><global_alt_configured>new</global_alt_configured><global_alt_num_failures>0</global_alt_num_failures><global_alt_consecutive_fails>0</global_alt_consecutive_fails><global_alt_ever_connected>False</global_alt_ever_connected><global_prim_number>18008537921</global_prim_number><global_prim_status>success</global_prim_status><global_prim_username>halopoolplan@earthlink.net</global_prim_username><global_prim_password>UKA8aKh5</global_prim_password><global_prim_configured>new</global_prim_configured><global_prim_num_failures>0</global_prim_num_failures><global_prim_consecutive_fails>0</global_prim_consecutive_fails><global_prim_ever_connected>False</global_prim_ever_connected><last_successful_number>2562700020</last_successful_number><last_successful_username>halopoolplan@earthlink.net</last_successful_username><last_successful_password>UKA8aKh5</last_successful_password><timestamp>Thu Jan 06 14:59:22 UTC 2011</timestamp></dial_up_status><lowest_connect_rate>31200</lowest_connect_rate><longest_dial_duration_sec>213</longest_dial_duration_sec>
+
+select id, user_id, timestamp, weight, weight_unit, battery, serial_number from weight_scales where user_id != 0 order by id desc;
+
+
