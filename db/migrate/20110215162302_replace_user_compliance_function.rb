564a0950ba0714c87777100ba47d780ca1866256
Chirag Patel <chirag@chirag.name>
2011-02-15 16:48:49 -0600

    refs #4183 udpate usage_minus_lost_data_gaps_and_battery_charge function to return hours instead of days,hours,mins,secs

--- db/migrate/20110215162302_replace_user_compliance_function.rb
+++ db/migrate/20110215162302_replace_user_compliance_function.rb
@@ -4,7 +4,7 @@ class ReplaceUserComplianceFunction < ActiveRecord::Migration
       DROP FUNCTION user_compliance(p_user_id integer, p_begin_time timestamp with time zone, p_end_time timestamp with time zone, p_lost_data_gap varchar);
        
       CREATE OR REPLACE FUNCTION usage_minus_lost_data_gaps_and_battery_charge(p_user_id integer, p_begin_time timestamp with time zone, p_end_time timestamp with time zone, p_lost_data_gap varchar)
-       RETURNS interval AS
+       RETURNS double precision AS
        $$
      	 declare
      	   row record;
@@ -33,8 +33,9 @@ class ReplaceUserComplianceFunction < ActiveRecord::Migration
      	   RAISE NOTICE 'total period = %', date_trunc('second', p_end_time - p_begin_time); 
      	   usage_minus_lost_data_gaps_and_battery_charge = date_trunc('second', p_end_time - (p_begin_time + battery_charge_duration + lost_data_duration));   
      	   RAISE NOTICE 'total usage = total period - battery_charge_duration - lost_data_duration = %', usage_minus_lost_data_gaps_and_battery_charge;
+         RAISE NOTICE 'total usage = total period - battery_charge_duration - lost_data_duration = % hours', extract('epoch' from usage_minus_lost_data_gaps_and_battery_charge) / 3600;
 
-     	   return usage_minus_lost_data_gaps_and_battery_charge;
+     	   return extract('epoch' from usage_minus_lost_data_gaps_and_battery_charge) / 3600;
      	 end;
        $$ language plpgsql;
 
