2f8a31944766af09ca39d9a9a1ff9666995cb5f1
Chirag Patel <chirag@chirag.name>
2011-02-21 13:27:03 -0600

    updated .sql file

--- bin/sql/battery_charge_periods.sql
+++ bin/sql/battery_charge_periods.sql
@@ -5,17 +5,23 @@ select * from skin_temps;
 
 /* run  the lost_data (vitals) query --------------------------------------------------------------------------------- */
 select * from vitals where user_id = 1 order by timestamp asc limit 1000;
-select * from lost_data_function(1, '2011-01-28', now(), '1 second');
+select * from lost_data_function(1, '2011-02-16', now(), '1 second');
 select *,end_time-begin_time as duration from lost_datas where user_id = 1;
 select sum(end_time-begin_time) from lost_datas where user_id = 1;
 delete from lost_datas;
+insert into vitals (timestamp, user_id) values ('2011-02-16 02:48:38.88439+00', 1);
 
 /* refs #4183 queries for calculate battery periods ------------------------------------------------------------------- */
-DROP TABLE battery_charge_periods;
-select * from events where event_type in ('BatteryPlugged', 'BatteryUnplugged') and user_id = 1 and timestamp > '2011-01-24 00:06:00+00' and timestamp <'2011-01-31 00:06:00+00' order by timestamp asc;
-select * from battery_charge_periods_function(, '2009-10-10', now());
-select sum(duration) from battery_charge_periods;
+select * from events where event_type in ('BatteryPlugged', 'BatteryUnplugged') and user_id = 1;
+select * from battery_charge_periods_function(1, '2009-10-10', now());
+
+select * from events where event_type in ('BatteryPlugged', 'BatteryUnplugged') and user_id = 1 and timestamp >= '2009-10-08 11:27:05+00' and timestamp <= '2009-10-08 13:26:05+00' order by timestamp asc;
+select * from battery_charge_periods_function(1, '2009-10-08 11:26:05+00', '2009-10-08 13:26:05+00');
+
 delete from battery_charge_periods;
+select * from battery_charge_periods where user_id = 1;
+select sum(duration) from battery_charge_periods;
+
 select * from usage_minus_lost_data_gaps_and_battery_charge(261, '2011-02-08', now(), '90 seconds');
 select * from usage_minus_lost_data_gaps_and_battery_charge_multiple(array_agg(select id from users limit 2), '2011-02-08', now(), '90 seconds');
 select array_agg(any (select id from users limit 2));
@@ -111,3 +117,51 @@ SELECT user_id, timestamp, event_type,
 	END
 from events e
 WHERE event_type in ('Fall', 'Panic');
+
+
+
+---------
+
+CREATE OR REPLACE FUNCTION battery_charge_periods_function(p_user_id integer, p_begin_time timestamp with time zone, p_end_time timestamp with time zone)
+  RETURNS void AS
+$BODY$
+      	declare
+      	   row record;
+      	   plugged_timestamp timestamp with time zone;
+      	   unplugged_timestamp timestamp with time zone;
+      	   no_data_flag boolean;
+      	 begin
+      	   no_data_flag = true;  
+      	   for row in (select timestamp, event_type from events where user_id = p_user_id AND timestamp <= p_end_time AND timestamp >= p_begin_time and event_type in ('BatteryPlugged', 'BatteryUnplugged') order by timestamp asc) loop
+	     no_data_flag = false;
+      	     if(row.event_type = 'BatteryPlugged') then
+      	       plugged_timestamp := row.timestamp;
+      	       unplugged_timestamp = NULL;      	       
+      	     else -- 'BatteryUnplugged'
+      	       if (plugged_timestamp is NULL) then --left boundary condition
+      		 insert into battery_charge_periods (user_id, begin_time, end_time, duration) values (p_user_id, p_begin_time, row.timestamp, row.timestamp-p_begin_time); 
+      		 RAISE NOTICE '1 plugged_timestamp = %, unplugged_timestamp = %, row.timestamp = %', plugged_timestamp, unplugged_timestamp, row.timestamp;
+      	       end if;
+      	       if (unplugged_timestamp is NULL and plugged_timestamp is not NULL) then --conditional in case there are 2 unpluggeds in a row
+      		 unplugged_timestamp := row.timestamp;
+      		 insert into battery_charge_periods (user_id, begin_time, end_time, duration) values (p_user_id, plugged_timestamp, unplugged_timestamp, unplugged_timestamp-plugged_timestamp);
+      		 RAISE NOTICE '2 plugged_timestamp = %, unplugged_timestamp = %, row.timestamp = %', plugged_timestamp, unplugged_timestamp, row.timestamp;
+      	       end if;
+      	     end if;
+      	   end loop;
+      	   if(row.event_type = 'BatteryPlugged') then
+      	     insert into battery_charge_periods (user_id, begin_time, end_time, duration) values (p_user_id, row.timestamp, p_end_time, p_end_time-row.timestamp);
+      	     RAISE NOTICE '3 plugged_timestamp = %, unplugged_timestamp = %, row.timestamp = %, row.event_type = %', plugged_timestamp, unplugged_timestamp, row.timestamp, row.event_type;
+      	   end if;
+      	   
+      	   if(no_data_flag true) then   
+      	     row = select timestamp, event_type from events where user_id = p_user_id AND timestamp <p_begin_time and event_type in ('BatteryPlugged', 'BatteryUnplugged') order by timestamp desc limit 1;
+      	     if(row.event_type = 'BatteryPlugged') then
+		insert into battery_charge_periods (user_id, begin_time, end_time, duration) values (p_user_id, p_begin_time, p_end_time, p_end_time-p_begin_time);
+	     end if;
+           end if;   
+      	 end;
+        $BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION battery_charge_periods_function(integer, timestamp with time zone, timestamp with time zone) OWNER TO postgres;
