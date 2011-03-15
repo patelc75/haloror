6c16ea9f60f0c726ccb8bd62175c7cabc7e92a2f
Chirag Patel <chirag@chirag.name>
2011-03-15 12:16:03 -0500

    update billing.sql to include query invoices table -- add group

--- bin/sql/billing.sql
+++ bin/sql/billing.sql
@@ -1,4 +1,4 @@
-/* Ordered by state for jill */
+-- Ordered by state for jill ------------------------------------------------------------------------
       SELECT distinct (users.id) as user_id, profiles.first_name, profiles.last_name, profiles.city, profiles.state, profiles.zipcode, profiles.home_phone, profiles.cell_phone, groups.name, users.status, users.test_mode, users.demo_mode, users.vip, users.created_at
         from users LEFT OUTER JOIN profiles ON users.id = profiles.user_id, roles, roles_users, groups 
         where users.id = roles_users.user_id 
@@ -9,3 +9,14 @@
         and groups.name != 'safety_care'
         and status = 'Installed' and demo_mode != true
         order by profiles.state asc, users.created_at desc;  
+
+
+-- Sort feature in Invoice - by Group, by Installed date, by Termination date -----------------------------
+-- (psql -F ',' -A haloror > (run the query) > \o outputfile.csv > (use mutt to email)---------------------
+select 
+(select group_or_first_name from roles_by_user_id(invoices.user_id) where role = 'halouser' and group_or_first_name != 'safety_care' limit 1) as group,
+profiles.first_name, profiles.last_name,
+invoices.*
+from invoices, users, profiles
+where users.id = invoices.user_id
+and profiles.user_id = users.id;
