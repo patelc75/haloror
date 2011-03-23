18c42c771f2968a02739c4142fef49229d8d28c9
Chirag Patel <chirag@chirag.name>
2011-03-22 23:57:34 -0500

    rename migration AddColumnToOrders to AddColumnToOrders2

--- db/migrate/20110314211701_add_column_to_orders.rb
+++
@@ -1,9 +0,0 @@
-class AddColumnToOrders2 < ActiveRecord::Migration
-  def self.up
-    add_column :orders, :shipping_option_id, :integer
-  end
-
-  def self.down
-    remove_column :orders, :shipping_option_id
-  end
-end
