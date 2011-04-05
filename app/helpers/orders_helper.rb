31f9b89e85f7ac7a047bb1d06f8b1c65390e13ce
ramonrails <ramonrails@gmail.com>
2011-04-06 00:18:14 +0530

    refs #4318. UI fixes for online store. smoke tested manually

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -26,19 +26,23 @@ module OrdersHelper
   # 
   #  Mon Apr  4 23:02:39 IST 2011, ramonrails
   #   * https://redmine.corp.halomonitor.com/issues/4318
-  def selected_total_cost( _tariff)
+  def selected_total_cost( _tariff, _order)
     if _tariff.blank?
       '?'
     else
-      if _tariff.shipping.blank?
+      _charge = if _tariff.shipping.blank?
         if @shipping_option.blank?
-          USD_value( _tariff.upfront_charge)
+          _tariff.upfront_charge
         else
-          USD_value( _tariff.upfront_charge + @shipping_option.price)
+          _tariff.upfront_charge + @shipping_option.price
         end
       else
-        USD_value( _tariff.upfront_charge)
+        _tariff.upfront_charge
       end
+      #   * apply dealer_install_fee charge only when checkbox "on"
+      #   * omitting this charge from tariff requires more complex code. this is easier and simpler
+      _charge -= _tariff.dealer_install_fee.to_i unless _order.dealer_install_fee_applies
+      USD_value( _charge)
     end
   end
   
