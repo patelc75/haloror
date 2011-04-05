68c28840b34fe34534d2dbe26eb733a0960ee018
ramonrails <ramonrails@gmail.com>
2011-04-05 20:53:54 +0530

    refs #4318. myhalo clip product now shows shipping ption and dealer install fee correctly

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -7,18 +7,18 @@ module OrdersHelper
   # 
   #  Mon Apr  4 23:02:31 IST 2011, ramonrails
   #   * https://redmine.corp.halomonitor.com/issues/4318
-  def selected_shipping_option
-     if @complete_tariff.blank? 
+  def selected_shipping_option( _tariff)
+     if _tariff.blank? 
       '?'
      else 
-       if @complete_tariff.shipping.blank? 
+       if _tariff.shipping.blank? 
          if @shipping_option.blank? 
           'Select above'
          else 
           USD_value( @shipping_option.price) 
          end 
        else 
-        USD_value( @complete_tariff.shipping) 
+        USD_value( _tariff.shipping) 
        end 
      end 
   end
@@ -26,18 +26,18 @@ module OrdersHelper
   # 
   #  Mon Apr  4 23:02:39 IST 2011, ramonrails
   #   * https://redmine.corp.halomonitor.com/issues/4318
-  def selected_total_cost
-    if @complete_tariff.blank?
+  def selected_total_cost( _tariff)
+    if _tariff.blank?
       '?'
     else
-      if @complete_tariff.shipping.blank?
+      if _tariff.shipping.blank?
         if @shipping_option.blank?
-          USD_value( @complete_tariff.upfront_charge)
+          USD_value( _tariff.upfront_charge)
         else
-          USD_value( @complete_tariff.upfront_charge + @shipping_option.price)
+          USD_value( _tariff.upfront_charge + @shipping_option.price)
         end
       else
-        USD_value( @complete_tariff.upfront_charge)
+        USD_value( _tariff.upfront_charge)
       end
     end
   end
