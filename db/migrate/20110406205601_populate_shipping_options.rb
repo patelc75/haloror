9b909b0d7e83742263bf34d93e5b3dfde59a4dae
Chirag Patel <chirag@chirag.name>
2011-04-18 15:37:42 -0500

    refs #4318 take out the word 'Shipping' in shipping options in migrations

--- db/migrate/20110406205601_populate_shipping_options.rb
+++ db/migrate/20110406205601_populate_shipping_options.rb
@@ -1,8 +1,8 @@
 class PopulateShippingOptions < ActiveRecord::Migration
   def self.up 
-    ShippingOption.create(:description => "UPS Overnight Shipping", :price => 60)
-    ShippingOption.create(:description => "UPS 2 Day Shipping", :price => 28)
-    ShippingOption.create(:description => "UPS Ground Shipping", :price => 16)        
+    ShippingOption.create(:description => "UPS Overnight", :price => 60)
+    ShippingOption.create(:description => "UPS 2 Day", :price => 28)
+    ShippingOption.create(:description => "UPS Ground", :price => 16)        
   end
 
   def self.down
