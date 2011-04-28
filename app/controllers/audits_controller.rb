95536e147af92ae476b9d72164a27fc4185600ac
Chirag Patel <chirag@chirag.name>
2011-04-27 23:50:03 -0500

    4423 Better auditing support + prevent login/crypted_password from updating every time the user object is saved

--- app/controllers/audits_controller.rb
+++ app/controllers/audits_controller.rb
@@ -12,13 +12,17 @@ class AuditsController < ApplicationController
     # @partial = ( _model_type.blank? ? 'blank_audit' : "#{_model_type}_audit")
     _model_id = params["#{_model_type}_id"] # we just collected this parameter name above
     @model_object = _model_type.classify.constantize.find( _model_id)
-    @audits = Audit.all( :conditions => { :auditable_id => _model_id, :auditable_type => _model_type.split('_').first.classify } )
-    @audits = @audits.paginate :per_page => 10, :page => params[:page]
+    @audits = Audit.all( :conditions => { :auditable_id => _model_id, :auditable_type => _model_type.split('_').first.classify } )    
+    
+    if _model_type == 'user'
+      profile = Profile.first(:conditions => { :user_id => _model_id })  
+      @audits += Audit.all( :conditions => { :auditable_id => profile.id, :auditable_type => 'Profile' } ) 
+      user_intake_object = @model_object.user_intakes[0]            
+      @audits += Audit.all( :conditions => { :auditable_id => user_intake_object.id, :auditable_type => 'Profile' } ) if !user_intake_object.nil?    
+    end
 
-    # respond_to do |format|
-    #   format.html # index.html.erb
-    #   format.xml  { render :xml => @audits }
-    # end
+    @audits.sort! {|a,b| b.created_at <=> a.created_at } if @audits.length > 1
+    @audits = @audits.paginate :per_page => 10, :page => params[:page] #ordering does not work for some reason: , :order => 'created_at desc'            
   end
 
   # GET /audits/1
