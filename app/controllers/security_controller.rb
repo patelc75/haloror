9ac2a279bf2e6c95e8dc4c15b9aafdf573949b95
ramonrails <ramonrails@gmail.com>
2011-06-03 02:29:04 +0530

    refs #4508

--- app/controllers/security_controller.rb
+++ app/controllers/security_controller.rb
@@ -1,12 +1,11 @@
 class SecurityController < ApplicationController
-  def index
-  end
   
-  def unauthorized
-    
-  end
+  def index; end
+  def unauthorized; end
+
   def is_admin
-    if current_user && current_user != :false && current_user.is_super_admin?()
+    # if current_user && current_user != :false && current_user.is_super_admin?()
+    if (current_user.class.name == 'User') && current_user.is_super_admin?
       render :text => 'true'
     else
       render :text => 'false'
