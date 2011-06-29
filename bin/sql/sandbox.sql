f8636ff77b10e92a46a79a5eb6dc0fccbca6784c
Chirag Patel <chirag@chirag.name>
2011-06-29 13:07:08 -0500

    sandbox.sql update

--- bin/sql/sandbox.sql
+++ bin/sql/sandbox.sql
@@ -16,9 +16,9 @@ select * from users_by_user_intake_id(89);
 -- change group for user intake when broken!
 select * from groups where name like 'ml_kevinkelly';          -- group_id = 145
 select * from roles where authorizable_id = 145;               -- role_id = 782      
-select * from users_by_user_intake_id(112);                    -- user_id = 1543
-insert into roles_users (user_id, role_id) values (1543, 782); -- update group                                          
-update user_intakes set group_id = 145 where id = 112;         -- update group
+select * from users_by_user_intake_id(133);                    -- user_id = 1599
+insert into roles_users (user_id, role_id) values (1599, 782); -- update group                                          
+update user_intakes set group_id = 145 where id = 133;         -- update group
 	
 -- refs #3666 debugging safetycare account number ------------------------------------------------------------------------------------------------
 select id, first_name, last_name, account_number from profiles where account_number is not null order by account_number desc;
